5cdd434355cae6c8fc88e65b98eb79bee6c58f5c
ejk <ejkreinar@gmail.com>
2017-11-11 21:37:23 -0500

    Switch to 18-bit fixed point for better resource usage

--- example-prjs/simple-demo/firmware/parameters.h
+++ example-prjs/simple-demo/firmware/parameters.h
@@ -11,13 +11,13 @@
 #define N_LAYER_1 32
 #define N_OUTPUTS 1
 
-typedef ap_fixed<32,10> accum_t;
-typedef ap_fixed<32,8> weight_t;
-typedef ap_fixed<32,8> bias_t;
-typedef ap_fixed<32,8> input_t;
-typedef ap_fixed<32,8> result_t;
+typedef ap_fixed<18,10> accum_t;
+typedef ap_fixed<18,8> weight_t;
+typedef ap_fixed<18,8> bias_t;
+typedef ap_fixed<18,8> input_t;
+typedef ap_fixed<18,8> result_t;
 //hls-fpga-machine-learning insert layer-precision
-typedef ap_fixed<32,8> layer1_t;
+typedef ap_fixed<18,8> layer1_t;
 
 //hls-fpga-machine-learning insert layer-config
 struct config1 : nnet::layer_t {
@@ -27,9 +27,9 @@ struct config1 : nnet::layer_t {
         static const unsigned roll_factor_in = 1;
         static const unsigned roll_factor_out = 1;
         static const bool store_weights_in_bram = false;
-        typedef ap_fixed<32,10> acc_t;
-        typedef ap_fixed<32,8> bias_t;
-        typedef ap_fixed<32,8> weight_t;
+        typedef ap_fixed<18,10> acc_t;
+        typedef ap_fixed<18,8> bias_t;
+        typedef ap_fixed<18,8> weight_t;
         };
 struct config2 : nnet::layer_t {
         static const unsigned n_in = N_LAYER_1;
@@ -38,9 +38,9 @@ struct config2 : nnet::layer_t {
         static const unsigned roll_factor_in = 1;
         static const unsigned roll_factor_out = 1;
         static const bool store_weights_in_bram = false;
-        typedef ap_fixed<32,10> acc_t;
-        typedef ap_fixed<32,8> bias_t;
-        typedef ap_fixed<32,8> weight_t;
+        typedef ap_fixed<18,10> acc_t;
+        typedef ap_fixed<18,8> bias_t;
+        typedef ap_fixed<18,8> weight_t;
         };
 
 #endif 
