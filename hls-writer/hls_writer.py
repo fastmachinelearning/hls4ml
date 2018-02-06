e977406d4c29dac1f8ce1565389024132e111b32
Javier Duarte <jduarte@caltech.edu>
2018-02-05 19:10:00 -0600

    correct inputs to unflatten and later conv layers

--- hls-writer/hls_writer.py
+++ hls-writer/hls_writer.py
@@ -100,10 +100,10 @@ def hls_writer(layer_list, yamlConfig):
                     newline += '    nnet::compute_layer<{}, {}, config{}>({}, logits{}, w{}, b{});\n'.format(input_type, output_type, i, input_object, i, i, i, i)
                 elif layer_list[i-1]['class_name']=='Conv1D':
                     if i>1 and layer_list[i-2]['class_name']=='Conv1D':
-                        newline += '    {} conv_layer{}_in[{}][{}];\n'.format(input_type,i,y_out,n_filt)
+                        newline += '    {} conv_layer{}_in[{}][{}];\n'.format(input_type,i,y_in,n_chan)
                         if yamlConfig["IOType"] == "io_parallel": newline += '    #pragma HLS ARRAY_PARTITION variable=logits{} complete dim=0\n'.format(i)
                         if yamlConfig["IOType"] == "io_serial":   newline += '    #pragma HLS STREAM variable=logits{} dim=1\n'.format(i)
-                        newline += '    nnet::unflatten<{}, {}, {}>({}, conv_layer{}_in);\n'.format(input_type, y_out, n_filt, input_object, i)                              
+                        newline += '    nnet::unflatten<{}, {}, {}>({}, conv_layer{}_in);\n'.format(input_type, y_in, n_chan, input_object, i)                              
                         newline += '    {} conv_layer{}_out[{}][{}];\n'.format(output_type,i,y_out,n_filt)
                         if yamlConfig["IOType"] == "io_parallel": newline += '    #pragma HLS ARRAY_PARTITION variable=conv_layer{}_out complete dim=0\n'.format(i)
                         if yamlConfig["IOType"] == "io_serial":   newline += '    #pragma HLS STREAM variable=conv_layer{}_out dim=1\n'.format(i)
