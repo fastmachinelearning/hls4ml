b1c10649d86071ea3a4ff7c99350d76eed5b7680
Javier Duarte <jduarte@caltech.edu>
2018-02-05 20:15:53 -0600

    add iostream

--- hls-template/firmware/myproject.cpp
+++ hls-template/firmware/myproject.cpp
@@ -16,6 +16,7 @@
 //    You should have received a copy of the GNU General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
+#include <iostream>
 
 #include "parameters.h"
 #include "myproject.h"
