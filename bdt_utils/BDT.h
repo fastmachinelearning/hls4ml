9dbce465045f9a5246cb147ecbda6a69faba2689
sioni <ss5310@ic.ac.uk>
2018-05-08 17:47:13 +0100

    BDT implementation added. Consolidate a bit.

---
+++ bdt_utils/BDT.h
@@ -0,0 +1,96 @@
+#ifndef BDT_H__
+#define BDT_H__
+
+#include "ap_fixed.h"
+
+namespace BDT{
+
+template<int n_nodes, int n_leaves, class input_t, class score_t>
+struct Tree {
+	int feature[n_nodes];
+	double threshold[n_nodes];
+	double value[n_nodes];
+	int children_left[n_nodes];
+	int children_right[n_nodes];
+	int parent[n_nodes];
+
+	score_t decision_function(input_t x){
+		#pragma HLS pipeline II = 1
+
+		bool comparison[n_nodes];
+		bool activation[n_nodes];
+		bool activation_leaf[n_leaves];
+		double value_leaf[n_leaves];
+
+		// Execute all comparisons
+		Compare: for(int i = 0; i < n_nodes; i++){
+			#pragma HLS unroll
+			// Only non-leaf nodes do comparisons
+			if(x[feature[i]] != -2){ // -2 means is a leaf (at least for sklearn)
+				comparison[i] = x[feature[i]] <= threshold[i];
+			}else{
+				comparison[i] = true;
+			}
+		}
+
+		// Determine node activity for all nodes
+		int iLeaf = 0;
+		Activate: for(int i = 0; i < n_nodes; i++){
+			#pragma HLS unroll
+			// Root node is always active
+			if(i == 0){
+				activation[i] = true;
+			}else{
+				// If this node is the left child of its parent
+				if(i == children_left[parent[i]]){
+					activation[i] = comparison[parent[i]] && activation[parent[i]];
+				}else{ // Else it is the right child
+					activation[i] = !comparison[parent[i]] && activation[parent[i]];
+				}
+			}
+			// Skim off the leaves
+			if(children_left[i] == -1){ // is a leaf
+				activation_leaf[iLeaf] = activation[i];
+				value_leaf[iLeaf] = value[i];
+				iLeaf++;
+			}
+		}
+
+		// Set each bit of addr to the corresponding leaf activation
+		ap_uint<n_leaves> active_leaf_addr;
+		Select: for(int i = 0; i < n_leaves; i++){
+			#pragma HLS unroll
+			active_leaf_addr[i] = activation_leaf[i];
+		}
+		score_t y;
+		switch (active_leaf_addr){
+		case 1 : y = value_leaf[0]; break;
+		case 2 : y = value_leaf[1]; break;
+		case 4 : y = value_leaf[2]; break;
+		case 8 : y = value_leaf[3]; break;
+		case 16 : y = value_leaf[4]; break;
+		case 32 : y = value_leaf[5]; break;
+		case 64 : y = value_leaf[6]; break;
+		case 128 : y = value_leaf[7]; break;
+		default : y = 0;
+		}
+		return y;
+	}
+};
+
+template<int n_trees, int n_nodes, int n_leaves, class input_t, class score_t>
+struct BDT{
+	Tree<n_nodes, n_leaves, input_t, score_t> trees[n_trees];
+	BDT(){}
+
+	score_t decision_function(input_t x){
+		score_t score = 0;
+		Trees: for(int i = 0; i < n_trees; i++){
+			score += trees[i].decision_function(x);
+		}
+		return score;
+	}
+};
+
+}
+#endif
--- bdt_utils/parameters.h
+++ bdt_utils/parameters.h
@@ -1,14 +1,15 @@
 #ifndef BDT_PARAMS_H__
 #define BDT_PARAMS_H__
 
-#include <stdexcept>
 #include "ap_fixed.h"
 
-namespace BDT{
+const int n_trees = 2;
+const int n_nodes = 15;
+const int n_leaves = 8;
+const int n_features = 4; // Number of features
 
-const int N_features = 4; // Number of features
 typedef ap_fixed<18,9> input_t; // Input features type
-typedef input_t input_arr_t[BDT::N_features];
+typedef input_t input_arr_t[n_features];
 typedef ap_fixed<18, 9> score_t; // Type for tree score
 
 // TODO make this work
@@ -30,16 +31,5 @@ typedef struct input_struct_t{
 	}
 
 } input_struct_t;
-
-template<int N>
-struct tree_params_t {
-	int feature[N];
-	double threshold[N];
-	double value[N];
-	int children_left[N];
-	int children_right[N];
-	int parent[N];
-};
-}
 
 #endif
