df8f8c7f137801f74a5221373cf3213994313e9f
Javier Duarte <jduarte1@fnal.gov>
2018-06-10 21:16:24 -0700

    need to activate hls4ml-env first; rename file

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 conda create --copy --name hls4ml-env python=2.7
 
-#
-#conda install --name hls4ml-env --file pytorch-training.conda
+source activate hls4ml-env
 
 ## CPU version of pytorch for now
 #conda install pytorch torchvision -c pytorch
 conda install pytorch-cpu torchvision-cpu -c pytorch
 
 conda install -c anaconda scikit-learn h5py pyyaml
-#source activate hls4ml-env
-
