18b67674037e39fa4162153cad40203a3936fe67
Nhan Tran <nhan.v.tran@gmail.com>
2018-07-10 10:55:27 -0500

    fix

--- install_miniconda3.sh
+++ install_miniconda3.sh
@@ -1,3 +1,3 @@
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
-export PATH="$CONDA_PREFIX/miniconda2/bin:$PATH"
+export PATH="$CONDA_PREFIX/miniconda3/bin:$PATH"
