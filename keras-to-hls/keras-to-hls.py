43525f912f2819dea862e50472df825b15b3854b
Javier Duarte <jduarte1@fnal.gov>
2018-09-11 18:17:56 -0500

    make default 4096 for all

--- keras-to-hls/keras-to-hls.py
+++ keras-to-hls/keras-to-hls.py
@@ -10,7 +10,7 @@ import sys
 from shutil import copyfile
 import math
 
-MAXMULT = 1024
+MAXMULT = 4096
 
 filedir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0,os.path.join(filedir, "..", "hls-writer"))
