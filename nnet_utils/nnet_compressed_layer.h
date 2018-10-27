d1d399df3d99b38ba9033f6c91768901a7bd76b0
Giuseppe Di Guglielmo <giuseppe@cs.columbia.edu>
2018-10-26 21:47:43 -0400

    Some cosmetics on io_parallel
    Signed-off-by: Giuseppe Di Guglielmo <giuseppe@cs.columbia.edu>

--- nnet_utils/nnet_compressed_layer.h
+++ nnet_utils/nnet_compressed_layer.h
@@ -38,14 +38,13 @@ void compute_compressed_layer(
 {
     // Create a separate module for each layer
 #pragma HLS inline off
+
+    // Explicitly optimize unchanging weights/biases
 #pragma HLS function_instantiate variable=weights,biases
 
+    // Intermediate computational buffers
     typename CONFIG_T::compressed_weight_t mult[CONFIG_T::n_nonzeros];
-//#pragma HLS data_pack variable=mult struct_level
-
     typename CONFIG_T::accum_t acc[CONFIG_T::n_out];
-
-    // Use a function_instantiate in case it helps to explicitly optimize unchanging weights/biases
 
     printf("INFO: compute_compressed_layer\n");
     printf("INFO:   n_in: %u\n", CONFIG_T::n_in);
@@ -55,6 +54,7 @@ void compute_compressed_layer(
     printf("INFO:   n_nonzeros: %u\n", CONFIG_T::n_nonzeros);
     printf("INFO:   store_weights_in_bram: %u\n", CONFIG_T::store_weights_in_bram);
 
+    // Parallel vs. Serial IO
     if (CONFIG_T::io_type == io_parallel) {
         // For parallel inputs:
         //   - completely partition arrays
@@ -62,36 +62,51 @@ void compute_compressed_layer(
 #pragma HLS ARRAY_PARTITION variable=biases complete
 #pragma HLS ARRAY_PARTITION variable=acc complete
 
+        // Pipelining force all the loops being unrolled
 #pragma HLS PIPELINE II=CONFIG_T::reuse_factor
 
+        // Replace ceil function with home-made macro prevents Vivado 2018.2 segfault
         int multiplier_limit = DIV_ROUNDUP(CONFIG_T::n_nonzeros, CONFIG_T::reuse_factor);
 #pragma HLS ALLOCATION instances=mul limit=multiplier_limit operation
 
-         if (CONFIG_T::store_weights_in_bram) {
+        // BRAMs vs. registers
+        if (CONFIG_T::store_weights_in_bram) {
+            // Reshape the arrays as memory with a larger bit-width
 #pragma HLS ARRAY_RESHAPE variable=weights block factor=multiplier_limit
 #pragma HLS RESOURCE variable=weights core=ROM_2P_BRAM
 #pragma HLS ARRAY_RESHAPE variable=mult block factor=multiplier_limit
 #pragma HLS RESOURCE variable=mult core=RAM_2P_BRAM
 
-    // Pack the row_index, col_index, and weight in a single 32-bit memory element
-#pragma HLS data_pack variable=weights struct_level
-#pragma HLS data_pack variable=mult struct_level
+            // Pack the row_index, col_index, and weight in a single 32-bit memory element
+//#pragma HLS data_pack variable=weights struct_level
+            // TODO: Packing mult provides worst results (disable it)
+//#pragma HLS data_pack variable=mult struct_level
          } else {
+             // Completely partition the remaining arrays
 #pragma HLS ARRAY_PARTITION variable=weights complete
 #pragma HLS ARRAY_PARTITION variable=mult complete
          }
 
     } else if (CONFIG_T::io_type == io_serial) {
-        // TODO: to be extended and tested! 
-
-#pragma HLS ARRAY_PARTITION variable=biases complete
-#pragma HLS ARRAY_PARTITION variable=acc complete
-
-
-        if (CONFIG_T::store_weights_in_bram) {
-#pragma HLS RESOURCE variable=weights core=ROM_2P_BRAM
-#pragma HLS RESOURCE variable=mul core=ROM_2P_BRAM
-        }
+//#pragma HLS ARRAY_PARTITION variable=biases complete
+//#pragma HLS ARRAY_PARTITION variable=acc complete
+//
+//        // Replace ceil function with home-made macro prevents Vivado 2018.2 segfault
+//        int multiplier_limit = DIV_ROUNDUP(CONFIG_T::n_nonzeros, CONFIG_T::reuse_factor);
+//#pragma HLS ALLOCATION instances=mul limit=multiplier_limit operation
+//
+//        if (CONFIG_T::store_weights_in_bram) {
+//#pragma HLS RESOURCE variable=weights core=ROM_2P_BRAM
+//#pragma HLS RESOURCE variable=mul core=RAM_2P_BRAM
+//
+//            // Pack the row_index, col_index, and weight in a single 32-bit memory element
+//#pragma HLS data_pack variable=weights struct_level
+//            // TODO: packing the structs in mult array provide worst results.
+////#pragma HLS data_pack variable=mult struct_level
+//        } else {
+//#pragma HLS ARRAY_PARTITION variable=weights complete
+//#pragma HLS ARRAY_PARTITION variable=mult complete
+//        }
 ////#pragma HLS DATAFLOW
     }
 
@@ -118,6 +133,7 @@ ACCUMULATOR_INIT_L:
         acc[i] = (typename CONFIG_T::accum_t) (biases[i]);
     }
 
+    // Accumulate over the columns
 COMPRESSED_ACCUMULATOR_L:
     for(unsigned i = 0; i < CONFIG_T::n_nonzeros; i++) {
         if (CONFIG_T::io_type == io_serial){
