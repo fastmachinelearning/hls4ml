519ccce4b4f018e8a19633df2e01ffb4b69e60eb
Nhan Tran <nhan.v.tran@gmail.com>
2019-01-31 23:04:07 -0600

    a working thing with dependence pragma  that  controls II but needs optimization of latency and resources

--- nnet_utils/nnet_layer.h
+++ nnet_utils/nnet_layer.h
@@ -48,6 +48,15 @@ struct layer_config
     // partitioning arrays cyclically to go with roll factors?
 };
 
+// begin function declarations
+template<class data_T, typename CONFIG_T>
+void matvec_op(
+    data_T                       data[CONFIG_T::n_in],
+    typename CONFIG_T::accum_t   acc[CONFIG_T::n_out],
+    typename CONFIG_T::weight_t  weights[CONFIG_T::n_in*CONFIG_T::n_out],
+    int                          reuse_index);
+// begin function declarations
+
  template<class data_T, class res_T, typename CONFIG_T>
 void compute_layer(
     data_T    data[CONFIG_T::n_in],
@@ -55,22 +64,9 @@ void compute_layer(
     typename CONFIG_T::weight_t  weights[CONFIG_T::n_in*CONFIG_T::n_out],
     typename CONFIG_T::bias_t    biases[CONFIG_T::n_out])
 {
-    
-    data_T cache;
-    typename CONFIG_T::accum_t mult[CONFIG_T::n_in*CONFIG_T::n_out];
-    typename CONFIG_T::accum_t acc[CONFIG_T::n_out];
-
-    // Use a function_instantiate in case it helps to explicitly optimize unchanging weights/biases
-    #pragma HLS function_instantiate variable=weights,biases
-
-    // For parallel inputs:
-    //   - completely partition arrays
-    //   - if we have a reuse factor, limit number of multipliers
-    #pragma HLS ARRAY_PARTITION variable=biases complete
-    #pragma HLS ARRAY_PARTITION variable=acc complete
 
     // Pipelining force all the loops being unrolled
-    #pragma HLS PIPELINE II=CONFIG_T::reuse_factor
+    // #pragma HLS PIPELINE II=CONFIG_T::reuse_factor
 
     // Replace ceil function with home-made macro prevents Vivado 2018.2 segfault
     const int totals_multipliers = CONFIG_T::n_in*CONFIG_T::n_out;
@@ -78,9 +74,16 @@ void compute_layer(
     // #pragma HLS ALLOCATION instances=mul limit=multiplier_limit operation
 
     // Workaround the above restriction.
+    // Use a function_instantiate in case it helps to explicitly optimize unchanging weights/biases
+    #pragma HLS function_instantiate variable=weights,biases
     #pragma HLS ARRAY_RESHAPE variable=weights block factor=multiplier_limit
-    // #pragma HLS ARRAY_RESHAPE variable=mult complete
-    #pragma HLS ARRAY_RESHAPE variable=mult block factor=multiplier_limit
+    #pragma HLS ARRAY_PARTITION variable=biases complete
+    
+    // typename CONFIG_T::accum_t mult[CONFIG_T::n_in*CONFIG_T::n_out];
+    typename CONFIG_T::accum_t acc[CONFIG_T::n_out];
+    // #pragma HLS ARRAY_RESHAPE variable=mult    block factor=multiplier_limit
+    #pragma HLS ARRAY_PARTITION variable=acc complete
+    #pragma HLS DEPENDENCE variable=acc inter false
 
     // Initialize accumulator with input biases
     ResetAccum: for(int iacc = 0; iacc < CONFIG_T::n_out; iacc++) {
@@ -91,42 +94,88 @@ void compute_layer(
     // ------- accumulated outside --------
     int rufactor=CONFIG_T::reuse_factor;
     ReuseLoop: for (int ir = 0; ir < rufactor; ir++){
-        //#pragma HLS PIPELINE ii=1 rewind
-	    // #pragma HLS PIPELINE II=CONFIG_T::reuse_factor
+        #pragma HLS PIPELINE ii=1 rewind
+        matvec_op<data_T,CONFIG_T>(data,acc,weights,ir);
 
-        ///
         // printf("on the clock tick \n");
-        MultLoop1: for (int im = 0; im < multiplier_limit; im++){
-            // #pragma UNROLL
+        // MultLoop1: for (int im = 0; im < multiplier_limit; im++){
+        //     #pragma UNROLL
             
-            int w_index   = ir + rufactor * im;
-            int in_index  = w_index / CONFIG_T::n_out;
-            int out_index = w_index % CONFIG_T::n_out;
+        //     int w_index   = ir + rufactor * im;
+        //     int in_index  = w_index / CONFIG_T::n_out;
+        //     int out_index = w_index % CONFIG_T::n_out;
 
-            if (w_index >= CONFIG_T::n_in*CONFIG_T::n_out) continue; // check out of bounds
+        //     if (w_index >= CONFIG_T::n_in*CONFIG_T::n_out) continue; // check out of bounds
 
-            // printf("ir = %i, im = %i, w_index = %i, in_index = %i, out_index = %i \n", ir, im, w_index, in_index, out_index);
-            mult[w_index] = data[in_index] * weights[w_index];
-        }
+        //     // printf("ir = %i, im = %i, w_index = %i, in_index = %i, out_index = %i \n", ir, im, w_index, in_index, out_index);
+        //     mult[w_index] = data[in_index] * weights[w_index];
+        // }
         ///
     }
 
     // Accumulate multiplication result
-    Accum1: for(int ii = 0; ii < CONFIG_T::n_in; ii++) {
-		// #pragma HLS PIPELINE II=CONFIG_T::reuse_factor
-        // #pragma HLS UNROLL 
-        Accum2: for(int jj = 0; jj < CONFIG_T::n_out; jj++) {
-            // #pragma HLS UNROLL
-	    	int index = ii*CONFIG_T::n_out+jj;
-	    	acc[jj] += mult[index];
-        }
-    }
+  //   Accum1: for(int ii = 0; ii < CONFIG_T::n_in; ii++) {
+		// // #pragma HLS PIPELINE II=CONFIG_T::reuse_factor rewind
+  //       #pragma HLS UNROLL 
+  //       Accum2: for(int jj = 0; jj < CONFIG_T::n_out; jj++) {
+  //           #pragma HLS UNROLL
+	 //    	int index = ii*CONFIG_T::n_out+jj;
+	 //    	acc[jj] += mult[index];
+  //       }
+  //   }
 
     // Cast to "res_t" type
     Result: for(int ires = 0; ires < CONFIG_T::n_out; ires++){
-        // #pragma HLS UNROLL
+        #pragma HLS UNROLL
+        printf("acc[%i] = %0.4f ", ires, (float) acc[ires]);
         res[ires] = (res_T) (acc[ires]);
     }    
+    printf("\n");
+
+}
+
+template<class data_T, typename CONFIG_T>
+void matvec_op(
+    data_T                       data[CONFIG_T::n_in],
+    typename CONFIG_T::accum_t   acc[CONFIG_T::n_out],
+    typename CONFIG_T::weight_t  weights[CONFIG_T::n_in*CONFIG_T::n_out],
+    int                          reuse_index){
+
+    #pragma HLS PIPELINE
+
+    int rufactor=CONFIG_T::reuse_factor;
+    const int multiplier_limit = DIV_ROUNDUP(CONFIG_T::n_in*CONFIG_T::n_out, CONFIG_T::reuse_factor);
+    typename CONFIG_T::accum_t mult[multiplier_limit];
+    #pragma HLS ARRAY_PARTITION variable=mult complete
+
+    MultLoop: 
+    for (int im = 0; im < multiplier_limit; im++){
+        #pragma UNROLL
+        
+        int w_index   = reuse_index + rufactor * im;
+        int in_index  = w_index / CONFIG_T::n_out;
+        int out_index = w_index % CONFIG_T::n_out;
+
+        if (w_index >= CONFIG_T::n_in*CONFIG_T::n_out) continue; // check out of bounds
+        printf("ir = %i, im = %i, w_index = %i, in_index = %i, out_index = %i, data = %0.4f, weight = %0.4f \n", reuse_index, im, w_index, in_index, out_index, (float) data[in_index], (float) weights[w_index]);
+        mult[im] = data[in_index] * weights[w_index];
+        acc[out_index] += mult[im];
+
+    }
+
+    // AccumLoop:
+    // for (int im = 0; im < multiplier_limit; im++){
+    //     #pragma UNROLL
+        
+    //     int w_index   = reuse_index + rufactor * im;
+    //     int out_index = w_index % CONFIG_T::n_out;
+
+    //     if (w_index >= CONFIG_T::n_in*CONFIG_T::n_out) continue; // check out of bounds
+    //     // printf("ir = %i, im = %i, w_index = %i, in_index = %i, out_index = %i \n", ir, im, w_index, in_index, out_index);
+    //     // printf("acc[%i] = %0.4f ", out_index, (float) acc[out_index]);
+    // }
+    // printf("\n");
+
 }
 
 }
