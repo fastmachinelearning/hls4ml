7b40048e74f1927f18a8c8a317ca39a28e0a7a5b
Giuseppe Di Guglielmo <giuseppe@cs.columbia.edu>
2019-02-22 15:14:11 -0500

    Stress testing FC layer implementation on the reuse factor

---
+++ keras-to-hls/stress-test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Let's pollute a sub-directory.
+DIR=stress-dir
+mkdir -p $DIR
+
+# Model name.
+MODEL="KERAS_3layer"
+# We assume the model files being
+# KerasJson: ../example-keras-model-files/MODEL.json
+# KerasH5:   ../example-keras-model-files/MODEL_weights.h5
+
+# Begin, end and step for Reuse Factor.
+RF_BEGIN=1
+RF_END=10
+RF_STEP=1
+
+# Count how many tests.
+let "test_count=0"
+
+# Iterate over reuse factor value.
+for rf in $(seq $RF_BEGIN $RF_STEP $RF_END); do
+    let "test_count++" && \
+        echo "Test # 1: ReuseFactor=$rf, Model:$MODEL" && \
+        sed "s/>>>REUSE<<</$rf/g" keras-config-REUSE-MODEL.yml | sed "s/>>>MODEL<<</$MODEL/g" > $DIR/keras-config-$rf-$MODEL.yml && \
+        cd $DIR && \
+        python ../keras-to-hls.py -c keras-config-$rf-$MODEL.yml && \
+        cd $MODEL\_RF$rf && \
+        cp ../../Makefile.ini Makefile && \
+        make && \
+        cd ../..
+done
