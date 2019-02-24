be079456cf918bb3cd1b9f7728abf5e8f9d22a0b
Giuseppe Di Guglielmo <giuseppe@cs.columbia.edu>
2019-02-24 12:14:41 -0500

    If Vivado HLS crashes, be ready for the next run

--- keras-to-hls/stress-test.sh
+++ keras-to-hls/stress-test.sh
@@ -26,14 +26,14 @@ let "test_count=0"
 
 # Iterate over reuse factor value.
 for rf in $(seq $(expr $RF_BEGIN - 1) $RF_STEP $RF_END); do
-    let "test_count++" && \
-        echo "Test # 1: ReuseFactor=$rf, Model:$MODEL" && \
-        sed "s/>>>REUSE<<</$rf/g" keras-config-REUSE-MODEL.yml | sed "s/>>>MODEL<<</$MODEL/g" > $DIR/keras-config-$rf-$MODEL.yml && \
-        cd $DIR && \
-        python ../keras-to-hls.py -c keras-config-$rf-$MODEL.yml && \
-        cd $MODEL\_RF$rf && \
-        cp ../../Makefile.ini Makefile && \
-        make && \
-        cd ../.. && \
-        ./parse-vivadohls-report.sh ./stress-dir/$MODEL\_RF$rf/myproject_prj $MODEL $rf $RESULT_FILE
+    let "test_count++"
+    echo "Test # 1: ReuseFactor=$rf, Model:$MODEL"
+    sed "s/>>>REUSE<<</$rf/g" keras-config-REUSE-MODEL.yml | sed "s/>>>MODEL<<</$MODEL/g" > $DIR/keras-config-$rf-$MODEL.yml
+    cd $DIR
+    python ../keras-to-hls.py -c keras-config-$rf-$MODEL.yml
+    cd $MODEL\_RF$rf
+    cp ../../Makefile.ini Makefile
+    make
+    cd ../..
+    ./parse-vivadohls-report.sh ./stress-dir/$MODEL\_RF$rf/myproject_prj $MODEL $rf $RESULT_FILE
 done
