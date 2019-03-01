a151d5853e1c5cdc5e33ce654ca195b397747953
Giuseppe Di Guglielmo <giuseppe@cs.columbia.edu>
2019-03-01 10:41:49 -0500

    Stress test (RF) script runs 4 parallel instances of Vivado HLS

--- keras-to-hls/stress-test.sh
+++ keras-to-hls/stress-test.sh
@@ -17,6 +17,9 @@ RF_BEGIN=1
 RF_END=64
 RF_STEP=1
 
+# Run at most $THREADS instances of Vivado.
+THREADS=4
+
 # Enable/disable Vivado HLS, Vivado (logic synthesis), and result collection.
 RUN_HLS=1
 RUN_LS=0
@@ -31,11 +34,11 @@ RESULT_FILE=stress_results.csv
 # Count how many tests.
 let "test_count=0"
 
-# Iterate over reuse factor value.
-for rf in $(seq $(expr $RF_BEGIN) $RF_STEP $RF_END); do
+run_hls4ml_vivado ()
+{
+    test_count=$1
+    rf=$2
 
-    # Keep trace of current test.
-    let "test_count++"
     echo "Test # $test_count: ReuseFactor=$rf, Model:$MODEL"
 
     # Move to the working directory.
@@ -47,34 +50,54 @@ for rf in $(seq $(expr $RF_BEGIN) $RF_STEP $RF_END); do
     if [ ! $? -eq 0 ]; then echo "Cannot create HLS4ML configuration file $DIR/keras-config-$rf-$MODEL.yml"; cd ..; continue; fi
 
     # Run HLS4ML generators.
-    python ../keras-to-hls.py -c keras-config-$rf-$MODEL.yml
+    python ../keras-to-hls.py -c keras-config-$rf-$MODEL.yml > keras-config-$rf-$MODEL.log
     if [ ! $? -eq 0 ]; then echo "Cannot run HLS4ML generator on with the configuration file $DIR/keras-config-$rf-$MODEL.yml"; cd ..; continue; fi
 
     # Run Vivado HLS.
     if [ $RUN_HLS -eq 1 ]; then
         cd $MODEL\_RF$rf
         if [ ! $? -eq 0 ]; then echo "Cannot find find directory $MODEL\_RF$rf"; cd ../..; continue; fi
-        vivado_hls -f build_prj.tcl
+        # Kill Vivado HLS if does not return after 3 hours.
+        timeout -k 30s 3h vivado_hls -f build_prj.tcl > /dev/null
         if [ ! $? -eq 0 ]; then echo "Vivado HLS failed in $DIR/$MODEL\_RF$rf"; cd ../..; continue; fi
         cd ..
     fi
 
-    # Run Vivado (it does not check if there was a previous HLS run).
-    if [ $RUN_LS -eq 1 ]; then
-        cd $MODEL\_RF$rf
-        echo "open_project myproject_prj" > run_vivado.tcl
-        echo "export_design -flow syn -format ip_catalog" >> run_vivado.tcl
-        echo "exit" >> run_vivado.tcl
-        timeout 4h vivado_hls -f run_vivado.tcl -l vivado.log
-        if [ ! $? -eq 0 ]; then echo "Vivado failed in $DIR/$MODEL\_RF$rf"; cd ../..; continue; fi
-        cd ..
-    fi
+#    # Run Vivado (it does not check if there was a previous HLS run).
+#    if [ $RUN_LS -eq 1 ]; then
+#        cd $MODEL\_RF$rf
+#        echo "open_project myproject_prj" > run_vivado.tcl
+#        echo "export_design -flow syn -format ip_catalog" >> run_vivado.tcl
+#        echo "exit" >> run_vivado.tcl
+#        # Kill Vivado HLS if does not return after 3 hours.
+#        timeout -k 30s 3h vivado_hls -l vivado.log -f run_vivado.tcl > /dev/null
+#        if [ ! $? -eq 0 ]; then echo "Vivado failed in $DIR/$MODEL\_RF$rf"; cd ../..; continue; fi
+#        cd ..
+#    fi
 
-    cd ..
+     cd ..
 
-    # Collect results (it does not check if there were HLS and LS runs).
-    # TODO: Report script does not extract LS information.
-    if [ $RUN_LOG -eq 1 ]; then
-        ./parse-vivadohls-report.sh ./stress-dir/$MODEL\_RF$rf/myproject_prj $MODEL $rf $RESULT_FILE
-    fi
+}
+
+# Iterate over the reuse factor value.
+for rf_base in $(seq $(expr $RF_BEGIN) $THREADS $RF_END); do
+
+    # Run parallel instances of Vivado.
+    for rf in $(seq $rf_base $RF_STEP $(expr $rf_base + $THREADS - 1)); do
+        let "test_count++"
+        run_hls4ml_vivado $test_count $rf &
+    done
+
+    # Wait for all of the previous instances to terminate before collecting the
+    # results and moving on to the next batch of runs.
+    wait
+
+    # Collect the results.
+    for rf in $(seq $rf_base $RF_STEP $(expr $rf_base + $THREADS - 1)); do
+        # Collect results (it does not check if there were HLS and LS runs).
+        # TODO: Report script does not extract LS information.
+        if [ $RUN_LOG -eq 1 ]; then
+            ./parse-vivadohls-report.sh ./stress-dir/$MODEL\_RF$rf/myproject_prj $MODEL $rf $RESULT_FILE
+        fi
+    done
 done
