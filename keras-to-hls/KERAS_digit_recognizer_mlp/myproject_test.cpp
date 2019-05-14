f682aa846b71d3803ab7549a09b8f05e98cd8b3e
Giuseppe Di Guglielmo <giuseppe@cs.columbia.edu>
2019-05-14 16:39:47 -0400

    Add tiny MNIST example

---
+++ keras-to-hls/KERAS_digit_recognizer_mlp/myproject_test.cpp
@@ -0,0 +1,113 @@
+//
+//    rfnoc-hls-neuralnet: Vivado HLS code for neural-net building blocks
+//
+//    Copyright (C) 2017 EJ Kreinar
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <math.h>
+
+#include "firmware/parameters.h"
+#include "firmware/myproject.h"
+#include "nnet_helpers.h"
+
+
+// In Vivado HLS, C and RTL simulations have different working directories,
+// let's call them generically WORK_DIR.
+//
+// - For C simulations,   WORK_DIR is "myproject_prj/solution1/csim/build"
+// - For RTL simulations, WORK_DIR is "myproject_prj/solution1/sim/wrapc"
+//
+// In both the cases, during the simulation, this testbench saves the log file
+// in the current (either C or RTL) WORK_DIR.
+//
+// For validation purposes, we consider the C-simulation results as the golden
+// reference. The validation compares (diff) the current log file (either from
+// C or RTL simulation) with the golden reference which always sits in
+// "WORK_DIR/../csim/build".
+//
+// NOTES:
+// - The C simulations always pass the validation, because the log file is
+//   compared with itself (they both sits in "WORK_DIR/../csim/build").
+// - The C simulation has to be run before the RTL simulation.
+//
+
+#ifdef VALIDATION
+#define LOG_FILE "results.log"
+
+std::string get_exe_path()
+{
+  char cwd[PATH_MAX];
+  if (getcwd(cwd, sizeof(cwd)) == NULL) {
+    return "./";
+  } else {
+    return (std::string(cwd) + "/");
+  }
+}
+#endif
+
+int main(int argc, char **argv)
+{
+
+  //hls-fpga-machine-learning insert data
+  //input_t  data_str[N_INPUTS] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+#include "inputs.h"
+
+  int retval = 0;
+
+  result_t res_str[N_OUTPUTS] = {0};
+  unsigned short size_in, size_out;
+  myproject(data_str, res_str, size_in, size_out);
+
+  for(int i=0; i<N_OUTPUTS; i++){
+    std::cout << res_str[i] << " ";
+  }
+  std::cout << std::endl;
+
+#ifdef VALIDATION
+  // The results are saved in the current working directory.
+  std::ofstream results;
+  results.open(LOG_FILE);
+  for(int i=0; i<N_OUTPUTS; i++){
+    results << res_str[i] << std::endl;
+  }
+  results.close();
+
+  const std::string WORK_DIR = get_exe_path();
+  const std::string GOLD_DIR = get_exe_path() + "../../csim/build/";
+
+  std::string diff_cmd = "diff --brief -w ";
+  diff_cmd += WORK_DIR + LOG_FILE;
+  diff_cmd += " ";
+  diff_cmd += GOLD_DIR + LOG_FILE;
+
+  retval = system(diff_cmd.c_str());
+  if (retval != 0) {
+      std::cout << "ERROR: test FAILED" << std::endl;
+      std::cout << "ERROR:   current sim: " << (WORK_DIR + LOG_FILE) << std::endl;
+      std::cout << "ERROR:   golden  sim: " << (GOLD_DIR + LOG_FILE) << std::endl;
+      retval = 1;
+  } else {
+      printf("INFO: test PASSED\n");
+  }
+#endif
+  return retval;
+}
---
+++ keras-to-hls/KERAS_digit_recognizer_mlp/py/digit_recognizer_mlp.pred.py
@@ -0,0 +1,66 @@
+from __future__ import print_function
+
+import keras
+from keras.models import model_from_json
+from keras.datasets import mnist
+from keras.utils import np_utils
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Model reconstruction from JSON file
+with open('../../example-keras-model-files/KERAS_digit_recognizer_mlp.json', 'r') as f:
+    model = model_from_json(f.read())
+
+# Load weights into the new model
+model.load_weights('../../example-keras-model-files/KERAS_digit_recognizer_mlp_weights.h5')
+
+
+# Load the MNIST data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_test = x_test.reshape(10000, 784)
+x_test = x_test.astype('float32')
+x_test /= 255
+
+# Convert class vectors to binary class matrices
+y_test = keras.utils.to_categorical(y_test, 10)
+
+# Choose a test image
+index_image = 2
+image = x_test[index_image]
+#print("INFO: x_test size", x_test.shape())
+
+# Run prediction
+pred = model.predict(np.array([image]))
+
+# Some information
+print('INFO: input shape: ', image.shape)
+print('INFO: image shape: ', image.reshape(28, 28).shape)
+print('INFO: predictions: ', pred[0])
+print('INFO: top prediction: ', pred.argmax())
+
+def print_array_to_h(data):
+    f = open("inputs.h","w")
+
+    f.write("input_t  data_str[N_INPUTS] = {")
+
+    i=0
+    for d in data:
+        if i==0:
+            f.write("%.12f" % d)
+        else:
+            f.write(", %.12f" % d)
+        i=i+1
+    f.write("};\n")
+    f.close()
+
+
+print_array_to_h(image.reshape(28*28, 1))
+
+# Show image
+plt.imshow(np.array([image]).reshape(28, 28), cmap='Greys')
+plt.show()
+
