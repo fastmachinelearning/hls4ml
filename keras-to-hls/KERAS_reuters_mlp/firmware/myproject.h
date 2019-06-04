83b1f0979e5539c71187927ebc32169b510e832a
Giuseppe Di Guglielmo <giuseppe@cs.columbia.edu>
2019-06-04 11:10:52 -0400

    Add Reuters example

---
+++ keras-to-hls/KERAS_reuters_mlp/firmware/myproject.h
@@ -0,0 +1,86 @@
+//
+//    rfnoc-hls-neuralnet: Vivado HLS code for neural-net building blocks
+//
+//    Copyright (C) 2017 EJ Kreinar
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef MYPROJECT_H_
+#define MYPROJECT_H_
+
+#include <complex>
+#include "ap_int.h"
+#include "ap_fixed.h"
+
+#include "parameters.h"
+
+#ifndef __SYNTHESIS__
+
+#include <fstream>
+#define xstr(a) str(a)
+#define str(a) #a
+template<class T, size_t SIZE>
+void load_txt_file(T *w, const char* fname) {
+
+    std::string full_path = std::string(xstr(WEIGHTS_DIR)) + "/" + std::string(fname);
+    std::ifstream infile(full_path.c_str(), std::ios::binary);
+
+    if (infile.fail()) {
+        std::cerr << "ERROR: file " << std::string(fname);
+        std::cerr << " does not exist" << std::endl;
+        exit(1);
+    }
+
+    size_t i = 0;
+    size_t size;
+    std::string line;
+
+    // The first line of the input file contains the total number of values.
+    if (std::getline(infile, line)) {
+         std::istringstream iss(line);
+         iss >> size;
+         if (size != SIZE) {
+            std::cerr << "ERROR: file " << std::string(fname);
+            std::cerr << " contains an unexpected number of elements (";
+            std::cerr << size << " rather than  "<< SIZE << ")" << std::endl;
+            exit(1);
+        }
+    };
+
+    // The second line of the input file contains all of the values.
+    if (std::getline(infile, line)) {
+        std::istringstream iss(line);
+        for (size_t i = 0; i < size; i++) {
+            double fdata;
+            iss >> fdata;
+            w[i] = T(fdata);
+        }
+    }
+}
+
+
+#endif
+
+
+// Prototype of top level function for C-synthesis
+void myproject(
+      input_t data[N_INPUTS],
+      result_t res[N_OUTPUTS],
+      unsigned short &const_size_in,
+      unsigned short &const_size_out);
+
+
+#endif
+
---
+++ keras-to-hls/KERAS_reuters_mlp/firmware/parameters.h
@@ -0,0 +1,63 @@
+#ifndef PARAMETERS_H_
+#define PARAMETERS_H_
+
+#include <complex>
+#include "ap_int.h"
+#include "ap_fixed.h"
+//#include "nnet_layer.h"
+#include "nnet_large_layer.h"
+#include "nnet_conv.h"
+#include "nnet_conv2d.h"
+#include "nnet_activation.h"
+#include "nnet_common.h"
+#include "nnet_batchnorm.h"
+#include "nnet_pooling.h"
+
+//hls-fpga-machine-learning insert numbers
+typedef ap_fixed<32,16> accum_default_t;
+typedef ap_fixed<32,16> weight_default_t;
+typedef ap_fixed<32,16> bias_default_t;
+typedef ap_fixed<32,16> input_t;
+typedef ap_fixed<32,16> result_t;
+#define N_INPUTS 1000
+#define N_LAYER_1 512
+#define N_OUTPUTS 46
+
+//hls-fpga-machine-learning insert layer-precision
+typedef ap_fixed<32,16> layer1_t;
+
+//hls-fpga-machine-learning insert layer-config
+struct config1 : nnet::layer_config {
+        static const unsigned n_in = N_INPUTS;
+        static const unsigned n_out = N_LAYER_1;
+        static const unsigned io_type = nnet::io_parallel;
+        static const unsigned reuse_factor = 2000;
+        static const unsigned n_zeros = 0;
+        static const bool store_weights_in_bram = false;
+        typedef accum_default_t accum_t;
+        typedef bias_default_t bias_t;
+        typedef weight_default_t weight_t;
+        };
+struct relu_config1 : nnet::activ_config {
+        static const unsigned n_in = N_LAYER_1;
+        static const unsigned table_size = 1024;
+        static const unsigned io_type = nnet::io_parallel;
+        };
+struct config2 : nnet::layer_config {
+        static const unsigned n_in = N_LAYER_1;
+        static const unsigned n_out = N_OUTPUTS;
+        static const unsigned io_type = nnet::io_parallel;
+        static const unsigned reuse_factor = 2000;
+        static const unsigned n_zeros = 0;
+        static const bool store_weights_in_bram = false;
+        typedef accum_default_t accum_t;
+        typedef bias_default_t bias_t;
+        typedef weight_default_t weight_t;
+        };
+struct softmax_config2 : nnet::activ_config {
+        static const unsigned n_in = N_OUTPUTS;
+        static const unsigned table_size = 1024;
+        static const unsigned io_type = nnet::io_parallel;
+        };
+
+#endif 
