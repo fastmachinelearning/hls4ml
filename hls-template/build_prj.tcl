0171cf2ea9bc41ad5639c15f54ce3f218a676c29
Giuseppe Di Guglielmo <giuseppe@cs.columbia.edu>
2019-07-02 11:41:38 -0400

    Remove -DVALIDATION (not necessary anymore)

--- hls-template/build_prj.tcl
+++ hls-template/build_prj.tcl
@@ -51,8 +51,8 @@ set RTL_COSIM_RESULTS "./tb_data/rtl_cosim_results.log"
 
 open_project -reset myproject_prj
 set_top myproject
-add_files firmware/myproject.cpp -cflags "-I[file normalize nnet_utils] -std=c++0x -DVALIDATION"
-add_files -tb myproject_test.cpp -cflags "-I[file normalize nnet_utils] -std=c++0x -DVALIDATION"
+add_files firmware/myproject.cpp -cflags "-I[file normalize nnet_utils] -std=c++0x"
+add_files -tb myproject_test.cpp -cflags "-I[file normalize nnet_utils] -std=c++0x"
 add_files -tb firmware/weights
 add_files -tb tb_data
 open_solution -reset "solution1"
