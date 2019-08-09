f49644934a90a6bfdec6d8d76b5480a744063539
Giuseppe Di Guglielmo <giuseppe@cs.columbia.edu>
2019-08-09 12:19:34 -0400

    Update envsetup for ex3

--- mentor_workspace/mntr_ex3/syn/envsetup.sh
+++ mentor_workspace/mntr_ex3/syn/envsetup.sh
@@ -7,22 +7,28 @@
 #
 
 # Base directory for CAD tools.
-export CAD_PATH=/opt/cad
+export CAD_PATH="/opt/cad"
 
 # We do need licensing for this example.
-export LM_LICENSE_FILE=${LM_LICENSE_FILE}:1720@bioeecad.ee.columbia.edu
+export LM_LICENSE_FILE=${LM_LICENSE_FILE}:"1720@bioeecad.ee.columbia.edu"
 
 # This path is host dependent.
-export CATAPULT_PATH=${CAD_PATH}/catapult
+export CATAPULT_PATH="${CAD_PATH}/catapult"
 
 # Let's use GCC provided with Catapult HLS.
-export PATH=${CATAPULT_PATH}/bin:${PATH}
+export PATH="${CATAPULT_PATH}/bin":${PATH}
 
 # We do need Mentor Modelsim (simulator) for this example.
-export PATH=${CAD_PATH}/msim/modeltech/bin/:$PATH
+export PATH="${CAD_PATH}/msim/modeltech/bin":$PATH
 
 # Let's use the SystemC headers and library provided with Catapult HLS.
-export SYSTEMC=${CATAPULT_PATH}/shared
+export SYSTEMC="${CATAPULT_PATH}/shared"
 
 # We need Vivado for logic synthesis.
 source ${CAD_PATH}/vivado/settings64.sh
+
+# Makefiles and scripts from Catapult HLS may rely on this variable.
+#export MGC_HOME=${CATAPULT_PATH}
+
+# Fix an issue on Ubuntu 18.04.
+#export LIBRARY_PATH="/usr/lib/x86_64-linux-gnu":$LIBRARY_PATH
