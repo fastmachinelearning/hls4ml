2d1ecf1596038506d3eccc7bad6a39226c7e5384
Giuseppe Di Guglielmo <giuseppe@cs.columbia.edu>
2019-08-14 09:44:52 -0400

    Add license ports:servers for Cadence and Synopsys

--- mentor_workspace/mntr_ex3/syn/envsetup.sh
+++ mentor_workspace/mntr_ex3/syn/envsetup.sh
@@ -9,9 +9,19 @@
 # Base directory for CAD tools.
 export CAD_PATH="/opt/cad"
 
+####
 # We do need licensing for this example.
+####
+
+# Mentor Graphics
 export LM_LICENSE_FILE=${LM_LICENSE_FILE}:"1720@bioeecad.ee.columbia.edu"
+# Synopsys
+export LM_LICENSE_FILE=${LM_LICENSE_FILE}:"27000@bioeecad.ee.columbia.edu"
+# Cadence
+export LM_LICENSE_FILE=${LM_LICENSE_FILE}:"5280@bioeecad.ee.columbia.edu"
+# Xilinx
 export XILINXD_LICENSE_FILE="2177@espdev.cs.columbia.edu"
+
 
 # This path is host dependent.
 export CATAPULT_PATH="${CAD_PATH}/catapult"
