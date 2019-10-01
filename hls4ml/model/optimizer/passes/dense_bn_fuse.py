1f3d0a4de82abdcf0d528fe39b4520fc67a459f8
hls4ml User <dejan.golubovic.27@gmail.com>
2019-10-01 13:35:45 +0000

    Fused Dense and Batch Normalization layers

---
+++ hls4ml/model/optimizer/passes/dense_bn_fuse.py
@@ -0,0 +1,26 @@
+from ..optimizer import OptimizerPass
+
+class FuseDenseAndBatchNormalization(OptimizerPass):
+    def match(self, node):
+        is_match = (node.__class__.__name__ == 'BatchNormalization' and 
+            node.get_input_node().__class__.__name__ == 'Dense')
+        return is_match
+
+    def transform(self, model, node):
+        # Fuse weight and bias of Dense layer with BN values
+        dense_node = node.get_input_node()
+
+        dense_weight = dense_node.weights['weight']
+        dense_bias = dense_node.weights['bias']
+
+        bn_scale = node.weights['scale']
+        bn_bias = node.weights['bias']
+
+        fused_weight = bn_scale.data * dense_weight.data
+        fused_bias = bn_scale.data * dense_bias.data + bn_bias.data
+
+        model.remove_node(node, rewire=True)
+        dense_node.weights['weight'].data = fused_weight
+        dense_node.weights['bias'].data = fused_bias
+
+        return True
