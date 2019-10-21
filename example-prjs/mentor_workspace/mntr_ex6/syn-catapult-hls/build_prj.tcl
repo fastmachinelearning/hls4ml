a9184dddbac920c791d23298abe2cfc6f4a454e9
Giuseppe Di Guglielmo <giuseppe@cs.columbia.edu>
2019-10-21 13:15:12 -0400

    Support various logic synthesis tools

--- example-prjs/mentor_workspace/mntr_ex6/syn-catapult-hls/build_prj.tcl
+++ example-prjs/mentor_workspace/mntr_ex6/syn-catapult-hls/build_prj.tcl
@@ -18,7 +18,7 @@ proc compare_files {file_1 file_2} {
     return $equal
 }
 
-if {$opt(asic)} {
+if {$opt(asic) > 0} {
     project new -name Catapult_asic
     set CSIM_RESULTS "./tb_data/catapult_asic_csim_results.log"
     set RTL_COSIM_RESULTS "./tb_data/catapult_asic_rtl_cosim_results.log"
@@ -37,58 +37,59 @@ solution options defaults
 
 solution options set Flows/ModelSim/VLOG_OPTS {-suppress 12110}
 solution options set Flows/ModelSim/VSIM_OPTS {-t ps -suppress 12110}
+solution options set Flows/DesignCompiler/OutNetlistFormat verilog
 solution options set /Input/CppStandard c++11
 #solution options set /Input/TargetPlatform x86_64
 flow package require /SCVerify
 
-directive set -DESIGN_GOAL area
-#directive set -OLD_SCHED false
-directive set -SPECULATE true
-directive set -MERGEABLE true
+#directive set -DESIGN_GOAL area
+##directive set -OLD_SCHED false
+#directive set -SPECULATE true
+#directive set -MERGEABLE true
 directive set -REGISTER_THRESHOLD 4096
-directive set -MEM_MAP_THRESHOLD 32
-directive set -LOGIC_OPT false
-directive set -FSM_ENCODING none
-directive set -FSM_BINARY_ENCODING_THRESHOLD 64
-directive set -REG_MAX_FANOUT 0
-directive set -NO_X_ASSIGNMENTS true
-directive set -SAFE_FSM false
-directive set -REGISTER_SHARING_MAX_WIDTH_DIFFERENCE 8
-directive set -REGISTER_SHARING_LIMIT 0
-directive set -ASSIGN_OVERHEAD 0
-directive set -TIMING_CHECKS true
-directive set -MUXPATH true
-directive set -REALLOC true
-directive set -UNROLL no
-directive set -IO_MODE super
-directive set -CHAN_IO_PROTOCOL standard
-directive set -ARRAY_SIZE 1024
-directive set -REGISTER_IDLE_SIGNAL false
-directive set -IDLE_SIGNAL {}
-directive set -STALL_FLAG false
-directive set -TRANSACTION_DONE_SIGNAL true
-directive set -DONE_FLAG {}
-directive set -READY_FLAG {}
-directive set -START_FLAG {}
-directive set -BLOCK_SYNC none
-directive set -TRANSACTION_SYNC ready
-directive set -DATA_SYNC none
-directive set -CLOCKS {clk {-CLOCK_PERIOD 0.0 -CLOCK_EDGE rising -CLOCK_UNCERTAINTY 0.0 -RESET_SYNC_NAME rst -RESET_ASYNC_NAME arst_n -RESET_KIND sync -RESET_SYNC_ACTIVE high -RESET_ASYNC_ACTIVE low -ENABLE_ACTIVE high}}
-directive set -RESET_CLEARS_ALL_REGS true
-directive set -CLOCK_OVERHEAD 20.000000
-directive set -OPT_CONST_MULTS use_library
-directive set -CHARACTERIZE_ROM false
-directive set -PROTOTYPE_ROM true
-directive set -ROM_THRESHOLD 64
-directive set -CLUSTER_ADDTREE_IN_COUNT_THRESHOLD 0
-directive set -CLUSTER_OPT_CONSTANT_INPUTS true
-directive set -CLUSTER_RTL_SYN false
-directive set -CLUSTER_FAST_MODE false
-directive set -CLUSTER_TYPE combinational
-directive set -COMPGRADE fast
+#directive set -MEM_MAP_THRESHOLD 32
+#directive set -LOGIC_OPT false
+#directive set -FSM_ENCODING none
+#directive set -FSM_BINARY_ENCODING_THRESHOLD 64
+#directive set -REG_MAX_FANOUT 0
+#directive set -NO_X_ASSIGNMENTS true
+#directive set -SAFE_FSM false
+#directive set -REGISTER_SHARING_MAX_WIDTH_DIFFERENCE 8
+#directive set -REGISTER_SHARING_LIMIT 0
+#directive set -ASSIGN_OVERHEAD 0
+#directive set -TIMING_CHECKS true
+#directive set -MUXPATH true
+#directive set -REALLOC true
+#directive set -UNROLL no
+#directive set -IO_MODE super
+#directive set -CHAN_IO_PROTOCOL standard
+#directive set -ARRAY_SIZE 1024
+#directive set -REGISTER_IDLE_SIGNAL false
+#directive set -IDLE_SIGNAL {}
+#directive set -STALL_FLAG false
+directive set -TRANSACTION_DONE_SIGNAL false
+#directive set -DONE_FLAG {}
+#directive set -READY_FLAG {}
+#directive set -START_FLAG {}
+#directive set -BLOCK_SYNC none
+#directive set -TRANSACTION_SYNC ready
+#directive set -DATA_SYNC none
+#directive set -CLOCKS {clk {-CLOCK_PERIOD 0.0 -CLOCK_EDGE rising -CLOCK_UNCERTAINTY 0.0 -RESET_SYNC_NAME rst -RESET_ASYNC_NAME arst_n -RESET_KIND sync -RESET_SYNC_ACTIVE high -RESET_ASYNC_ACTIVE low -ENABLE_ACTIVE high}}
+#directive set -RESET_CLEARS_ALL_REGS true
+#directive set -CLOCK_OVERHEAD 20.000000
+#directive set -OPT_CONST_MULTS use_library
+#directive set -CHARACTERIZE_ROM false
+#directive set -PROTOTYPE_ROM true
+#directive set -ROM_THRESHOLD 64
+#directive set -CLUSTER_ADDTREE_IN_COUNT_THRESHOLD 0
+#directive set -CLUSTER_OPT_CONSTANT_INPUTS true
+#directive set -CLUSTER_RTL_SYN false
+#directive set -CLUSTER_FAST_MODE false
+#directive set -CLUSTER_TYPE combinational
+#directive set -COMPGRADE fast
 
 # Design specific options.
-if {$opt(asic)} {
+if {$opt(asic) > 0} {
 solution options set Flows/QuestaSIM/SCCOM_OPTS {-g -x c++ -Wall -Wno-unused-label -Wno-unknown-pragmas -DRTL_SIM -D__ASIC__}
 solution options set /Input/CompilerFlags {-DMNTR_CATAPULT_HLS -D__ASIC__}
 } else {
@@ -139,9 +140,15 @@ if {$opt(csim)} {
 # Run HLS.
 if {$opt(hsynth)} {
 
-    if {$opt(asic)} {
+    if {$opt(asic) == 1} {
         solution library add nangate-45nm_beh -- -rtlsyntool DesignCompiler -vendor Nangate -technology 045nm
         solution library add ccs_sample_mem
+    } elseif {$opt(asic) == 2} {
+        solution library add nangate-45nm_beh -- -rtlsyntool RTLCompiler -vendor Nangate -technology 045nm
+        solution library add ccs_sample_mem
+    } elseif {$opt(asic) == 3} {
+#        solution library add nangate-45nm_beh -- -rtlsyntool RTLCompiler -vendor Nangate -technology 045nm
+#        solution library add ccs_sample_mem
     } else {
         solution library add mgc_Xilinx-KINTEX-u-2_beh -- -rtlsyntool Vivado -manufacturer Xilinx -family KINTEX-u -speed -2 -part xcku115-flva2104-2-i
         solution library add Xilinx_RAMS
@@ -249,11 +256,14 @@ if {$opt(hsynth)} {
 
     if {$opt(lsynth)} {
 
-        if {$opt(asic)} {
-            # TODO: DC is not installed yet. This will fail.
-            flow run /DesignCompiler/dc_shell ./rtl.v.dc v
+        if {$opt(asic) == 1} {
+            flow run /DesignCompiler/dc_shell ./concat_rtl.v.dc v
+        } elseif {$opt(asic) == 2} {
+            flow run /RTLCompiler/rc ./concat_rtl.v.dc v
+        } elseif {$opt(asic) == 3} {
+            flow run /RTLCompiler/rc ./concat_rtl.v.dc v
         } else {
-            flow run /Vivado/synthesize -shell vivado/rtl.v.xv
+            flow run /Vivado/synthesize -shell vivado/concat_rtl.v.xv
         }
 
     }
--- example-prjs/mentor_workspace/mntr_ex6/syn-catapult-hls/build_prj_asic.tcl
+++ example-prjs/mentor_workspace/mntr_ex6/syn-catapult-hls/build_prj_asic.tcl
@@ -1,4 +1,9 @@
 array set opt {
+    # The 'asic' flag specifies if you are targeting ASIC or FPGA:
+    # = 0 -> Vivado (FPGA)
+    # = 1 -> Mentor Design Compiler
+    # = 2 -> Cadence Encounter RTL Compiler
+    # = 3 -> Cadence Genus
     asic       1
     csim       1
     hsynth     1
