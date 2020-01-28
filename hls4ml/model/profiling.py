7e548b92205084ed67e68625080de77fc0213f5b
Sioni Summers <sioni.summers10@imperial.ac.uk>
2020-01-28 18:19:01 +0100

    Use tf.keras throughout. Add histogram plot method. Tidy up code: activations now plotted with same method as weights. Only plot non-zero values for easier reading.

--- hls4ml/model/profiling.py
+++ hls4ml/model/profiling.py
@@ -1,5 +1,5 @@
 import importlib
-libs = [('numpy', 'np'), ('pandas', 'pandas'), ('keras', 'keras'),
+libs = [('numpy', 'np'), ('pandas', 'pandas'), ('tensorflow', 'tensorflow'),
         ('seaborn', 'sb'), ('matplotlib.pyplot', 'plt')]
 for (name, short) in libs:
     try:
@@ -11,36 +11,86 @@ for (name, short) in libs:
         print(exception)
     else:
         globals()[short] = lib
+globals()['keras'] = tensorflow.keras
 
 def violinplot(data):
-    vp = sb.violinplot(x='x', y='weight', hue='layer', data=data)
+    f = plt.figure()
+    hue = 'layer' if 'layer' in data.keys() else None
+    vp = sb.violinplot(x='x', y='weight', hue=hue, data=data[data['x'] > 0])
     vp.set_yticklabels(vp.get_yticklabels(), rotation=45, ha='right')
-    vp.get_legend().remove()
+    if hue is not None:
+        vp.get_legend().remove()
     vp.set_xscale('log', basex=2)
-    plt.title('Distribution of weights')
-    plt.tight_layout()
-    return vp
+    return f
 
 def boxplot(data):
-    vp = sb.boxplot(x='x', y='weight', hue='layer', data=data, showfliers=False)
+    f = plt.figure()
+    hue = 'layer' if 'layer' in data.keys() else None
+    vp = sb.boxplot(x='x', y='weight', hue=hue, data=data[data['x'] > 0], showfliers=False)
     vp.set_yticklabels(vp.get_yticklabels(), rotation=45, ha='right')
-    vp.get_legend().remove()
+    if hue is not None:
+        vp.get_legend().remove()
     vp.set_xscale('log', basex=2)
-    plt.title('Distribution of weights')
-    plt.tight_layout()
-    return vp
+    return f
+
+def histogram(data):
+    from cycler import cycler
+    from matplotlib.ticker import MaxNLocator
+    # Power of 2 bins covering data range
+    high = np.ceil(np.log2(max(data['x']))) + 1
+    low = np.floor(np.log2(min(data[data['x'] > 0]['x']))) - 1
+    bits = np.arange(low, high, 1)
+    bins = 2 ** bits
+    f = plt.figure()
+    colors = sb.color_palette("husl", len(data['weight'].unique()))
+    for i, weight in enumerate(data['weight'].unique()):
+        x = data[data['weight'] == weight]['x']
+        h, b = np.histogram(x, bins=bins)
+        h = h * 1. / float(sum(h)) # normalize
+        plt.bar(bits[:-1], h, width=1, fill=False, label=weight, edgecolor=colors[i])
+    plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
+    plt.xlabel('log2(x)')
+    plt.ylabel('frequency')
+    plt.legend()
+    return f
 
 def FacetGrid(data):
-    vp = sb.FacetGrid(data, row='weight', hue='layer')
+    hue = 'layer' if 'layer' in data.keys() else None
+    vp = sb.FacetGrid(data[data['x'] > 0], row='weight', hue=hue)
     vp.map(sb.kdeplot, "x", clip_on=False, shade=True, alpha=1, lw=1.5, bw=.2)
     vp.map(sb.kdeplot, "x", clip_on=False, color="w", lw=2, bw=.2)
     vp.map(plt.axhline, y=0, lw=2, clip_on=False)
     vp.fig.subplots_adjust(hspace=-.25)
-    return vp
+    return vp.fig
+
+plots = {'violinplot' : violinplot,
+         'boxplot' : boxplot,
+         'FacetGrid' : FacetGrid,
+         'histogram' : histogram}
 
 def numerical(model, X=None, plot='boxplot'):
-    data = {'x' : [], 'layer' : [], 'weight' : []}
+    """
+    Perform numerical profiling of a model
+
+    Parameters
+    ----------
+    model : keras model
+        The keras model to profile
+    X : array-like, optional
+        Test data on which to evaluate the model to profile activations
+        Must be formatted suitably for the model.predict(X) method
+    plot : str, optional
+        The type of plot to produce.
+        Options are: 'boxplot' (default), 'violinplot', 'histogram', 'FacetGrid'
+
+    Returns
+    -------
+    tuple
+        The pair of produced figures. First weights and biases, then activations
+    """
+
     print("Profiling weights")
+    data = {'x' : [], 'layer' : [], 'weight' : []}
     for layer in model.layers:
         name = layer.name
         weights = layer.get_weights()
@@ -49,22 +99,18 @@ def numerical(model, X=None, plot='boxplot'):
             n = len(w)
             data['x'].extend(abs(w).tolist())
             data['layer'].extend([name for j in range(n)])
-            #data['weight'].extend([i for j in range(len(w))])
             data['weight'].extend(['{}/{}'.format(name, i) for j in range(n)])
 
     data = pandas.DataFrame(data)
-
-    plots = {'violinplot' : violinplot,
-             'boxplot' : boxplot,
-             'FacetGrid' : FacetGrid}
-
     wp = plots[plot](data) # weight plot
+    plt.title("Distribution of (non-zero) weights")
+    plt.tight_layout()
 
     ap = None
     if X is not None:
         print("Profiling activations")
         # test layer by layer on data
-        data = {'x' : [], 'layer' : []}
+        data = {'x' : [], 'weight' : []}
         partial_model = keras.models.Sequential()
         for layer in model.layers:
             print("   {}".format(layer.name))
@@ -73,14 +119,11 @@ def numerical(model, X=None, plot='boxplot'):
             if not isinstance(layer, keras.layers.InputLayer):
                 y = partial_model.predict(X).flatten()
                 data['x'].extend(abs(y).tolist())
-                data['layer'].extend([layer.name for i in range(len(y))])
+                data['weight'].extend([layer.name for i in range(len(y))])
 
-        plt.figure()
         data = pandas.DataFrame(data)
-        ap = sb.violinplot(x='x', y='layer', data=data)
-        ap.set_yticklabels(ap.get_yticklabels(), rotation=45, ha='right')
-        ap.set_xscale('log', basex=2)
-        plt.title('Distribution of activations')
-
+        ap = plots[plot](data) # activation plot
+        plt.title("Distribution of (non-zero) activations")
+        plt.tight_layout()
     return wp, ap
 
--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(name='hls4ml',
             'pandas',
             'seaborn',
             'matplotlib',
-            'keras',
             'tensorflow'
         ]
       },
