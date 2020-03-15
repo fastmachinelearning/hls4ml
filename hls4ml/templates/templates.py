1ce9211b9a1b349485c754d2dd3b03078f350fc8
Yutaro <yutaro.iiyama@cern.ch>
2020-03-16 00:43:58 +0100

    reorganized vivado templates

--- hls4ml/templates/templates.py
+++ hls4ml/templates/templates.py
@@ -4,6 +4,7 @@ class Backend(object):
         self.name = name
         self.config_templates = {}
         self.function_templates = {}
+        self.include_lists = {}
 
     def get_config_template(self, kind):
         return self.config_templates.get(kind)
@@ -11,9 +12,13 @@ class Backend(object):
     def get_function_template(self, kind):
         return self.function_templates.get(kind)
 
-    def register_templates(self, name, function_template, config_template):
+    def get_include_list(self, kind):
+        return self.include_lists.get(kind, [])
+
+    def register_templates(self, name, function_template, config_template, include_list=[]):
         self.function_templates[name] = function_template
         self.config_templates[name] = config_template
+        self.include_lists[name] = include_list
 
     def register_source(self, file_name, source, destination_dir='nnet_utils'):
         raise NotImplementedError
--- hls4ml/writer/vivado_writer.py
+++ hls4ml/writer/vivado_writer.py
@@ -58,6 +58,33 @@ class VivadoWriter(Writer):
         if not os.path.isdir("{}/firmware/weights".format(model.config.get_output_dir())):
             os.makedirs("{}/firmware/weights".format(model.config.get_output_dir()))
 
+    @staticmethod
+    def _make_array_pragma(variable):
+        config = variable.pragma
+        if type(config) is tuple:
+            mode = config[0]
+            if mode in ['partition', 'reshape']:
+                typ = config[1]
+                if typ != 'complete':
+                    factor = config[2]
+            elif mode == 'stream':
+                depth = config[1]
+        else:
+            mode = config
+            typ = 'complete'
+            factor = 0
+
+        if mode in ['partition', 'reshape']:
+            if typ == 'complete':
+                template = '#pragma HLS ARRAY_{mode} variable={name} {type} dim={dim}'
+            else:
+                template = '#pragma HLS ARRAY_{mode} variable={name} {type} factor={factor} dim={dim}'
+
+            return template.format(mode=mode.upper(), name=variable.name, type=typ, factor=factor, dim=0)
+
+        elif mode == 'stream':
+            return '#pragma HLS STREAM variable={name} depth={depth} dim={dim}'.format(name=variable.name, depth=depth, dim=0)
+
     def write_project_cpp(self, model):
         ###################
         ## myproject.cpp
@@ -88,12 +115,6 @@ class VivadoWriter(Writer):
                 newline += indent + insize_str + ',\n'
                 newline += indent + outsize_str + '\n'
 
-            elif '//hls-fpga-machine-learning insert weights' in line:
-                newline = line
-                for layer in model.get_layers():
-                    for w in layer.get_weights():
-                        newline += '#include "weights/{}.h"\n'.format(w.name)
-
             elif '//hls-fpga-machine-learning insert load weights' in line:
                 newline = line
                 for layer in model.get_layers():
@@ -108,16 +129,25 @@ class VivadoWriter(Writer):
                 newline = line
                 all_inputs = [i.cppname for i in model_inputs]
                 all_outputs = [o.cppname for o in model_outputs]
+
+                for i in model_inputs: newline += indent + self._make_array_pragma(i) + '\n'
+                for o in model_outputs: newline += indent + self._make_array_pragma(o) + '\n'
+
+                interface_mode = model.config.get_config_value('InterfaceMode')
+                if interface_mode is None:
+                    if model.config.get_config_value('IOType') == 'io_parallel':
+                        interface_mode = 'ap_vld'
+                    elif model.config.get_config_value('IOType') == 'io_serial':
+                        interface_mode = 'axis'
+                        
+                newline += indent + '#pragma HLS INTERFACE {} port={},{} \n'.format(interface_mode, ','.join(all_inputs), ','.join(all_outputs))
+
                 if model.config.get_config_value("IOType") == "io_parallel":
-                    for i in model_inputs: newline += indent + '#pragma HLS ARRAY_RESHAPE variable={} complete dim=0 \n'.format(i.cppname)
-                    for o in model_outputs: newline += indent + '#pragma HLS ARRAY_RESHAPE variable={} complete dim=0 \n'.format(o.cppname)
-                    newline += indent + '#pragma HLS INTERFACE ap_vld port={},{} \n'.format(','.join(all_inputs), ','.join(all_outputs))
                     if model.config.model_strategy == 'Resource':
                         newline += indent + '#pragma HLS DATAFLOW \n'
                     else:
                         newline += indent + '#pragma HLS PIPELINE \n'
-                if model.config.get_config_value("IOType") == "io_serial":
-                    newline += indent + '#pragma HLS INTERFACE axis port={},{} \n'.format(','.join(all_inputs), ','.join(all_outputs))
+                elif model.config.get_config_value('IOType') == 'io_serial':
                     newline += indent + '#pragma HLS DATAFLOW \n'
 
                 inval_str = '\n    '.join(['const_size_in_{} = {};'.format(i, inp.size_cpp()) for i, inp in enumerate(model_inputs, 1)])
@@ -138,7 +168,7 @@ class VivadoWriter(Writer):
                             if def_cpp is not None:
                                 newline += '    ' + def_cpp + ';\n'
                                 if var.pragma:
-                                    newline += '    ' + var.pragma + '\n'
+                                    newline += '    ' + self._make_array_pragma(var) + '\n'
                     func = layer.function_cpp()
                     if func:
                         for line in func:
@@ -192,10 +222,10 @@ class VivadoWriter(Writer):
         f.close()
         fout.close()
 
-    def write_parameters(self, model):
+    def write_defines(self, model):
         filedir = os.path.dirname(os.path.abspath(__file__))
-        f = open(os.path.join(filedir,'../templates/vivado/firmware/parameters.h'),'r')
-        fout = open('{}/firmware/parameters.h'.format(model.config.get_output_dir()),'w')
+        f = open(os.path.join(filedir,'../templates/vivado/firmware/defines.h'),'r')
+        fout = open('{}/firmware/defines.h'.format(model.config.get_output_dir()),'w')
 
         for line in f.readlines():
 
@@ -213,6 +243,30 @@ class VivadoWriter(Writer):
                     all_precision.update(layer_precision)
                 for used_type in all_precision.values():
                     newline += used_type.definition_cpp()
+
+            else:
+                newline = line
+            fout.write(newline)
+        f.close()
+        fout.close()
+
+    def write_parameters(self, model):
+        filedir = os.path.dirname(os.path.abspath(__file__))
+        f = open(os.path.join(filedir,'../templates/vivado/firmware/parameters.h'),'r')
+        fout = open('{}/firmware/parameters.h'.format(model.config.get_output_dir()),'w')
+
+        for line in f.readlines():
+
+            if '//hls-fpga-machine-learning insert includes' in line:
+                newline = line
+                for include in sorted(set(sum((layer.include_list for layer in model.get_layers()), []))):
+                    newline += '#include "%s"\n' % include
+
+            elif '//hls-fpga-machine-learning insert weights' in line:
+                newline = line
+                for layer in model.get_layers():
+                    for w in layer.get_weights():
+                        newline += '#include "weights/{}.h"\n'.format(w.name)
 
             elif "//hls-fpga-machine-learning insert layer-config" in line:
                 newline = line
@@ -293,24 +347,26 @@ class VivadoWriter(Writer):
                 newline = line.replace('myproject', model.config.get_project_name())
             elif '//hls-fpga-machine-learning insert data' in line:
                 newline = line
+                newline += '      std::vector<float>::const_iterator in_begin = in.begin();\n'
+                newline += '      std::vector<float>::const_iterator in_end;\n'
                 for inp in model.get_input_variables():
-                    input_str = '      ' + inp.definition_cpp() + ' = {};\n'
-                    default_val = ','.join('in[{}]'.format(i) for i in range(inp.size()))
-                    newline += input_str.format('{' + default_val + '}')
+                    newline += '      ' + inp.definition_cpp() + ';\n'
+                    newline += '      in_end = in_begin + ({});\n'.format(inp.size_cpp())
+                    newline += '      std::copy(in_begin, in_end, {});\n'.format(inp.cppname)
+                    newline += '      in_begin = in_end;\n'
                 for out in model.get_output_variables():
-                    output_str = '      ' + out.definition_cpp() + ' = {};\n'
-                    default_val = ','.join(str(o) for o in [0] * out.size())
-                    newline += output_str.format('{' + default_val + '}')
+                    # brace-init zeros the array out because we use std=c++0x
+                    newline += '      ' + out.definition_cpp() + '{};\n'
+                    # but we can still explicitly zero out if you want
+                    newline += '      std::fill_n({}, {}, 0.);\n'.format(out.cppname, out.size())
             elif '//hls-fpga-machine-learning insert zero' in line:
                 newline = line
                 for inp in model.get_input_variables():
-                    input_str = '    ' + inp.definition_cpp() + ' = {};\n'
-                    default_val = ','.join(str(i) for i in [0] * inp.size())
-                    newline += input_str.format('{' + default_val + '}')
+                    newline += '    ' + inp.definition_cpp() + ';\n'
+                    newline += '    std::fill_n({}, {}, 0.);\n'.format(inp.cppname, inp.size_cpp())
                 for out in model.get_output_variables():
-                    output_str = '    ' + out.definition_cpp() + ' = {};\n'
-                    default_val = ','.join(str(o) for o in [0] * out.size())
-                    newline += output_str.format('{' + default_val + '}')
+                    newline += '    ' + out.definition_cpp() + '{};\n'
+                    newline += '      std::fill_n({}, {}, 0.);\n'.format(out.cppname, out.size())
             elif '//hls-fpga-machine-learning insert top-level-function' in line:
                 newline = line
 
@@ -421,6 +477,7 @@ class VivadoWriter(Writer):
         self.write_project_cpp(model)
         self.write_project_header(model)
         self.write_weights(model)
+        self.write_defines(model)
         self.write_parameters(model)
         self.write_test_bench(model)
         self.write_build_script(model)
