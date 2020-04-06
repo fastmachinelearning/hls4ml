50a895c73cfd083964e33facbe6fdee11b1fa6c2
Vladimir Loncar <vloncar@users.noreply.github.com>
2020-04-06 10:13:18 +0200

    Remove traces of Brisk backend (for now)

--- hls4ml/writer/__init__.py
+++ hls4ml/writer/__init__.py
@@ -2,7 +2,5 @@ from __future__ import absolute_import
 
 from .writers import Writer, register_writer, get_writer
 from .vivado_writer import VivadoWriter
-from .brisk_writer import BriskWriter
 
 register_writer('Vivado', VivadoWriter)
-register_writer('Brisk', BriskWriter)
