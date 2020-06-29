c07ff359f0cf2b5a970b6261ac57b995bb5cc1ed
Duchstf <minhduc8199@gmail.com>
2020-06-29 17:28:47 -0500

    add support for fetching example models

---
+++ hls4ml/utils/example_models.py
@@ -0,0 +1,31 @@
+import os
+from .config import create_vivado_config
+
+def fetch_example_model(model_name):
+    """
+    Download an example model from github repo to working directory, and return the corresponding configuration:
+
+    https://github.com/hls-fpga-machine-learning/example-models
+
+    Args:
+        - model_name: string, name of the example model, one of the followings:
+            * 'keras_3layer'
+    
+    """
+
+    #Initiate the configuration file
+    config = create_vivado_config()
+        
+    #Download the example model
+    if model_name == 'keras_3layer':
+        os.system('wget https://raw.githubusercontent.com/hls-fpga-machine-learning/example-models/master/keras/keras_3layer.h5')
+
+        #Additional configuration parameters
+        config['KerasH5'] = 'keras_3layer.h5'
+        config['HLSConfig']['Model'] = {}
+        config['HLSConfig']['Model']['Precision'] = 'ap_fixed<16,6>'
+        config['HLSConfig']['Model']['ReuseFactor'] = '1'
+
+    #Add more models to the if statement if you want to
+    
+    return config
