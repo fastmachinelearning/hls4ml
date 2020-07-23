19bf92a288f7bf1a0151df5b1c7f3fb2ba9e3b12
Duc Hoang <minhduc8199@gmail.com>
2020-07-23 17:11:20 -0500

    Update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='hls4ml',
-      version='0.2.2',
+      version='0.3.0',
       description='Machine learning in FPGAs using HLS',
       long_description=long_description,
       long_description_content_type='text/markdown',
