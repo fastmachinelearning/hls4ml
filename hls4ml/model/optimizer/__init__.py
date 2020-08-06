b5c12ba0eb200c5c432eee4081281c38b85bda84
Vladimir Loncar <vloncar@users.noreply.github.com>
2020-08-06 18:19:43 +0200

    SkipOptimizers config parameter

--- hls4ml/model/optimizer/__init__.py
+++ hls4ml/model/optimizer/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-from hls4ml.model.optimizer.optimizer import OptimizerPass, register_pass, get_optimizer, optimize_model
+from hls4ml.model.optimizer.optimizer import OptimizerPass, register_pass, get_optimizer, optimize_model, get_available_passes
 
 
 from hls4ml.model.optimizer.passes.nop import EliminateLinearActivation
