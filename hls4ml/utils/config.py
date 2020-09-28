8ab6f0abe146875cff5776b86b60b3706643c830
Vladimir Loncar <vloncar@users.noreply.github.com>
2020-09-28 16:17:21 +0200

    Add global pooling layers to config util

--- hls4ml/utils/config.py
+++ hls4ml/utils/config.py
@@ -68,7 +68,7 @@ def config_from_keras_model(model, granularity='model', default_precision='ap_fi
     core_layers = ['InputLayer', 'Dropout', 'Flatten', 'Reshape']
     dense_layers = ['Dense', 'BinaryDense', 'TernaryDense']
     conv_layers = ['Conv1D', 'Conv2D', 'BinaryConv2D']
-    pooling_layers = ['MaxPooling1D', 'MaxPooling2D', 'AveragePooling1D', 'AveragePooling2D']
+    pooling_layers = ['MaxPooling1D', 'MaxPooling2D', 'GlobalMaxPooling1D', 'GlobalMaxPooling2D', 'AveragePooling1D', 'AveragePooling2D', 'GlobalAveragePooling1D', 'GlobalAveragePooling2D']
     norm_layers = ['BatchNormalization']
     activation_layers = ['Activation', 'LeakyReLU', 'ThresholdedReLU', 'ELU', 'PReLU', 'Softmax', 'ReLU']
     merge_layers = ['Add', 'Subtract', 'Multiply', 'Average', 'Maximum', 'Minimum', 'Concatenate']
