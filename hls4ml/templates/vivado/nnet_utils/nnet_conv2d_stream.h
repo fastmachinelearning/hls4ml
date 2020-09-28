eb59f82ceb7e1570301fc50760f1bce567e19bcc
Vladimir Loncar <vloncar@users.noreply.github.com>
2020-09-28 19:27:42 +0200

    Fuse  steps in Conv2D to reduce latency

--- hls4ml/templates/vivado/nnet_utils/nnet_conv2d_stream.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_conv2d_stream.h
@@ -45,90 +45,9 @@ void compute_scaled_indices(
     }
 }
 
-template<class data_T, typename CONFIG_T>
-void fill_buffer(
-    const data_T& in_elem,
-    ap_uint<CONFIG_T::filt_height * CONFIG_T::filt_width> *pixel_idx,
-    hls::stream<typename data_T::value_type> data_window[CONFIG_T::filt_height * CONFIG_T::filt_width * CONFIG_T::n_chan]
-) {
-
-    CopyDataPack: for (unsigned p = 0; p < data_T::size / CONFIG_T::n_chan; p++) {
-        #pragma HLS PIPELINE
-        CopyDataFilt: for (unsigned f = 0; f < CONFIG_T::filt_height * CONFIG_T::filt_width; f++) {
-            CopyDataChan: for (unsigned c = 0; c < CONFIG_T::n_chan; c++) {
-                if (pixel_idx[p][f]) data_window[f * CONFIG_T::n_chan + c].write_nb(in_elem[p * CONFIG_T::n_chan + c]);
-            }
-        }
-    }
-}
-
-template<class data_T, class res_T, typename CONFIG_T>
-void mult_buffer(
-    hls::stream<typename data_T::value_type> data_window[CONFIG_T::filt_height * CONFIG_T::filt_width * CONFIG_T::n_chan],
-    hls::stream<res_T> &res,
-    res_T &res_pack,
-    unsigned &outputs_ready,
-    typename CONFIG_T::weight_t weights[CONFIG_T::filt_height * CONFIG_T::filt_width * CONFIG_T::n_chan * CONFIG_T::n_filt],
-    typename CONFIG_T::bias_t biases[CONFIG_T::n_filt]
-) {
-    #pragma HLS INLINE
-
-    constexpr int n_in = CONFIG_T::filt_height * CONFIG_T::filt_width * CONFIG_T::n_chan;
-    constexpr int n_out = CONFIG_T::n_filt;
-
-    typename CONFIG_T::accum_t mult[n_in * n_out];
-    typename CONFIG_T::accum_t acc[n_out];
-    #pragma HLS ARRAY_PARTITION variable=mult complete
-    #pragma HLS ARRAY_PARTITION variable=acc complete
-
-    ResetResBuffer: for (unsigned f = 0; f < n_out; f++) {
-        #pragma HLS UNROLL
-        //out_pixel[f] = biases[f];
-        acc[f] = biases[f];
-    }
-
-    ProductInLoop: for(unsigned ii = 0; ii < n_in; ii++) {
-        typename data_T::value_type cache = data_window[ii].read();
-        ProductOutLoop: for(unsigned jj = 0; jj < n_out; jj++) {
-            int index = ii * n_out + jj;
-            //out_pixel[jj] += (res_T) cache * weights[index];
-            mult[index] = cache * weights[index];
-        }
-    }
-
-    AccumLoop: for(unsigned index = 0, jj = 0; index < n_in * n_out; index++, jj++) {
-        #pragma HLS UNROLL
-        if(jj == n_out) {
-            jj = 0;
-        }
-        acc[jj] += mult[index];
-    }
-
-    CastLoop: for(unsigned jj = 0; jj < n_out; jj++) {
-        #pragma HLS UNROLL
-        if (res_T::size / CONFIG_T::n_filt == 1) {
-            res_pack[jj] = (typename res_T::value_type) acc[jj];
-        } else {
-            res_pack[outputs_ready * n_out + jj] = (typename res_T::value_type) acc[jj];
-        }
-
-    }
-
-    if (res_T::size / CONFIG_T::n_filt == 1) {
-        res.write(res_pack);
-    } else {
-        if (outputs_ready == (res_T::size / CONFIG_T::n_filt) - 1) {
-            res.write(res_pack);
-            outputs_ready = 0;
-        } else {
-            outputs_ready++;
-        }
-    }
-
-}
-
 template<class data_T, class res_T, typename CONFIG_T>
 void compute_output(
+    const data_T& in_elem,
     hls::stream<typename data_T::value_type> data_window[CONFIG_T::filt_height * CONFIG_T::filt_width * CONFIG_T::n_chan],
     hls::stream<res_T> &res,
     res_T &res_pack,
@@ -137,11 +56,71 @@ void compute_output(
     typename CONFIG_T::bias_t biases[CONFIG_T::n_filt],
     ap_uint<CONFIG_T::filt_height * CONFIG_T::filt_width> *pixel_idx
 ) {
+    #pragma HLS INLINE
 
-    PixelLoop: for (unsigned p = 0; p < data_T::size / CONFIG_T::n_chan; p++) {
+    constexpr int n_in = CONFIG_T::filt_height * CONFIG_T::filt_width * CONFIG_T::n_chan;
+    constexpr int n_out = CONFIG_T::n_filt;
+
+    typename CONFIG_T::accum_t mult[n_in * n_out];
+    typename CONFIG_T::accum_t acc[n_out];
+
+    #pragma HLS ARRAY_PARTITION variable=mult complete dim=0
+    #pragma HLS ARRAY_PARTITION variable=acc complete dim=0
+
+    MultLoop: for (unsigned p = 0; p < data_T::size / CONFIG_T::n_chan; p++) {
         #pragma HLS PIPELINE
+        CopyDataFilt: for (unsigned f = 0; f < CONFIG_T::filt_height * CONFIG_T::filt_width; f++) {
+               #pragma HLS UNROLL
+            CopyDataChan: for (unsigned c = 0; c < CONFIG_T::n_chan; c++) {
+                #pragma HLS UNROLL
+                if (pixel_idx[p][f]) data_window[f * CONFIG_T::n_chan + c].write(in_elem[p * CONFIG_T::n_chan + c]);
+            }
+        }
         if (pixel_idx[p][CONFIG_T::filt_height * CONFIG_T::filt_width - 1]) {
-            mult_buffer<data_T, res_T, CONFIG_T>(data_window, res, res_pack, outputs_ready, weights, biases);
+
+            ResetResBuffer: for (unsigned f = 0; f < n_out; f++) {
+                #pragma HLS UNROLL
+                acc[f] = biases[f];
+            }
+
+            ProductInLoop: for(unsigned ii = 0; ii < n_in; ii++) {
+                #pragma HLS UNROLL
+                typename data_T::value_type cache = data_window[ii].read();
+                ProductOutLoop: for(unsigned jj = 0; jj < n_out; jj++) {
+                    #pragma HLS UNROLL
+                    int index = ii * n_out + jj;
+                    mult[index] = cache * weights[index];
+                }
+            }
+
+            AccumInLoop: for(unsigned ii = 0; ii < n_in; ii++) {
+                #pragma HLS UNROLL
+                AccumOutLoop: for(unsigned jj = 0; jj < n_out; jj++) {
+                    #pragma HLS UNROLL
+                    acc[jj] += mult[ii * n_out + jj];
+                }
+            }
+
+            CastLoop: for(unsigned jj = 0; jj < n_out; jj++) {
+                #pragma HLS UNROLL
+                if (res_T::size / CONFIG_T::n_filt == 1) {
+                    res_pack[jj] = (typename res_T::value_type) acc[jj];
+                } else {
+                    res_pack[outputs_ready * n_out + jj] = (typename res_T::value_type) acc[jj];
+                }
+            }
+        
+            if (res_T::size / CONFIG_T::n_filt == 1) {
+                res.write(res_pack);
+            } else {
+                if (outputs_ready == (res_T::size / CONFIG_T::n_filt) - 1) {
+                    res.write(res_pack);
+                    outputs_ready = 0;
+                } else {
+                    outputs_ready++;
+                }
+            }
+
         }
     }
 }
@@ -156,7 +135,6 @@ void conv_2d_cl(
     assert(CONFIG_T::pad_top == 0 && CONFIG_T::pad_bottom == 0 && CONFIG_T::pad_left == 0 && CONFIG_T::pad_right == 0);
     assert(CONFIG_T::filt_height == CONFIG_T::filt_width);
     assert(CONFIG_T::stride_height <= CONFIG_T::filt_height && CONFIG_T::stride_width <= CONFIG_T::filt_width);
-    //TODO add support for 'same' padding
 
     hls::stream<typename data_T::value_type> data_window[CONFIG_T::filt_height * CONFIG_T::filt_width * CONFIG_T::n_chan];
     const int win_depth = CONFIG_T::filt_height * CONFIG_T::out_width;
@@ -166,7 +144,7 @@ void conv_2d_cl(
 
     #pragma HLS ARRAY_PARTITION variable=weights complete
     #pragma HLS ARRAY_PARTITION variable=biases complete
-    //#pragma HLS ARRAY_PARTITION variable=CONFIG_T::pixels complete
+    #pragma HLS ARRAY_PARTITION variable=CONFIG_T::pixels complete
 
     res_T res_pack;
     #pragma HLS DATA_PACK variable=res_pack
@@ -178,10 +156,9 @@ void conv_2d_cl(
     ReadInputHeight: for (unsigned i_ih = 0; i_ih < CONFIG_T::in_height; i_ih++) {
         ReadInputWidth: for (unsigned i_iw = 0; i_iw < CONFIG_T::in_width / (data_T::size / CONFIG_T::n_chan); i_iw++) {
             #pragma HLS LOOP_FLATTEN
-            //#pragma HLS PIPELINE // This works, but uses far more LUTs
+            #pragma HLS PIPELINE
             compute_scaled_indices<data_T, CONFIG_T>(i_ih, i_iw, pixel_idx);
-            fill_buffer<data_T, CONFIG_T>(data.read(), pixel_idx, data_window);
-            compute_output<data_T, res_T, CONFIG_T>(data_window, res, res_pack, outputs_ready, weights, biases, pixel_idx);
+            compute_output<data_T, res_T, CONFIG_T>(data.read(), data_window, res, res_pack, outputs_ready, weights, biases, pixel_idx);
         }
     }
 }
