9ca877fcfe10e7fa1cd3196120ac9aafd50cf973
Vladimir Loncar <vloncar@users.noreply.github.com>
2020-10-20 17:02:32 +0200

    "Legacy" softmax implementation

--- hls4ml/templates/vivado/nnet_utils/nnet_activation_stream.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_activation_stream.h
@@ -209,7 +209,74 @@ void softmax_stable(hls::stream<data_T> &data, hls::stream<res_T> &res){
         }
         res.write(out_pack);
     }
+}
 
+template<class data_T, class res_T, typename CONFIG_T>
+void softmax_legacy(hls::stream<data_T> &data, hls::stream<res_T> &res) {
+    // Initialize the lookup table
+#ifdef __HLS_SYN__
+    bool initialized = false;
+    typename CONFIG_T::table_t exp_table[CONFIG_T::table_size];
+    typename CONFIG_T::table_t invert_table[CONFIG_T::table_size];
+#else
+    static bool initialized = false;
+    static typename CONFIG_T::table_t exp_table[CONFIG_T::table_size];
+    static typename CONFIG_T::table_t invert_table[CONFIG_T::table_size];
+#endif
+    if (!initialized) {
+        init_exp_table_legacy<CONFIG_T, CONFIG_T::table_size>(exp_table);
+        init_invert_table_legacy<CONFIG_T, CONFIG_T::table_size>(invert_table);
+        initialized = true;
+    }
+
+    // Index into the lookup table based on data for exponentials
+    typename CONFIG_T::table_t exp_res[CONFIG_T::n_in];
+    typename CONFIG_T::table_t exp_diff_res;
+    typename data_T::value_type data_cache[CONFIG_T::n_in];
+
+    SoftmaxInitLoop: for(unsigned i = 0; i < CONFIG_T::n_in / data_T::size; i++) {
+        #pragma HLS PIPELINE
+        data_T in_pack = data.read();
+        SoftmaxInitPackLoop: for(unsigned j = 0; j < data_T::size; j++) {
+            #pragma HLS UNROLL
+            data_cache[i * data_T::size + j] = in_pack[j];
+            exp_res[i * data_T::size + j] = 0;
+        }
+    }
+
+    SoftmaxExpLoop: for (int i = 0; i < CONFIG_T::n_in; i++) {
+        #pragma HLS PIPELINE
+        SoftmaxExpInner: for (int j = 0; j < CONFIG_T::n_in; j++) {
+            #pragma HLS UNROLL
+            
+            if (i == j) {
+                exp_diff_res = 1;
+            } else {
+                int data_round = (data_cache[j] - data_cache[i]) * CONFIG_T::table_size / 16;
+                int index = data_round + 8 * CONFIG_T::table_size / 16;
+                if (index < 0) index = 0;
+                if (index > CONFIG_T::table_size - 1) index = CONFIG_T::table_size - 1;
+                exp_diff_res = exp_table[index];
+            }
+            
+            exp_res[i] += exp_diff_res;
+        }
+    }
+
+    SoftmaxInvLoop: for(unsigned i = 0; i < CONFIG_T::n_in / res_T::size; i++) {
+        #pragma HLS PIPELINE
+        res_T out_pack;
+        SoftmaxInvPackLoop: for(unsigned j = 0; j < res_T::size; j++) {
+            #pragma HLS UNROLL
+            
+            int exp_res_index = exp_res[i * res_T::size + j] * CONFIG_T::table_size / 64;
+            if (exp_res_index < 0) exp_res_index = 0;
+            if (exp_res_index > CONFIG_T::table_size - 1) exp_res_index = CONFIG_T::table_size - 1;
+            
+            out_pack[i * res_T::size + j] = (typename res_T::value_type) invert_table[exp_res_index];
+        }
+        res.write(out_pack);
+    }
 }
 
 template<class data_T, class res_T, typename CONFIG_T>
@@ -222,7 +289,7 @@ void softmax(hls::stream<data_T> &data, hls::stream<res_T> &res){
         softmax_stable<data_T, res_T, CONFIG_T>(data, res);
         break;
     case softmax_implementation::legacy:
-        assert("Legacy softmax implementation is not supported with io_stream" && false);
+        softmax_legacy<data_T, res_T, CONFIG_T>(data, res);
         break;
     }    
 }
