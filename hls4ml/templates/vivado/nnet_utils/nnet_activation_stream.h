4ce99b40cfaeeedbe2416b5bde177363d2502743
Vladimir Loncar <vloncar@users.noreply.github.com>
2020-10-20 13:38:43 +0200

    Stable softmax implementation for io_stream

--- hls4ml/templates/vivado/nnet_utils/nnet_activation_stream.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_activation_stream.h
@@ -85,7 +85,7 @@ void relu(hls::stream<data_T> &data, hls::stream<res_T> &res) {
 }
 
 template <class data_T, class res_T, typename CONFIG_T>
-void softmax(hls::stream<data_T> &data, hls::stream<res_T> &res){
+void softmax_latency(hls::stream<data_T> &data, hls::stream<res_T> &res){
     // Initialize the lookup tables
 #ifdef __HLS_SYN__
     bool initialized = false;
@@ -139,6 +139,92 @@ void softmax(hls::stream<data_T> &data, hls::stream<res_T> &res){
         }
         res.write(out_pack);
     }
+}
+
+template <class data_T, class res_T, typename CONFIG_T>
+void softmax_stable(hls::stream<data_T> &data, hls::stream<res_T> &res){
+    // Initialize the lookup tables
+#ifdef __HLS_SYN__
+    bool initialized = false;
+    typename CONFIG_T::exp_table_t exp_table[CONFIG_T::table_size];
+    typename CONFIG_T::inv_table_t invert_table[CONFIG_T::table_size];
+#else
+    static bool initialized = false;
+    static typename CONFIG_T::exp_table_t exp_table[CONFIG_T::table_size];
+    static typename CONFIG_T::inv_table_t invert_table[CONFIG_T::table_size];
+
+#endif
+    if (!initialized) {
+        // Note we are exponentiating the inputs, which have type data_T
+        init_exp_table<typename data_T::value_type, CONFIG_T>(exp_table);
+        // Note we are inverting the exponentials, which have type exp_table_t
+        init_invert_table<typename CONFIG_T::exp_table_t, CONFIG_T>(invert_table);
+        initialized = true;
+    }
+
+    typename data_T::value_type data_array[CONFIG_T::n_in];
+    #pragma HLS ARRAY_PARTITION variable=data_array complete
+    SoftmaxArrayLoop: for(unsigned i = 0; i < CONFIG_T::n_in / data_T::size; i++){
+        #pragma HLS PIPELINE
+        data_T in_pack = data.read();
+        SoftmaxArrayPackLoop: for(unsigned j = 0; j < data_T::size; j++){
+            #pragma HLS UNROLL
+            data_array[i * data_T::size + j] = in_pack[j];
+        }
+    }
+
+    // Find the max and compute all delta(x_i, x_max)
+    Op_max<typename data_T::value_type> op_max;
+    typename data_T::value_type x_max = reduce<typename data_T::value_type, CONFIG_T::n_in, Op_max<typename data_T::value_type>>(data_array, op_max);
+
+    // For the diffs, use the same type as the input but force rounding and saturation
+    ap_fixed<data_T::value_type::width, data_T::value_type::iwidth,AP_RND,AP_SAT> d_xi_xmax[CONFIG_T::n_in];
+    for(unsigned i = 0; i < CONFIG_T::n_in; i++){
+        #pragma HLS UNROLL
+        d_xi_xmax[i] = data_array[i] - x_max;
+    }
+
+    // Calculate all the e^x's
+    typename CONFIG_T::exp_table_t exp_res[CONFIG_T::n_in];
+    #pragma HLS ARRAY_PARTITION variable=exp_res complete
+    typename CONFIG_T::exp_table_t exp_sum(0);
+    for(unsigned i = 0; i < CONFIG_T::n_in; i++){
+        #pragma HLS UNROLL
+        unsigned x = softmax_idx_from_real_val<typename data_T::value_type, CONFIG_T>(d_xi_xmax[i]);
+        exp_res[i] = exp_table[x];
+    }
+
+    // Explicitly sum the results with an adder tree.
+    // Rounding & Saturation mode, which improve accuracy, prevent Vivado from expression balancing
+    Op_add<typename CONFIG_T::exp_table_t> op_add;
+    exp_sum = reduce<typename CONFIG_T::exp_table_t, CONFIG_T::n_in, Op_add<typename CONFIG_T::exp_table_t>>(exp_res, op_add);
+
+    typename CONFIG_T::inv_table_t inv_exp_sum = invert_table[softmax_idx_from_real_val<typename CONFIG_T::exp_table_t,CONFIG_T>(exp_sum)];
+    SoftmaxInvLoop: for(unsigned i = 0; i < CONFIG_T::n_in / res_T::size; i++){
+        #pragma HLS PIPELINE
+        res_T out_pack;
+        SoftmaxInvPackLoop: for(unsigned j = 0; j < res_T::size; j++){
+            #pragma HLS UNROLL
+            out_pack[i * res_T::size + j] = exp_res[i * res_T::size + j] * inv_exp_sum;
+        }
+        res.write(out_pack);
+    }
+
+}
+
+template<class data_T, class res_T, typename CONFIG_T>
+void softmax(hls::stream<data_T> &data, hls::stream<res_T> &res){
+    switch(CONFIG_T::implementation){
+    case softmax_implementation::latency:
+        softmax_latency<data_T, res_T, CONFIG_T>(data, res);
+        break;
+    case softmax_implementation::stable:
+        softmax_stable<data_T, res_T, CONFIG_T>(data, res);
+        break;
+    case softmax_implementation::legacy:
+        assert("Legacy softmax implementation is not supported with io_stream" && false);
+        break;
+    }    
 }
 
 }
