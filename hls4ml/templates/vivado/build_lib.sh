b80e98a943dd66b8f52a893f83f42754ec4069e2
Vladimir Loncar <vloncar@users.noreply.github.com>
2020-10-25 20:05:29 +0100

    Give the .so file a unique name (fixes library reloading)

--- hls4ml/templates/vivado/build_lib.sh
+++ hls4ml/templates/vivado/build_lib.sh
@@ -9,8 +9,9 @@ fi
 LDFLAGS=
 INCFLAGS="-Ifirmware/ap_types/"
 PROJECT=myproject
+LIB_STAMP=mystamp
 
 ${CC} ${CFLAGS} ${INCFLAGS} -c firmware/${PROJECT}.cpp -o ${PROJECT}.o
 ${CC} ${CFLAGS} ${INCFLAGS} -c ${PROJECT}_bridge.cpp -o ${PROJECT}_bridge.o
-${CC} ${CFLAGS} ${INCFLAGS} -shared ${PROJECT}.o ${PROJECT}_bridge.o -o firmware/${PROJECT}.so
+${CC} ${CFLAGS} ${INCFLAGS} -shared ${PROJECT}.o ${PROJECT}_bridge.o -o firmware/${PROJECT}-${LIB_STAMP}.so
 rm -f *.o
