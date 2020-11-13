85a3d054c171a39d40606600b72cf444a7bdf006
Vladimir Loncar <vloncar@users.noreply.github.com>
2020-11-13 17:52:40 +0100

    Prevent pipelining in Dense with "resource" strategy

--- hls4ml/templates/vivado/nnet_utils/nnet_dense_stream.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_dense_stream.h
@@ -1,5 +1,5 @@
-#ifndef NNET_STREAM_LAYER_H_
-#define NNET_STREAM_LAYER_H_
+#ifndef NNET_DENSE_STREAM_H_
+#define NNET_DENSE_STREAM_H_
 
 #include "nnet_common.h"
 #include "nnet_types.h"
@@ -16,10 +16,9 @@ void dense_wrapper(
     typename CONFIG_T::weight_t weights[CONFIG_T::n_in*CONFIG_T::n_out],
     typename CONFIG_T::bias_t   biases[CONFIG_T::n_out]
 ) {
-    #pragma HLS PIPELINE II=CONFIG_T::reuse_factor
-
     #pragma HLS INLINE region
     if (CONFIG_T::strategy == nnet::latency) {
+        #pragma HLS PIPELINE II=CONFIG_T::reuse_factor
         dense_latency<data_T, res_T, CONFIG_T>(data, res, weights, biases);
     } else {
         dense_resource<data_T, res_T, CONFIG_T>(data, res, weights, biases);
@@ -40,7 +39,9 @@ void dense(
     #pragma HLS ARRAY_PARTITION variable=res complete
 
     DataPrepare: for(int i_in = 0; i_in < CONFIG_T::n_in / data_T::size; i_in++) {
-        #pragma HLS PIPELINE
+        if (CONFIG_T::n_in / data_T::size > 1) {
+            #pragma HLS PIPELINE
+        }
         data_T data_pack = data_stream.read();
         DataPack: for (int i_pack = 0; i_pack < data_T::size; i_pack++) {
             #pragma HLS UNROLL
@@ -51,7 +52,9 @@ void dense(
     dense_wrapper<typename data_T::value_type, typename res_T::value_type, CONFIG_T>(data, res, weights, biases);
 
     ResWrite: for(unsigned i_out = 0; i_out < CONFIG_T::n_out / res_T::size; i_out++) {
-        #pragma HLS PIPELINE
+        if (CONFIG_T::n_out / res_T::size > 1) {
+            #pragma HLS PIPELINE
+        }
         res_T res_pack;
         ResPack: for (int i_pack = 0; i_pack < res_T::size; i_pack++) {
             #pragma HLS UNROLL
