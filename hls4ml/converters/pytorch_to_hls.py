cc3887d06e6493151ce2adf4624ca3592fadf56d
Duc Hoang <minhduc8199@gmail.com>
2020-12-10 21:29:54 -0600

    Local converter changes

--- hls4ml/converters/pytorch_to_hls.py
+++ hls4ml/converters/pytorch_to_hls.py
@@ -9,6 +9,7 @@ import re
 
 from hls4ml.model import HLSModel
 
+####---------------Initial Data Reader------------------######
 class PyTorchDataReader:
     def __init__(self, config):
         self.config = config
@@ -17,14 +18,18 @@ class PyTorchDataReader:
             self.torch_model = torch.load(config['PytorchModel'], map_location=lambda storage, loc: storage)
         else:
             self.torch_model = torch.load(config['PytorchModel'])
-            
+        
+        #Get input tensor's shape
         self.input_shape = config.get('InputShape')
+        
         if self.input_shape == None:
             raise Exception('Must specify input shape ("InputShape") in config!')
+        
+        #Convert it to a list
         self.input_shape = self.input_shape.strip('(,)').split(',')
         self.input_shape = [int(n) for n in self.input_shape]
-        if len(self.input_shape) == 1:  #add dummy channel dimension
-            self.input_shape += [1]
+        #if len(self.input_shape) == 1:  #add dummy channel dimension
+            #self.input_shape += [1]
 
         self.state_dict = self.torch_model.state_dict()
     
@@ -36,180 +41,108 @@ class PyTorchDataReader:
             data = self.state_dict[layer_name + '.' + var_name].numpy().transpose()
 
         return data
-    
-def get_tuple_spec(layer_spec, spec, default = None):
-    r = re.search(spec+r'=\((.+?),*\)',layer_spec)
-    if r is None:
-        return default
-    nums = r.group(1).split(',')
-    return tuple([int(num) for num in nums])
 
-def get_int_spec(layer_spec, spec, default = None):
-    r = re.search(spec+r'=(\d+)',layer_spec)
-    if r is None:
-        return default
-    return int(r.group(1))
+####---------------Layer handling------------------######
+layer_handlers = {}
 
-def get_blank_spec(layer_spec, default = None):
-    r = re.search(r'(\d+, )+',layer_spec)
-    if r is None:
-        return default
-    nums = r.group(0).split(', ')[:-1]
-    return [int(num) for num in nums]
+def register_pytorch_layer_handler(layer_name, handler_func):
+    if layer_name in layer_handlers:
+        raise Exception('Layer {} already registered'.format(layer_name))
+    else:
+        layer_handlers[layer_name] = handler_func
 
-def pytorch_to_hls(yamlConfig):
+def get_supported_pytorch_layers():
+    return list(layer_handlers.keys())
+
+def pytorch_handler(*args):
+    def decorator(function):
+        function.handles = [arg for arg in args]
+        return function
+    return decorator
+
+####---------------Main processing function------------------######
+def pytorch_to_hls(config):
 
     ######################
     ##  Do translation
     ######################
-
-    print('Interpreting Model')
-    reader = PyTorchDataReader(yamlConfig)
-
-    core_layers = ['Linear','Conv1d','Conv2d']
-    skip_layers = ['Dropout', 'Flatten']
-    activation_layers = ['ReLU', 'Sigmoid', 'Tanh', 'SELU', 'LeakyReLU', 'Softmax', 'Softplus', 'Softsign']
-    pool_operations = ['MaxPool1d']
-    supported_layers = core_layers + skip_layers + activation_layers + pool_operations
-
+    
     #This is a list of dictionaries to hold all the layer info we need to generate HLS
     layer_list = []
+
+    print('Interpreting Model ...')
+    reader = PyTorchDataReader(config)
+    model = reader.torch_model
+
+    #Define layers to skip for conversion to HLS
+    skip_layers = ['Dropout', 'Flatten']
     
+    #All supported layers
+    supported_layers = get_supported_pytorch_layers() + skip_layers
+    
+    #Map inputs of skipped and split (activation) layers
+    inputs_map = {}
+
+    input_layers = None
+    output_layers = None
+    
+    layer_config = None
+    
+    #Input shape tracking
+    input_shapes = [list(reader.input_shape)] #In case there are multiple inputs
+    print("Input Shape: ", input_shapes)
+    
+    #Output shape tracking
+    output_shapes = {}
+    output_shape = None
+    
+    #Loop through layers
+    print('Topology:')
+    layer_counter = 0
+    for layer_name, pytorch_layer in model.named_children():
+        
+        pytorch_class = pytorch_layer.__class__.__name__
+        
+        if pytorch_class not in supported_layers:
+            raise Exception('Unsupported layer {}'.format(pytorch_class))
+                
+        #If not the first layer then input shape is taken from last layer's output
+        if layer_counter != 0:
+            input_shapes = [output_shape] #In case there are multiple inputs
+        
+        #Handle skipped layers
+        if pytorch_class in skip_layers:
+            if pytorch_class == 'Flatten':
+                output_shapes[layer_name] = [input_shapes[0][0], np.prod(input_shapes[0][1:])]
+            else:
+                output_shapes[layer_name] = input_shapes[0]
+            continue #!!
+                
+        if pytorch_class in supported_layers:
+            layer_counter = layer_counter + 1
+        
+        #Process the layer
+        layer, output_shape = layer_handlers[pytorch_class](pytorch_layer, layer_name, input_shapes, reader, config)
+
+        print('Layer name: {}, layer type: {}, current shape: {}'.format(layer['name'], layer['class_name'], output_shape))
+        layer_list.append(layer)
+        
+        assert(output_shape is not None)
+        output_shapes[layer['name']] = output_shape
+           
+       
+    #Add input layer
     input_layer = {}
     input_layer['name'] = 'input1'
     input_layer['class_name'] = 'InputLayer'
-    input_layer['input_shape'] = list(reader.input_shape)
-    input_layer['n_out'] = reader.input_shape[0]
-    layer_list.append(input_layer)
+    input_layer['input_shape'] = [layer_list[0]['n_in']]
+    layer_list.insert(0, input_layer)
 
-    current_shape = list(reader.input_shape)
-    print(current_shape)
-
-    #Loop through layers
-    print('Topology:')
-    modelstr = repr(reader.torch_model).split('\n')
-    print(modelstr,'\n')
-    for pytorch_layer in modelstr:
-        layer_match = re.match(r'\((\d+)\): (\w+)\((.*)\)', pytorch_layer.strip())
-        if layer_match is None:
-            continue
-        
-        layer_idx  = layer_match.group(1)
-        layer_type = layer_match.group(2)
-        layer_spec = layer_match.group(3)
-        in_size = current_shape[0]
-
-        # #Dictionary to fill in and append to layer_list
-        layer={}
-
-       
-        if layer_type not in supported_layers:
-            raise Exception('Unsupported layer {}'.format(layer_type))
-            
-        if layer_type == 'Flatten':
-            current_shape = [np.prod(current_shape), 1]
-            print('Flattened!, new shape:', current_shape)
-
-        if layer_type in skip_layers:
-            continue
-
-        if layer_type == 'Linear':
-            layer['class_name'] = 'Dense'
-            layer['name'] = layer_idx
-            
-            layer['n_in'] = get_int_spec(layer_spec,'in_features')
-            layer['n_out'] = get_int_spec(layer_spec,'out_features')
-
-            if None in layer.values():
-                raise Exception('Unable to interpret Linear layer ({})'.format(layer_spec))
-            assert in_size == layer['n_in'], 'Size mismatch between layer {} and {}. ({} != {})'.format(layer_list[-1]['name'],layer['name'],in_size,layer['n_in'])
-
-            current_shape[0] = layer['n_out']   #internal info for next layer
-
-        if layer_type == 'Conv1d':
-            layer['class_name'] = 'Conv1D'
-            layer['name'] = layer_idx
-            layer['data_format'] = 'channels_first'
-
-
-            layer['n_in'] = in_size  #TODO dilation
-            layer['n_chan'], layer['n_filt'] = get_blank_spec(layer_spec)
-            layer['filt_width'] = get_tuple_spec(layer_spec,'kernel_size')[0]
-            layer['stride'] = get_tuple_spec(layer_spec,'stride')[0]
-            layer['pad_left'] = layer['pad_right'] = get_tuple_spec(layer_spec,'padding',default=(0,))[0]
-            layer['dilation'] = get_tuple_spec(layer_spec, 'dilation', default=(1,))[0]
-            layer['n_out'] = int((layer['n_in'] + 2*layer['pad_left'] - layer['dilation']*(layer['filt_width']-1)- 1)/layer['stride'] + 1)
-
-            if None in layer.values():
-                raise Exception('Unable to interpret Conv1D layer ({})'.format(layer_spec))
-            assert layer['n_chan'] == current_shape[1], 'Channel size mismatch between layer {} and {}. ({} != {})'.format(layer_list[-1]['name'],current_shape[1],in_size,layer['n_chan'])
-
-            current_shape = [layer['n_filt'],layer['n_out']]
-
-        if layer_type == 'Conv2d':
-            layer['class_name'] = 'Conv2D'
-            layer['name'] = layer_idx
-            layer['data_format'] = 'channels_first'
-
-            layer['n_in'] = in_size  #TODO dilation
-            layer['n_chan'], layer['n_filt'] = get_blank_spec(layer_spec)
-
-            layer['in_height'],layer['in_width'] = current_shape[1],current_shape[2]    #TODO check order
-            layer['filt_height'],layer['filt_width'] = get_tuple_spec(layer_spec,'kernel_size')
-            layer['stride_height'],layer['stride_width'] = get_tuple_spec(layer_spec,'stride')
-            layer['dilation'] = get_tuple_spec(layer_spec, 'dilation', default=(1,1))[0]
-
-            layer['pad_top'] = layer['pad_bottom'] = get_tuple_spec(layer_spec,'padding',default=(0,0))[0]
-            layer['pad_left'] = layer['pad_right'] = get_tuple_spec(layer_spec,'padding',default=(0,0))[1]
-
-            layer['out_height']  = int((layer['in_height'] + 2*layer['pad_top'] - layer['dilation']*(layer['filt_height']-1)- 1)/layer['stride_height'] + 1)
-            layer['out_width']  = int((layer['in_width'] + 2*layer['pad_left'] - layer['dilation']*(layer['filt_width']-1)- 1)/layer['stride_width'] + 1)
-
-            assert layer['n_chan'] == current_shape[0], 'Channel size mismatch between layer {} and {}. ({} != {})'.format(layer_list[-1]['name'],current_shape[1],in_size,layer['n_chan'])
-
-            current_shape = [layer['n_filt'],layer['out_height'],layer['out_width']]
-
-
-        if layer_type in activation_layers:
-            layer['activation'] = layer_type.lower()
-            if layer['activation'] == 'Softmax':
-                layer['class_name'] = 'Softmax'
-            else:
-                layer['class_name'] = 'Activation'
-            layer['name'] = layer['activation'] + '_' + str(layer_idx)
-            layer['n_in'] = layer['n_out'] = layer_list[-1]['n_out']
-            
-            current_shape = current_shape   #remains unchaqnged, here for clarity
-        
-        if layer_type in pool_operations:
-            if layer_type == 'MaxPool1d':
-                layer['class_name'] = 'MaxPooling1D'
-                layer['name'] = layer_idx
-
-                layer['n_in'] = in_size
-                layer['n_filt'] = current_shape[1]
-                layer['pool_size'] = get_int_spec(layer_spec,'kernel_size') #for some reason, these are ints but Conv1d are tuples. Pytorch consistency?!
-                layer['stride'] = get_int_spec(layer_spec,'stride')
-                layer['pad_left'] = layer['pad_right'] = get_int_spec(layer_spec,'padding',default=0)
-                layer['dilation'] = get_int_spec(layer_spec,'dilation',default=1)
-                layer['n_out'] = int((layer['n_in'] + 2*layer['pad_left'] - layer['dilation']*(layer['pool_size']-1)- 1)/layer['stride'] + 1)
-
-                if layer['dilation'] != 1:
-                    print('Warning: dilation not yet supported by backend!')
-
-                current_shape = [layer['n_out'],layer['n_filt']]
-
-
-        #TODO reshape
-        print('Layer index: {}, layer type: {}, current shape: {}'.format(layer['name'], layer['class_name'], current_shape))
-
-        layer_list.append(layer)
 
     #################
     ## Generate HLS
     #################
-
-    reader = PyTorchDataReader(yamlConfig)
+    
     print('Creating HLS model')
-    hls_model = HLSModel(yamlConfig, reader, layer_list)
+    hls_model = HLSModel(config, reader, layer_list)
     return hls_model
