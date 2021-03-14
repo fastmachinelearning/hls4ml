89d187edba35e5eb547d0cc632f0dfaa85604666
Vladimir Loncar <vloncar@users.noreply.github.com>
2021-03-14 21:56:16 +0100

    Support code-generation for Conv1D

--- hls4ml/templates/vivado/nnet_utils/nnet_instr_gen.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_instr_gen.h
@@ -7,6 +7,18 @@
 namespace nnet {
 
 template<class data_T, typename CONFIG_T>
+class FillLineBuffer1D{
+    public:
+    static void fill_line(
+        data_T data[CONFIG_T::in_width * CONFIG_T::n_chan],
+        data_T line[CONFIG_T::filt_width * CONFIG_T::n_chan],
+        const unsigned pixel_idx
+    ) {
+        // To be implemented in subclasses
+    }
+};
+
+template<class data_T, typename CONFIG_T>
 class FillLineBuffer2D{
     public:
     static void fill_line(
