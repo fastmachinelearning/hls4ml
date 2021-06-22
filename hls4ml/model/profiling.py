b8429ea501a74b03b7415618e84049cc719719f1
Maksymilian Graczyk <maksymilian.graczyk18@imperial.ac.uk>
2021-06-22 10:07:34 +0200

    Make new_config['HLSConfig'] deeply copied in get_unoptimized_hlsmodel()

--- hls4ml/model/profiling.py
+++ hls4ml/model/profiling.py
@@ -7,6 +7,7 @@ import seaborn as sb
 import uuid
 import os
 import shutil
+import json
 from collections import defaultdict
 
 from hls4ml.model.hls_model import HLSModel
@@ -29,6 +30,8 @@ def get_unoptimized_hlsmodel(model):
     from hls4ml.converters import convert_from_config
 
     new_config = model.config.config.copy()
+    new_config['HLSConfig'] = json.loads(json.dumps(new_config['HLSConfig']))
+
     new_output_dir = uuid.uuid4().hex
 
     while os.path.exists(new_output_dir):
