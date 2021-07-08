5872458125b37fae6088e3b218d3bdee382d72bd
Duchstf <minhduc8199@gmail.com>
2021-07-08 12:09:03 -0500

    delegate the reshape functions to be optimized in qonnx-frontend for now.

--- hls4ml/converters/onnx/reshape.py
+++ hls4ml/converters/onnx/reshape.py
@@ -1,25 +1,4 @@
 from hls4ml.converters.onnx_to_hls import onnx_handler, get_onnx_input_name
-import numpy as np
-
-reshape_layers = ['Reshape', 'Squeeze', 'Unsqueeze'] #Fold Squeeze and Unsqueeze into reshape
-@onnx_handler(*reshape_layers)
-def parse_reshape_layer(reader, node, inputs_map, input_shapes, graph, config):
-    
-    layer = {}
-    layer['name'] = node.name
-    layer['class_name'] = 'Reshape'
-
-    layer['inputs'] = get_onnx_input_name(node, graph)
-
-    # if node.op_type == 'Reshape':
-    #     layer['target_shape'] = node.shape
-    # elif node.op_type == 'Squeeze':
-    #     layer['target_shape'] = 
-    # elif node.op_type == 'Unsqueeze':
-    #     output_shape = input_shapes[0][:1] + layer['target_shape']
-    
-    return layer, output_shape
-
 
 @onnx_handler('Transpose')
 def parse_transpose_layer(reader, node, inputs_map, input_shapes, graph, config):
