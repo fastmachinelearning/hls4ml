9f287ba4b52c31a2136ffb46c9302d899b58f73a
Javier M. Duarte <jduarte@ucsd.edu>
2021-09-24 21:22:39 -0700

    tranpose2d stream

--- hls4ml/templates/vivado/nnet_utils/nnet_merge_stream.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_merge_stream.h
@@ -299,6 +299,110 @@ void concatenate3d(
     }
 }
 
+template<class input1_T, class input2_T, class res_T, typename CONFIG_T>
+void concatenate2d_0(
+    hls::stream<input1_T> &data1,
+    hls::stream<input2_T> &data2,
+    hls::stream<res_T> &res)
+{
+    ConcatLoopHeight1: for (int i = 0; i < CONFIG_T::n_elem1_0; i++) {
+        #pragma HLS PIPELINE II=1
+
+        input1_T in_data1 = data1.read();
+        res_T out_data;
+        #pragma HLS DATA_PACK variable=out_data
+
+        ConcatPackInput1: for (int k = 0; k < input1_T::size; k++) {
+            #pragma HLS UNROLL
+            out_data[k] = in_data1[k];
+        }
+
+        res.write(out_data);
+    }
+    ConcatLoopHeight2: for (int i = 0; i < CONFIG_T::n_elem2_0; i++) {
+        #pragma HLS PIPELINE II=1
+
+        input2_T in_data2 = data2.read();
+        res_T out_data;
+        #pragma HLS DATA_PACK variable=out_data
+
+        ConcatPackInput2: for (int k = 0; k < input2_T::size; k++) {
+            #pragma HLS UNROLL
+            out_data[k] = in_data2[k];
+	}
+
+	res.write(out_data);
+    }
+}
+
+template<class input1_T, class input2_T, class res_T, typename CONFIG_T>
+void concatenate2d_1(
+    hls::stream<input1_T> &data1,
+    hls::stream<input2_T> &data2,
+    hls::stream<res_T> &res)
+{
+    ConcatLoopHeight: for (int i = 0; i < CONFIG_T::n_elem1_0; i++) {
+        #pragma HLS PIPELINE II=1
+
+        input1_T in_data1 = data1.read();
+        input2_T in_data2 = data2.read();
+        res_T out_data;
+        #pragma HLS DATA_PACK variable=out_data
+
+        ConcatPackInput1: for (int k = 0; k < input1_T::size; k++) {
+            #pragma HLS UNROLL
+            out_data[k] = in_data1[k];
+	}
+            
+	ConcatPackInput2: for (int k = 0; k < input2_T::size; k++) {
+            #pragma HLS UNROLL
+            out_data[input1_T::size + k] = in_data2[k];
+	}
+
+	res.write(out_data);
+    }
+}
+
+template<class input1_T, class input2_T, class res_T, typename CONFIG_T>
+void concatenate2d(
+    hls::stream<input1_T> &data1,
+    hls::stream<input2_T> &data2,
+    hls::stream<res_T> &res)
+{
+    if (CONFIG_T::axis == 2 || CONFIG_T::axis == -1) {
+        concatenate2d_1<input1_T, input2_T, res_T, CONFIG_T>(data1, data2, res);
+    } else {
+        concatenate2d_0<input1_T, input2_T, res_T, CONFIG_T>(data1, data2, res);
+    }
+}
+
+template<class input1_T, class input2_T, class res_T, typename CONFIG_T>
+void concatenate1d(
+    hls::stream<input1_T> &data1,
+    hls::stream<input2_T> &data2,
+    hls::stream<res_T> &res)
+{
+    res_T out_data;
+    #pragma HLS DATA_PACK variable=out_data
+    ConcatLoop1: for (int i = 0; i < CONFIG_T::n_elem1_0 / input1_T::size; i++) {
+        #pragma HLS PIPELINE
+        input1_T in_data1 = data1.read();
+        ConcatPack1: for (int j = 0; j < res_T::size; j++) {
+            #pragma HLS UNROLL
+	    out_data[j] = in_data1[j];
+        }
+        res.write(out_data);
+    }
+    ConcatLoop2: for (int i = 0; i < CONFIG_T::n_elem2_0 / input2_T::size; i++) {
+        #pragma HLS PIPELINE
+        input2_T in_data2 = data2.read();
+        ConcatPack2: for (int j = 0; j < res_T::size; j++) {
+            #pragma HLS UNROLL
+	    out_data[j] = in_data2[j];
+        }
+        res.write(out_data);
+    }
+}
 }
 
 #endif
