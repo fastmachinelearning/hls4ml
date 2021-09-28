ea8002946114fd6f02371cd2d25b4d5860b4e389
Jovan Mitrevski <jmitrevs@fnal.gov>
2021-09-28 01:50:24 -0500

    fix conv1d io_parallel resource

--- hls4ml/templates/vivado/nnet_utils/nnet_conv1d_resource.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_conv1d_resource.h
@@ -41,7 +41,7 @@ void conv_1d_full(
     data_T data_conv[CONFIG_T::filt_width * CONFIG_T::n_chan * CONFIG_T::out_width];
     data_T data_col[CONFIG_T::filt_width * CONFIG_T::n_chan];
     res_T res_col[CONFIG_T::n_filt];
-    
+
     //#pragma HLS ARRAY_PARTITION variable=data_conv complete
     #pragma HLS ARRAY_PARTITION variable=data_col complete
     #pragma HLS ARRAY_PARTITION variable=res_col complete
@@ -142,17 +142,17 @@ void conv_1d_resource_cf(
 template<class data_T, typename CONFIG_T>
 void im2col_1d_cl(data_T data[CONFIG_T::in_width * CONFIG_T::n_chan], data_T data_col[CONFIG_T::filt_width * CONFIG_T::n_chan], const int col) {
     int index = 0;
-    ChannelLoop:
-    for (int channel = CONFIG_T::n_chan; channel--; data++) {
-		#pragma HLS UNROLL
-        KernelLoop:
-        for (int kernel_col = 0; kernel_col < CONFIG_T::filt_width; kernel_col++) {
-            int input_col = -CONFIG_T::pad_left + kernel_col * CONFIG_T::dilation + col * CONFIG_T::stride_width;
-            if (input_col >= 0 && input_col < CONFIG_T::in_width) {
-                //*(data_col++) = data[input_col * CONFIG_T::n_chan];
-                data_col[index] = data[input_col * CONFIG_T::n_chan];
+    KernelLoop:
+    for (int kernel_col = 0; kernel_col < CONFIG_T::filt_width; kernel_col++) {
+        #pragma HLS UNROLL
+
+	ChannelLoop:
+	for (int channel = 0; channel < CONFIG_T::n_chan; channel++) {
+	    int index_data = (col*CONFIG_T::stride_width+kernel_col-CONFIG_T::pad_left) * CONFIG_T::n_chan + channel;
+
+            if (index_data >= 0 && index_data < CONFIG_T::in_width*CONFIG_T::n_chan) {
+		data_col[index] = data[index_data];
             } else {
-                //*(data_col++) = 0;
                 data_col[index] = 0;
             }
             index++;
@@ -191,7 +191,6 @@ void conv_1d_resource_cl(
         dense_resource<data_T, res_T, typename CONFIG_T::mult_config>(data_col, res_col, weights, biases);
         for (int j = 0; j < CONFIG_T::n_filt; j++) {
             res[i * CONFIG_T::n_filt + j] = res_col[j];
-            //res[j * CONFIG_T::out_width + i] = res_col[j]; // Transposed order
         }
     }
 }
