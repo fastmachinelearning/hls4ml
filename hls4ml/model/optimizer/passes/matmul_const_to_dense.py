d437d5f384e84d9acd1dc726c2865608dba10b9d
kf7lsu <mtrahms@gmail.com>
2021-11-07 16:22:56 -0800

    change from merging mul, sub, matmul, const to just merge matmul const

--- hls4ml/model/optimizer/passes/matmul_const_to_dense.py
+++ hls4ml/model/optimizer/passes/matmul_const_to_dense.py
@@ -4,54 +4,38 @@ from hls4ml.model.optimizer import OptimizerPass
 class MatmulConstToDense(OptimizerPass):
     """ Convert MatMul with constant to a dense layer """
     def match(self, node):
-        if node.__class__.__name__ == 'MatMul' and len(node.inputs) == 2:
-            if node.get_input_node(node.inputs[0]).__class__.__name__ == 'Constant':
-                merge_node = node.get_input_node(node.inputs[1]
-                if merge_node.__class__.__name__ == 'Merge' 
-                    and merge_node.get_input_node(node.inputs[0]).__class__.__name__ == 'Merge':
-                    return True
-                else:
-                    return False
-            elif node.get_input_node(node.inputs[1]).__class__.__name__ == 'Constant':
-                merge_node = node.get_input_node(node.inputs[0]
-                if merge_node.__class__.__name__ == 'Merge' 
-                    and merge_node.get_input_node(node.inputs[0]).__class__.__name__ == 'Merge':
-                    return True
-                else:
-                    return False
-        return False
+        is_match = (node.__class__.__name__ == 'MatMul' and len(node.inputs) == 2 and 
+                    (node.get_input_node(node.inputs[0]).__class__.__name__ == 'Constant' or 
+                        node.get_input_node(node.inputs[1]).__class__.__name__ == 'Constant'))
+        return is_match
 
     def transform(self, model, node):
-        """ Substitute Matmul + const + sub + mul for a single dense """
-        #determining layer ordering
+        """ Substitute Matmul + Constant for a single dense """
+        #determining Constant layer input
         matmul_node = node
-        sub_node = None
-        mul_node = None
         const_node = None
+        const_inp_idx = 0
         if matmul_node.get_input_node(matmul_node.inputs[0]).__class__.__name__ == 'Constant':
             const_node = matmul_node.get_input_node(matmul_node.inputs[0])
-            sub_node = matmul_node.get_input_node(matmul_node.inputs[1])
         else:
             const_node = matmul_node.get_input_node(matmul_node.inputs[1])
-            sub_node = matmul_node.get_input_node(matmul_node.inputs[0])
-        mul_node = sub_node.get_input_node(sub_node.inputs[0])
+            const_inp_idx = 1
         
         #creating the attributes
-        input_shape = sub_node.get_input_variable().shape
+        #TODO: what other attributes need to be set here?
+        #get input shape by taking the output shape of the non constant input node        
+        input_shape = matmul_node.get_input_node(matmul_node.inputs[1-const_inp_idx]).get_output_variable().shape 
         output_shape = matmul_node.get_output_variable().shape
         attributes = {
             'n_in': input_shape,
             'n_out': output_shape,
             }
 
-        #making new node, takes inputs from mul node, replaces matmul node, no need to rewire
+        #making new node
         new_dense = model.make_node("Dense", matmul_node.name, attributes, mul_node.inputs.copy())
-        new_dense.weights['weight'] = const_node.value
-        #TODO: how to integrate factor and subtrahend into this?
+        new_dense.weights['weight'] = const_node.weights['value']
         
         #removing and replacing old nodes
         model.replace_node(matmul_node, new_dense)
-        model.remove_node(sub_node, rewire=False)
-        model.remove_node(const_node, rewire=False)
 
         return True
