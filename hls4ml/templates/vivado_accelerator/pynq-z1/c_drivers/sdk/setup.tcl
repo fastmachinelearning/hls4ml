c1f467066b6c8ec31f009d0f9ea1878084dcf5da
Giuseppe Di Guglielmo <giuseppe@cs.columbia.edu>
2021-11-28 17:53:54 -0800

    Move data.h generator into hls4ml

--- hls4ml/templates/vivado_accelerator/pynq-z1/c_drivers/sdk/setup.tcl
+++ hls4ml/templates/vivado_accelerator/pynq-z1/c_drivers/sdk/setup.tcl
@@ -6,6 +6,7 @@ if { $::argc == 1 } {
     set myproject [lindex $::argv 0]
     createhw -name ${myproject}\_platform -hwspec ../hdf/${myproject}\_wrapper.hdf
     createapp -name ${myproject}\_standalone -app {Hello World} -proc ps7_cortexa9_0 -hwproject ${myproject}\_platform -os standalone
+    configapp -app ${myproject}\_standalone build-config release
     #createapp -name ${myproject}\_standalone -app {Hello World} -proc psu_cortexa53_0 -hwproject ${myproject}\_platform -os standalone -arch 64
     #configapp -app ${myproject}\_standalone -add define-compiler-symbols {FLAG=VALUE}
     #projects -build
--- hls4ml/writer/vivado_accelerator_writer.py
+++ hls4ml/writer/vivado_accelerator_writer.py
@@ -327,6 +327,62 @@ class VivadoAcceleratorWriter(VivadoWriter):
             f.write('set bit_width_hls_input {}\n'.format(out_bit))
         f.close()
 
+    # TODO: Extract parameters from the model
+    def write_header_file(X, y, y_keras, y_hls, n_samples, filename='data.h'):
+        header_file = open(filename, 'w')
+        (n_X_samples, n_X_inputs) = X.shape
+        (n_y_samples, n_y_outputs) = y.shape
+        (n_y_keras_samples, n_y_keras_outputs) = y_keras.shape
+        (n_y_hls_samples, n_y_hls_outputs) = y_hls.shape
+   
+        header_file.write('#ifndef __DATA_H__\n')
+        header_file.write('#define __DATA_H__\n')
+        header_file.write('/* ouf of {} */\n'.format(n_X_samples))
+        header_file.write('#define N_SAMPLES {}\n'.format(n_samples))
+        header_file.write('\n')
+        header_file.write('#define N_X_INPUTS {}\n'.format(n_X_inputs))
+        header_file.write('const float data_X_inputs[N_SAMPLES*N_X_INPUTS] = {\n')
+        for s in range(n_samples):
+            header_file.write('    ')
+            for i in range(n_X_inputs):
+                header_file.write('{}, '.format(X[s][i]))
+            header_file.write('\n')
+        header_file.write('};\n')
+        header_file.write('\n')
+        header_file.write('/* Ground truth - for validation */\n')
+        header_file.write('#define N_Y_OUTPUTS {}\n'.format(n_y_outputs))
+        header_file.write('const float data_y_outputs[N_SAMPLES*N_Y_OUTPUTS] = {\n')
+        for s in range(n_samples):
+            header_file.write('    ')
+            for o in range(n_y_outputs):
+                header_file.write('{}, '.format(y[s][o]))
+            header_file.write('\n')
+        header_file.write('};\n')
+        header_file.write('\n')
+        header_file.write('/* Keras outputs - for validation */\n')
+        header_file.write('#define N_Y_KERAS_OUTPUTS {}\n'.format(n_y_keras_outputs))
+        header_file.write('')
+        header_file.write('const float data_y_keras_outputs[N_SAMPLES*N_Y_KERAS_OUTPUTS] = {\n')
+        for s in range(n_samples):
+            header_file.write('    ')
+            for o in range(n_y_keras_outputs):
+                header_file.write('{}, '.format(y_keras[s][o]))
+            header_file.write('\n')
+        header_file.write('};\n')
+        header_file.write('\n')
+        header_file.write('/* csim outputs - for verification */\n')
+        header_file.write('#define N_Y_HLS_OUTPUTS {}\n'.format(n_y_hls_outputs))
+        header_file.write('')
+        header_file.write('const float data_y_hls_outputs[N_SAMPLES*N_Y_HLS_OUTPUTS] = {\n')
+        for s in range(n_samples):
+            header_file.write('    ')
+            for o in range(n_y_hls_outputs):
+                header_file.write('{}, '.format(y_hls[s][o]))
+            header_file.write('\n')
+        header_file.write('};\n')
+        header_file.write('#endif\n')
+        header_file.close()
+ 
     def write_driver(self, model):
         filedir = os.path.dirname(os.path.abspath(__file__))
         if os.path.isdir(os.path.join(filedir, self.vivado_accelerator_config.get_driver_path())):
