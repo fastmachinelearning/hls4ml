4c7321a6ca3454171d47c475dfd9fcf6bb205ae8
Jules Muhizi <jmuhizi@college.harvard.edu>
2022-01-25 12:17:37 -0500

    added weight transponse and qkeras quantization support

--- hls4ml/converters/__init__.py
+++ hls4ml/converters/__init__.py
@@ -85,9 +85,12 @@ def parse_yaml_config(config_file):
     """
     def construct_keras_model(loader, node):
         from tensorflow.keras.models import load_model
+        from qkeras.utils import _add_supported_quantized_objects
+        co = {}
+        _add_supported_quantized_objects(co)
 
         model_str = loader.construct_scalar(node)
-        return load_model(model_str)
+        return load_model(model_str, custom_objects=co)
 
     yaml.add_constructor(u'!keras_model', construct_keras_model, Loader=yaml.SafeLoader)
 
