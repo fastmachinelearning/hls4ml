a3ef1735344bf96b3fa3dcbf69a793fadc592ddb
ChiRui Chen <kugelblitz.ee05@gmail.com>
2022-04-03 00:13:56 +0800

    using keras upsampling2d method

--- hls4ml/templates/vivado/nnet_utils/nnet_image_stream.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_image_stream.h
@@ -5,7 +5,7 @@
 #include "hls_stream.h"
 
 namespace nnet {
-
+/*
 template<class data_T, typename CONFIG_T>
 void resize_nearest(
     hls::stream<data_T> &image,
@@ -35,6 +35,62 @@ void resize_nearest(
 
             resized.write(out_data);
         }
+    }
+}
+*/
+template<class data_T, typename CONFIG_T>
+void resize_nearest(
+    hls::stream<data_T> &image,
+    hls::stream<data_T> &resized
+) {
+    assert(CONFIG_T::new_height % CONFIG_T::height == 0);
+    assert(CONFIG_T::new_width % CONFIG_T::width == 0);
+    constexpr unsigned ratio_height = CONFIG_T::new_height / CONFIG_T::height;
+    constexpr unsigned ratio_width = CONFIG_T::new_width / CONFIG_T::width;
+    constexpr unsigned ii = ratio_height * ratio_width;
+    
+    std::cout<<"test"<<std::endl;
+    
+    for (unsigned h = 0; h < CONFIG_T::height; h++) {
+    #pragma HLS PIPELINE
+    
+          data_T data_in_row[CONFIG_T::width];
+          
+          
+              for (unsigned i = 0; i < CONFIG_T::width; i++) {
+                  
+                  
+                  data_T  in_data = image.read();
+                  for (unsigned j = 0; j < CONFIG_T::n_chan; j++) {
+                      #pragma HLS UNROLL
+                      
+                      data_in_row[i][j] = in_data[j];
+                  }
+              }
+              
+              for (unsigned i = 0; i <ratio_height; i++) {
+                    for (unsigned l = 0; l < CONFIG_T::width; l++) {
+                          for (unsigned j = 0; j < ratio_width; j++) {
+                          #pragma HLS UNROLL
+                        
+                            data_T out_data;
+                            #pragma HLS DATA_PACK variable=out_data
+                            for (unsigned k = 0; k < CONFIG_T::n_chan; k++) {
+                            #pragma HLS UNROLL
+                            out_data[k] = data_in_row[l][k];
+                                          
+                             }
+                             resized.write(out_data);   
+                          }
+                    
+                    }
+              }
+                 
+                       
+                            
+                            
+                        
+                
     }
 }
 
