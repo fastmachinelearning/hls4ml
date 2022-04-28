af4f66ddc9d9e4208556a007f0071bc1dd018a63
Jovan Mitrevski <jmitrevs@fnal.gov>
2022-04-28 12:11:22 -0500

    don't override infered types in test

--- test/pytest/test_qonnx.py
+++ test/pytest/test_qonnx.py
@@ -40,14 +40,6 @@ def test_tfc_2w2a():
     # TODO should be auto-derived by QuantizeDenseOutput pass after some adaptation
     config['LayerName'] = {}
     config['LayerName']['global_in'] = {'Precision' : 'ap_fixed<16,2>'}
-    config['LayerName']['Dense_MatMul_0'] = {'Precision' : {'accum' : 'ap_int<10>',
-                                                      'result'  : 'ap_int<10>'}}
-    config['LayerName']['Dense_MatMul_1'] = {'Precision' : {'accum' : 'ap_int<10>',
-                                                      'result'  : 'ap_int<10>'}}
-    config['LayerName']['Dense_MatMul_2'] = {'Precision' : {'accum' : 'ap_int<10>',
-                                                      'result'  : 'ap_int<10>'}}
-    config['LayerName']['Dense_MatMul_3'] = {'Precision' : {'accum' : 'ap_int<10>',
-                                                      'result'  : 'ap_int<10>'}}
     hls_model = hls4ml.converters.convert_from_onnx_model(model,
                                                           output_dir='hls4mlprj_qonnx_tfc-2w2a',
                                                           part='xcu250-figd2104-2L-e',
