snapshot, parse CNV, but incorrect result

hls4ml/converters/onnx/convolution.py
@@ -25,6 +25,7 @@ def parse_conv_layer(reader, node, inputs_map, input_shapes, graph, config):
     if get_onnx_attribute(node, 'group') != 1:
         raise ValueError("Only 1 group supported corrently")
 
+    layer['in_width'] = input_shapes[0][-2]
     layer['n_chan'] = input_shapes[0][-1]
     layer['n_filt'] = input_shapes[1][0]
 
@@ -36,9 +37,9 @@ def parse_conv_layer(reader, node, inputs_map, input_shapes, graph, config):
     #set some values needed later
     if layer['n_dim'] == 1:
         # this is 1D convolution
-        full_width = input_shapes[0][-2] + pads[0] + pads[1]
+        full_width = layer['in_width'] + pads[0] + pads[1]
         eff_kernel_width = kernel_shape[0] * dilations[0]
-        layer['n_out'] = int(np.ceil((full_width - eff_kernel_width + 1) / strides[0]))
+        layer['out_width'] = int(np.ceil((full_width - eff_kernel_width + 1) / strides[0]))
         # for compatibility interpret some variables
         layer['pad_left'] = pads[0]
         layer['pad_right'] = pads[1]
@@ -47,7 +48,8 @@ def parse_conv_layer(reader, node, inputs_map, input_shapes, graph, config):
         layer['dilation_width'] = dilations[0]
     else:
         # 2d
-        full_height = input_shapes[0][-3] + pads[0] + pads[2]
+        layer['in_height'] = input_shapes[0][-3]
+        full_height = layer['in_height'] + pads[0] + pads[2]
         eff_kernel_height = kernel_shape[0] * dilations[0]
         out_height = int(np.ceil((full_height - eff_kernel_height + 1) / strides[0]))
         layer['out_height'] = out_height

hls4ml/model/optimizer/passes/conv_to_convxd.py
@@ -1,7 +1,8 @@
 import numpy as np
 from hls4ml.model.optimizer import OptimizerPass
-from hls4ml.model.types import FixedPrecisionType
-from hls4ml.model.layers import Conv, Constant
+from hls4ml.model.types import IntegerPrecisionType
+from hls4ml.model.layers import Conv, Constant, Conv1D, Conv2D
+from hls4ml.model.optimizer.passes.quant_opt import propagete_type_conv
 
 class ConvToConvXD(OptimizerPass):
     """ Convert Conv with constant to a Conv1D or Conv2D layer """
@@ -33,42 +34,36 @@ class ConvToConvXD(OptimizerPass):
         quant_precision = None
 
         if weight_precision and input_precision and (bias_precision or not bias_node):
-            if (weight_precision.width != weight_precision.integer
-                or input_precision.width != input_precision.integer):
-                raise ValueError("quant_precisions must always have the same width and integer parameters")
-
-            num_feature_maps = weight_node.value.shape[0]
-            Nacc = attributes['filt_width'] * attributes.get('filt_height', 1) * num_feature_maps
-            bitwidth = weight_precision.width + input_precision.width + int(np.ceil(np.log2(Nacc)))
-            signed = weight_precision.signed or input_precision.signed
-            # copy staruation and rounding from "other"
-            rounding_mode = input_precision.rounding_mode
-            saturation_mode = input_precision.saturation_mode
-
-            # correct if bias
-            if bias_node:
-                bitwidth = max(bitwidth + (bias_precision.signed and not signed),
-                               bias_precision.width + (signed and not bias_precision.signed)) + 1
-                signed = signed or bias_precision.signed
-            quant_precision = FixedPrecisionType(bitwidth, bitwidth, signed, rounding_mode, saturation_mode)
+            quant_precision = propagete_type_conv(input_precision, weight_precision, bias_precision,
+                num_feature_maps=weight_node.value.shape[0], filt_width=attributes['filt_width'],
+                filt_height=attributes.get('filt_height', 1))
 
         #creating the attributes
 
         # The ConvxD nodes expect the weight data to be in a different format, not (M, k1.., C)
         if attributes['n_dim'] == 1:
-            nodetype = "Conv1D"
-            attributes["weight_data"] =  np.transpose(weight_node.value, (1, 2, 0))
+            nodetype = Conv1D
+            weight_data =  np.transpose(weight_node.value, (1, 2, 0))
         else:
-            nodetype = "Conv2D"
-            attributes["weight_data"] =  np.transpose(weight_node.value, (1, 2, 3, 0))
+            nodetype = Conv2D
+            weight_data =  np.transpose(weight_node.value, (1, 2, 3, 0))
         attributes["weight_precision"] = weight_precision
         attributes["weight_quantizer"] =  weight_node.get_attr("quantizer")
         attributes["quant_precision"] = quant_precision
 
+        node.add_weights_variable(name='weight', var_name='w{index}', data=weight_data,
+                                  precision=weight_precision, quantizer=attributes['weight_quantizer'])
+ 
         if bias_node:
-            attributes["bias_data"] =  bias_node.value
             attributes["bias_precision"] = bias_precision,
             attributes["bias_quantizer"] =  bias_node.get_attr("quantizer")
+            node.add_weights_variable(name='bias', var_name='b{index}', data=bias_node.value,
+                                      precision=bias_precision, quantizer=attributes['bias_quantizer'])
+        else:
+            node.add_weights_variable(name='bias', var_name='b{index}', data=np.zeros(node.get_output_variable().shape),
+                                      precision=IntegerPrecisionType(1, False))
+
+
 
         #making new node
         new_node = model.make_node(nodetype, f"{nodetype}_{node.name}", attributes,