14c6ef2d7e9644f72deb912db484b8d22e63600d
Jovan Mitrevski <jmitrevs@fnal.gov>
2022-05-05 14:42:27 -0500

    update batchnorm and banchnorm/Dense/Conv fusion

--- hls4ml/model/optimizer/passes/bn_fuse.py
+++ hls4ml/model/optimizer/passes/bn_fuse.py
@@ -1,31 +1,69 @@
+import numpy as np
+
 from hls4ml.model.optimizer import OptimizerPass
 from hls4ml.model.layers import BatchNormalization, Dense, Conv1D, Conv2D
 
 class FuseBatchNormalization(OptimizerPass):
     def match(self, node):
-        is_match = isinstance(node, BatchNormalization) and \
-            isinstance(node.get_input_node(), (Dense, Conv1D, Conv2D)) and \
-            node.get_input_node().get_attr('weight_quantizer') is None and \
-            node.get_input_node().get_attr('bias_quantizer') is None
-        return is_match
+        prev_node = node.get_input_node(node.inputs[0])
+        basic_match = (isinstance(node, BatchNormalization)
+            and isinstance(prev_node, (Dense, Conv1D, Conv2D))
+            and not prev_node.get_attr("quant_precision"))
+
+        if basic_match:
+            s0 = prev_node.weights['weight'].data_unquantized
+            b0 = prev_node.weights['bias'].data_unquantized
+            s1 = node.weights['scale'].data_unquantized
+            b1 = node.weights['bias'].data_unquantized
+            scale_compatible = (
+                (prev_node.get_attr("weight_quantizer") is None
+                 and node.get_attr("scale_quantizer") is None)
+                or (s0 == np.ones_like(s0)).all()
+                or (s1 == np.ones_like(s1)).all())
+            bias_compatible = (
+                (prev_node.get_attr("bias_quantizer") is None
+                 and node.get_attr("bias_quantizer") is None)
+                or (b0 == np.zeros_like(b0)).all()
+                or (b1 == np.zeros_like(b1)).all())
+            return scale_compatible and bias_compatible
+        else:
+            return False
+
 
     def transform(self, model, node):
-        # Fuse weight and bias of Dense/Conv1D/Conv2D layer with BN values
-        parent_node = node.get_input_node()
+        """ Fuse weight and bias of Dense/Conv1D/Conv2D layer with BN values
+        """
 
-        parent_weight = parent_node.weights['weight']
-        parent_bias = parent_node.weights['bias']
+        # copying much of the logic from FuseConsecutiveBatchNormalization
+        # (hence weight = scale in the variable names)
 
-        bn_scale = node.weights['scale']
-        bn_bias = node.weights['bias']
+        prev_node = node.get_input_node(node.inputs[0])
 
-        fused_weight = bn_scale.data * parent_weight.data
-        fused_bias = bn_scale.data * parent_bias.data + bn_bias.data
+        s0 = prev_node.weights['weight'].data_unquantized
+        b0 = prev_node.weights['bias'].data_unquantized
+        s1 = node.weights['scale'].data_unquantized
+        b1 = node.weights['bias'].data_unquantized
+
+        s_quantizer = (node.get_attr("scale_quantizer") if (s0 == np.ones_like(s0)).all()
+                       else prev_node.get_attr("weight_quantizer"))
+        b_quantizer = (node.get_attr("bias_quantizer") if (b0 == np.zeros_like(b0)).all()
+                       else prev_node.get_attr("bias_quantizer"))
+
+        prev_node.set_attr("weight_quantizer", s_quantizer)
+        prev_node.set_attr("bias_quantizer", b_quantizer)
+        if s_quantizer:
+            prev_node.set_attr("weight_precision", s_quantizer.hls_type)
+        if b_quantizer:
+            prev_node.set_attr("bias_precision", b_quantizer.hls_type)
+
+        scale_new = s0 * s1
+        bias_new = s1 * b0 + b1
+
+        prev_node.set_attr("quant_precision", node.get_attr("quant_precision"))
 
         model.remove_node(node, rewire=True)
-        parent_weight.data = fused_weight
-        parent_bias.data = fused_bias
-        if not parent_node.get_attr('use_bias', True):
-            parent_bias.update_precision(bn_bias.type.precision)
+
+        prev_node.add_weights_variable(name='weight', var_name='w{index}', data=scale_new, quantizer=s_quantizer)
+        prev_node.add_weights_variable(name='bias', var_name='b{index}', data=bias_new, quantizer=b_quantizer)
 
         return True
