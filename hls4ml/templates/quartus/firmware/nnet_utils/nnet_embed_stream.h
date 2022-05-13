c0c25b0a5d4c67d7071549165983544c10e0564e
Benjamin Ramhorst <bramhorst27@gmail.com>
2022-05-13 10:24:40 +0100

    Quartus Embedding Layer

---
+++ hls4ml/templates/quartus/firmware/nnet_utils/nnet_embed_stream.h
@@ -0,0 +1,10 @@
+/*
+*   PLACEHOLDER - The common pass embedding.py includes both parallel and streaming implementations; streaming is currently not supported in Quartus
+*/
+
+#ifndef NNET_EMBED_STREAM_H_
+#define NNET_EMBED_STREAM_H_
+
+namespace nnet {}
+
+#endif
