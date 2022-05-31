5c90ebd27d9a0fa94676a5aab9535c20140d4477
Vladimir Loncar <vloncar@users.noreply.github.com>
2022-05-31 18:32:55 +0200

    Pointwise conv2d for resource strategy

--- hls4ml/templates/vivado/nnet_utils/nnet_conv2d.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_conv2d.h
@@ -86,6 +86,22 @@ void conv_2d_cl(
     }
 }
 
+template<class data_T, class res_T, typename CONFIG_T>
+void pointwise_conv_2d_cl(
+    data_T data[CONFIG_T::in_height * CONFIG_T::in_width * CONFIG_T::n_chan],
+    res_T  res[CONFIG_T::out_height * CONFIG_T::out_width * CONFIG_T::n_filt],
+    typename CONFIG_T::weight_t weights[CONFIG_T::n_chan * CONFIG_T::n_filt],
+    typename CONFIG_T::bias_t   biases[CONFIG_T::n_filt])
+{
+    assert(CONFIG_T::filt_width == 1);
+
+    if (CONFIG_T::strategy == nnet::latency) {
+        pointwise_conv_2d_latency_cl<data_T, res_T, CONFIG_T>(data, res, weights, biases);
+    } else {
+        pointwise_conv_2d_resource_cl<data_T, res_T, CONFIG_T>(data, res, weights, biases);
+    }
+}
+
 }//end namespace
 
 #endif
