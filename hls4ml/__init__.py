e889a6017ca6d39d38d2be31e856a0844efba680
Javier Duarte <jduarte@ucsd.edu>
2022-06-21 20:53:30 -0700

    update

--- hls4ml/__init__.py
+++ hls4ml/__init__.py
@@ -4,6 +4,13 @@ from hls4ml import converters
 from hls4ml import report
 from hls4ml import utils
 
+try:
+    from ._version import version as __version__
+    from ._version import version_tuple
+except ImportError:
+    __version__ = "unknown version"
+    version_tuple = (0, 0, "unknown version")
+
 def reseed(newseed):
     print('\npytest-randomly: reseed with {}'.format(newseed))
     try:
