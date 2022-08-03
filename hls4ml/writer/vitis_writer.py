aaa0d991c37252c8d26295510f86a8bc1fff77fb
Vladimir Loncar <vloncar@users.noreply.github.com>
2022-08-03 20:05:36 +0200

    Add Vitis HLS implementation overrides

--- hls4ml/writer/vitis_writer.py
+++ hls4ml/writer/vitis_writer.py
@@ -1,6 +1,6 @@
 import os
-from shutil import copyfile, copytree
-from distutils.dir_util import copy_tree
+import glob
+from shutil import copy
 from hls4ml.writer.vivado_writer import VivadoWriter
 
 class VitisWriter(VivadoWriter):
@@ -8,8 +8,24 @@ class VitisWriter(VivadoWriter):
     def __init__(self):
         super().__init__()
 
+    def write_nnet_utils_overrides(self, model):
+        ###################
+        ## nnet_utils
+        ###################
+
+        filedir = os.path.dirname(os.path.abspath(__file__))
+
+        srcpath = os.path.join(filedir,'../templates/vitis/nnet_utils/')
+        dstpath = '{}/firmware/nnet_utils/'.format(model.config.get_output_dir())
+
+        headers = [os.path.basename(h) for h in glob.glob(srcpath + '*.h')]
+
+        for h in headers:
+            copy(srcpath + h, dstpath + h)
+
     def write_hls(self, model):
         """
         Write the HLS project. Calls the steps from VivadoWriter, adapted for Vitis
         """
         super(VitisWriter, self).write_hls(model)
+        self.write_nnet_utils_overrides(model)
