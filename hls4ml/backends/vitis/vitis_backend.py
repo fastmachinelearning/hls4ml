ef5530df0678d7121c2500233ce2d754e1f3e8f5
Vladimir Loncar <vloncar@users.noreply.github.com>
2022-08-03 20:05:57 +0200

    Supported feature validation for Vitis backend

--- hls4ml/backends/vitis/vitis_backend.py
+++ hls4ml/backends/vitis/vitis_backend.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 from hls4ml.backends import VivadoBackend
-from hls4ml.model.flow import register_flow
+from hls4ml.model.flow import register_flow, get_flow
 from hls4ml.report import parse_vivado_report
 
 
@@ -12,10 +12,19 @@ class VitisBackend(VivadoBackend):
         self._register_flows()
 
     def _register_flows(self):
-        vivado_ip = 'vivado:ip'
+        validation_passes = [
+            'vitis:validate_conv_implementation',
+            'vitis:validate_strategy',
+        ]
+        validation_flow = register_flow('validation', validation_passes, requires=['vivado:init_layers'], backend=self.name)
+
         writer_passes = ['make_stamp', 'vitis:write_hls']
-        self._writer_flow = register_flow('write', writer_passes, requires=[vivado_ip], backend=self.name)
-        self._default_flow = vivado_ip
+        self._writer_flow = register_flow('write', writer_passes, requires=['vitis:ip'], backend=self.name)
+
+        ip_flow_requirements = get_flow('vivado:ip').requires.copy()
+        ip_flow_requirements.insert(ip_flow_requirements.index('vivado:init_layers'), validation_flow)
+
+        self._default_flow = register_flow('ip', None, requires=ip_flow_requirements, backend=self.name)
 
     def build(self, model, reset=False, csim=True, synth=True, cosim=False, validation=False, export=False, vsynth=False):
         if 'linux' in sys.platform:
