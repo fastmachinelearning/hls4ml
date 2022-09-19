9271648dd48ecee17d01b225a6bd494bfacc3e0a
Benjamin Ramhorst <bramhorst27@gmail.com>
2022-09-19 13:12:28 +0100

    Quartus Softsign LUT optimization in stream

--- test/pytest/test_softsign.py
+++ test/pytest/test_softsign.py
@@ -7,48 +7,27 @@ from pathlib import Path
 
 test_root_path = Path(__file__).parent
 
-def flat_distribution(shape):
-    return np.random.rand(*shape)
-
-
-@pytest.fixture()
-def generate_data(function, input_shape):
-    return function((1000, *input_shape))
-
-
-# TODO: include latency strategy with flat_distribution when it can be made to pass
-@pytest.mark.parametrize('backend,strategy,function,input_shape,io_type', [   
-                            ('Vivado', 'stable', flat_distribution, (4,), 'io_parallel'),
-                            ('Quartus', 'stable', flat_distribution, (4,), 'io_parallel'),
-
-                            # IO_stram avaliable just for VIVADO
-                            ('Vivado', 'stable', flat_distribution, (4,), 'io_stream'),
-                            ('Vivado', 'stable', flat_distribution, (4, 4, 3), 'io_stream')
+@pytest.mark.parametrize('backend', ['Vivado', 'Quartus'])
+@pytest.mark.parametrize('input_shape, io_type', [
+                            ((8, ), 'io_parallel'),
+                            ((8, ), 'io_stream'),
+                            ((8, 8, 3), 'io_stream')
                         ])
-def test_softsign(backend, strategy, generate_data, input_shape, io_type):
-    X = generate_data
+def test_softsign(backend, input_shape, io_type):
+    X = np.random.rand(1000, *input_shape)
     model = tf.keras.models.Sequential()
     model.add(tf.keras.layers.Activation(input_shape=input_shape, activation='softsign', name='softsign'))
     model.compile()
     
-    f_type = 'ac_fixed<18,8,true,AC_RND,AC_SAT>' if backend == 'Quartus' else 'ap_fixed<18,8,AP_RND,AP_SAT>'
-    cfg = hls4ml.utils.config_from_keras_model(model, granularity='name')
-    cfg['LayerName']['softsign']['Strategy'] = strategy
-    cfg['LayerName']['softsign']['inv_table_t'] = f_type
-    cfg['LayerName']['softsign']['exp_table_t'] = f_type
-    
-    odir = str(test_root_path / 'hls4mlprj_softsign_{}'.format(strategy))
+    cfg = hls4ml.utils.config_from_keras_model(model, granularity='name')    
+    odir = str(test_root_path / 'hls4mlprj_softsign_{}_{}_{}'.format(backend, io_type, str(input_shape)))
     hls_model = hls4ml.converters.convert_from_keras_model(model, hls_config=cfg, io_type=io_type,
                                                            output_dir=odir, backend=backend)
     hls_model.compile()
    
     y_keras = model.predict(X)
     y_hls4ml = hls_model.predict(X).reshape(y_keras.shape)
-
     acc_hls4ml = accuracy_score(np.argmax(y_keras, axis=-1).ravel(), np.argmax(y_hls4ml, axis=-1).ravel())
 
     print('Accuracy hls4ml relative to keras: {}'.format(acc_hls4ml))
-
-    assert acc_hls4ml >= 0.98
-
-
+    assert acc_hls4ml >= 0.97
