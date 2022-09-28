4018820fb6a779ac37f64ed4e9865b4d2e9c0cc1
Vladimir Loncar <vloncar@users.noreply.github.com>
2022-09-28 23:46:30 +0200

    Rewrite pow and recirpocal functions

--- hls4ml/backends/symbolic/passes/expr_templates.py
+++ hls4ml/backends/symbolic/passes/expr_templates.py
@@ -4,6 +4,7 @@ from hls4ml.model.layers import SymbolicExpression
 from hls4ml.backends.template import LayerConfigTemplate, FunctionCallTemplate
 
 from sympy.printing.cxx import CXX11CodePrinter
+from sympy.core.numbers import Integer
 
 # Expression templates
 
@@ -14,13 +15,29 @@ expr_include_list = ['hls_math.h']
 class HLSCodePrinter(CXX11CodePrinter):
     _ns = 'hls::'
 
+    def _symbol_to_array(self, name):
+        return re.sub(r'([a-zA-Z]+)(\d+)', r'\1[\2]', name)
+
     def _print_Float(self, flt):
         float_str = super()._print_Float(flt)
         return f'model_default_t({float_str})'
 
+    def _print_Pow(self, expr):
+        if isinstance(expr.exp, Integer):
+            symbol = next(iter(expr.free_symbols))
+            symbol = self._symbol_to_array(str(symbol))
+            if expr.exp > 1:
+                return '*'.join([str(symbol) for _ in range(expr.exp)]) 
+            elif expr.exp == -1: # 1/x
+                return f'hls::recip({symbol})'
+            else:
+                return super()._print_Pow(expr)
+        else:
+            return super()._print_Pow(expr)
+
     def _print_Symbol(self, expr):
         name = super()._print_Symbol(expr)
-        return re.sub(r'([a-zA-Z]+)(\d+)', r'\1[\2]', name)
+        return self._symbol_to_array(name)
 
 class ExpressionFunctionTemplate(FunctionCallTemplate):
     def __init__(self):
