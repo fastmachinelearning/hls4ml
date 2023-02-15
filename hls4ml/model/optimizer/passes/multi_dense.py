d14426fbd95eaf0e95e248f309d82f28c8253a65
Vladimir Loncar <vloncar@users.noreply.github.com>
2023-02-15 11:02:52 +0100

    Correctly expand dims of pointwise layer

--- hls4ml/model/optimizer/passes/multi_dense.py
+++ hls4ml/model/optimizer/passes/multi_dense.py
@@ -51,7 +51,7 @@ class ReplaceMultidimensionalDenseWithConv(OptimizerPass):
         class_name = 'PointwiseConv' + str(dim) + 'D'
         pw_node = model.make_node(class_name, node.name, pointwise_attrs, node.inputs.copy())
         if len(node.weights['weight'].data.shape) == 2: # This can happen if we assign weights of Dense layer to 1x1 Conv2D
-            pw_node.weights['weight'].data = np.expand_dims(node.weights['weight'].data, axis=(0,1))
+            pw_node.weights['weight'].data = np.expand_dims(node.weights['weight'].data, axis=tuple(range(dim)))
         pw_node.weights['bias'].data = node.weights['bias'].data
         model.replace_node(node, pw_node)
         
