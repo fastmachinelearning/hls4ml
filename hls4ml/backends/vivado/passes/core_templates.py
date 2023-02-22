8a8eae21decb9f7cc82881f60b8dc07666d7972f
Jan-Frederik Schulte <jschulte@cern.ch>
2023-02-22 09:19:06 -0500

    add sigmoid, fixes to run examples

--- hls4ml/backends/vivado/passes/core_templates.py
+++ hls4ml/backends/vivado/passes/core_templates.py
@@ -148,6 +148,7 @@ class ActivationConfigTemplate(LayerConfigTemplate):
     def format(self, node):
         params = self._default_config_params(node)
         params['type'] = node.get_attr('activation')
+
         return self.template.format(**params)
 
 
