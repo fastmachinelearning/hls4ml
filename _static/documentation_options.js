8e763d27cc3e341cf6b046373402404a5830ae14
GitHub Action <action@github.com>
2023-03-10 15:29:46 +0000

    Update Sphinx Documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.5.1.dev642+g05bac8d7',
+    VERSION: '0.5.1.dev644+ga4b0e0c3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
