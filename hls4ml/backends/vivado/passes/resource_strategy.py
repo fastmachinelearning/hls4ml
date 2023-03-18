ba89ef8a62bcd2de1e4d811c4552925f6ab0a233
Javier Duarte <jduarte@ucsd.edu>
2023-03-18 08:24:21 -0700

    apply resource

--- hls4ml/backends/vivado/passes/resource_strategy.py
+++ hls4ml/backends/vivado/passes/resource_strategy.py
@@ -7,7 +7,7 @@ class ApplyResourceStrategy(OptimizerPass):
     ''' Transposes the weights to use the dense_resource matrix multiply routine '''
     def match(self, node):
         
-        node_matches = isinstance(node, (Dense, Conv1D, SeparableConv1D, Conv2D, SeparableConv2D, LSTM, GRU))
+        node_matches = isinstance(node, (Dense, Conv1D, SeparableConv1D, Conv2D, SeparableConv2D, LSTM, GRU, Conv1DTranspose, Conv2DTranspose))
 
         is_resource_strategy = node.get_attr('strategy', '').lower() == 'resource'
         already_transformed = node.get_attr('_weights_transposed', False) == True
