efaefd46e9a3acae7ebddf589ea2f76c4a7c86a1
Jovan Mitrevski <jmitrevs@fnal.gov>
2023-03-31 17:16:25 -0500

    pre-commit for converters

--- hls4ml/converters/pytorch/pooling.py
+++
@@ -1,6 +0,0 @@
-import math
-from hls4ml.converters.pytorch_to_hls import parse_default_pytorch_layer
-from hls4ml.converters.pytorch_to_hls import pytorch_handler
-
-#pooling_layers = ['MaxPooling1D']
-#@pytorch_handler(*pooling_layers)
