a65ab3472da9a21225be92fa4e09dce50e59d407
Jovan Mitrevski <jmitrevs@fnal.gov>
2023-04-06 16:17:56 -0500

    fix cast in remaining places for binary CNNs

--- hls4ml/templates/vivado/nnet_utils/nnet_conv2d_latency.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_conv2d_latency.h
@@ -73,7 +73,7 @@ PartitionLoop:
         // Cast to "res_t" type
         Result:
             for (int i_res = 0; i_res < mult_n_out; i_res++) {
-                *(res++) = cast<data_T, res_T, CONFIG_T>(acc[i_res]);
+                *(res++) = cast<data_T, res_T, typename CONFIG_T::mult_config>(acc[i_res]);
             }
         }
     }
