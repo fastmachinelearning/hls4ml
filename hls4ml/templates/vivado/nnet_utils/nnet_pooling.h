d78af8f68eead4898865af20428cc5a480ad479c
Jan-Frederik Schulte <jschulte@cern.ch>
2023-04-13 11:55:57 -0400

    diff clean

--- hls4ml/templates/vivado/nnet_utils/nnet_pooling.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_pooling.h
@@ -89,7 +89,7 @@ struct pooling1d_config {
     static const unsigned n_out = (n_in - pool_width) / stride_width + 1;
     static const unsigned pad_left = 0;
     static const unsigned pad_right = 0;
-    // Pooling functionpad_right
+    // Pooling function
     static const Pool_Op pool_op = Max;
 };
 
