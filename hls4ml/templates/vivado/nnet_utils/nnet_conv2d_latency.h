df5288f56d1d6003161409854240f352ed1cfc06
Dylan R <dylansr90@gmail.com>
2023-04-21 14:10:35 -0500

    update for vitis compliance

--- hls4ml/templates/vivado/nnet_utils/nnet_conv2d_latency.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_conv2d_latency.h
@@ -15,6 +15,7 @@ void conv_2d_latency_cl(
     typename CONFIG_T::bias_t biases[CONFIG_T::n_filt]) {
     constexpr unsigned mult_n_in = CONFIG_T::filt_height * CONFIG_T::filt_width * CONFIG_T::n_chan;
     constexpr unsigned mult_n_out = CONFIG_T::n_filt;
+    constexpr unsigned n_unroll_in = mult_n_in/CONFIG_T::reuse_factor;
 
     data_T data_buf[CONFIG_T::n_pixels][mult_n_in];
     #pragma HLS ARRAY_PARTITION variable=data_buf complete dim=0
@@ -33,7 +34,7 @@ void conv_2d_latency_cl(
 
 PartitionLoop:
     for (int i_part = 0; i_part < CONFIG_T::n_partitions; i_part++) {
-        #pragma HLS PIPELINE II=CONFIG_T::reuse_factor
+        #pragma HLS PIPELINE II=CONFIG_T::reuse_factor rewind
 
         CONFIG_T::template fill_buffer<data_T, CONFIG_T>::fill_buffer(data, data_buf, i_part);
 
@@ -46,9 +47,7 @@ PartitionLoop:
         // Do the matrix-multiply
         Product1:
             for (int i_in = 0; i_in < mult_n_in; i_in++) {
-                if (CONFIG_T::n_partitions == 1) {
-                    #pragma HLS UNROLL
-                }
+                #pragma HLS UNROLL factor=n_unroll_in
                 cache = data_buf[i_pxl][i_in];
             Product2:
                 for (int i_out = 0; i_out < mult_n_out; i_out++) {
@@ -67,9 +66,7 @@ PartitionLoop:
         // Accumulate multiplication result
         Accum1:
             for (int i_in = 0; i_in < mult_n_in; i_in++) {
-                if (CONFIG_T::n_partitions == 1) {
-                    #pragma HLS UNROLL
-                }
+                #pragma HLS UNROLL factor=n_unroll_in
             Accum2:
                 for (int i_out = 0; i_out < mult_n_out; i_out++) {
                     acc[i_out] += mult[i_in * mult_n_out + i_out];
