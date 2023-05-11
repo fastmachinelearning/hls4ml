569a6a4fff741d59dc96b3ccc5432e3c07e84e97
David Burnette <hlslibs@mentor.com>
2023-05-11 15:05:46 -0700

    added settings for testbench data and turned on csim

--- example-prjs/fashion/vivado.py
+++ example-prjs/fashion/vivado.py
@@ -1,6 +1,11 @@
 import hls4ml
 import yaml
 import numpy as np
+import tensorflow as tf
+
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data()
+np.savetxt('tb_data/tb_input_features.dat', np.array(test_images.reshape(test_images.shape[0], -1), dtype='int32'), fmt='%d')
+np.savetxt('tb_data/tb_output_predictions.dat', np.array(test_labels.reshape(test_labels.shape[0], -1), dtype='int32'), fmt='%d')
 
 print(hls4ml.__version__)
 with open('qkeras/config.yml', 'r') as ymlfile:
@@ -9,8 +14,10 @@ with open('qkeras/config.yml', 'r') as ymlfile:
 print('NETWORK')
 print(config)
 config['OutputDir'] = 'my-Vivado-test'
+config['InputData'] = 'tb_data/tb_input_features.dat'
+config['OutputPredictions'] = 'tb_data/tb_output_predictions.dat'
 config['Backend'] = 'Vivado'
 config['IOType'] = 'io_stream'
 hls_model = hls4ml.converters.keras_to_hls(config)
-hls_model.build()
+hls_model.build(csim=True)
 
