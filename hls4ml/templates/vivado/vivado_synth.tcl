9cab74a2eb314f2bcb785af6f38576055f97ef5a
Benjamin Ramhorst <bramhorst27@gmail.com>
2023-06-11 18:58:32 +0100

    Add post-synthesis design optimisation to remove unused BRAM

--- hls4ml/templates/vivado/vivado_synth.tcl
+++ hls4ml/templates/vivado/vivado_synth.tcl
@@ -3,4 +3,5 @@ source [file join $tcldir project.tcl]
 
 add_files ${project_name}_prj/solution1/syn/verilog
 synth_design -top ${project_name} -part $part
+opt_design -retarget -propconst -sweep -bram_power_opt -shift_register_opt
 report_utilization -file vivado_synth.rpt
