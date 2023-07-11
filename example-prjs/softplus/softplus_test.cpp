fa7cb822beb76459699239c91216162619d33689
BLMartin99 <Ms.BaileyL.Martin@gmail.com>
2023-07-11 10:17:10 -0700

    save updates

--- example-prjs/softplus/softplus_test.cpp
+++ example-prjs/softplus/softplus_test.cpp
@@ -101,7 +101,7 @@ CCS_MAIN(int argc, char *argv[])
 
       for(int i = 0; i < OUT_HEIGHT_2*OUT_WIDTH_2; i++)
       {
-	std::cout << "Expected: " << pr[i] << " Actual: " << layer3_out[i][0] << std::endl;
+	std::cout << "Expected: " << pr[i] << " Actual: " << layer3_out[i][0].to_double() << std::endl;
       }
 
       //hls-fpga-machine-learning insert tb-output
