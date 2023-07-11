29a95c18a7488917aca6ba6272a4dbb109a07343
BLMartin99 <Ms.BaileyL.Martin@gmail.com>
2023-07-11 13:21:45 -0700

    Issue with softplus table

--- hls4ml/templates/catapult/nnet_utils/nnet_activation_stream.h
+++ hls4ml/templates/catapult/nnet_utils/nnet_activation_stream.h
@@ -620,10 +620,10 @@ void softplus(ac_channel<data_T> &data, ac_channel<res_T> &res) {
         #pragma hls_unroll
         SoftplusPackLoop: for (int j = 0; j < res_T::size; j++) {
             //#pragma HLS UNROLL
-            int data_round = in_data[j]*CONFIG_T::table_size/16;
-            int index = data_round + 8*CONFIG_T::table_size/16;
+            int data_round = (int)in_data[j].to_double()*(int)CONFIG_T::table_size/16;
+            int index = data_round + 8*(int)CONFIG_T::table_size/16;
             if (index < 0)   index = 0;
-            else if (index > CONFIG_T::table_size-1) index = CONFIG_T::table_size-1;
+            else if (index > CONFIG_T::table_size-1) index = (int)CONFIG_T::table_size-1;
             out_data[j] = softplus_table[index];
         }
         res.write(out_data);
