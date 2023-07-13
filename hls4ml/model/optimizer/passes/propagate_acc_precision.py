snapshot that runs qonnx test, but gets incorrect results

hls4ml/model/optimizer/passes/propagate_acc_precision.py
@@ -2,9 +2,41 @@ import math  # prefer to use math.ceil for scalar values (returns int)
 
 import numpy as np
 
-from hls4ml.model.layers import Conv1D, Conv2D
+from hls4ml.model.layers import Conv1D, Conv2D, Dense
 from hls4ml.model.optimizer import OptimizerPass
 from hls4ml.model.types import FixedPrecisionType, NamedType
+
+
+class PropagateDensePrecision(OptimizerPass):
+    """
+    Propagate precision for Dense nodes. Restrict it to only cases where
+    the precision is set by a quant node, since otherwise the values get huge.
+    """
+
+    def match(self, node):
+        is_match = isinstance(node, Dense)
+        return is_match
+
+    def transform(self, model, node):
+        input_precision = node.get_input_node().get_attr("quant_precision")
+        weight_precision = node.get_attr("weight_precision")
+        if not input_precision or not weight_precision:
+            return False
+
+        bias_precision = node.get_attr("bias_precision")
+        input_variable = node.get_input_variable()
+        num_acc = input_variable.shape[-1]
+
+        accum_precision = _propagate_type_acc(input_precision, weight_precision, bias_precision, num_acc)
+
+        accum_t = NamedType(f'layer{node.index}_accum_t', accum_precision)
+        node.set_attr('accum_t', accum_t)
+
+        if not node.get_attr("quant_precision"):
+            # output precision not set by quant node
+            node.update_output_precision(accum_precision)
+
+        return False
 
 
 class PropagateConvPrecision(OptimizerPass):
@@ -27,14 +59,9 @@ class PropagateConvPrecision(OptimizerPass):
         filt_width = node.get_attr('filt_width')
         filt_height = node.get_attr('filt_height', 1)
 
-        accum_precision = _propagate_type_conv(
-            input_precision,
-            weight_precision,
-            bias_precision,
-            num_feature_maps=num_feature_maps,
-            filt_width=filt_width,
-            filt_height=filt_height,
-        )
+        num_acc = filt_width * filt_height * num_feature_maps
+
+        accum_precision = _propagate_type_acc(input_precision, weight_precision, bias_precision, num_acc)
 
         accum_t = NamedType(f'layer{node.index}_accum_t', accum_precision)
         node.set_attr('accum_t', accum_t)
@@ -46,14 +73,14 @@ class PropagateConvPrecision(OptimizerPass):
         return False
 
 
-def _propagate_type_conv(input_precision, weight_precision, bias_precision, num_feature_maps, filt_width, filt_height):
+def _propagate_type_acc(input_precision, weight_precision, bias_precision, num_acc):
     '''
     Propagate the precion type across a multiply. Rounding modes are propagated from input_precision
     '''
 
-    Nacc = filt_width * filt_height * num_feature_maps
-    bitwidth = weight_precision.width + input_precision.width + math.ceil(np.log2(Nacc))
-    integer = weight_precision.integer + input_precision.integer + math.ceil(np.log2(Nacc))
+    # check to make sure none are None
+    bitwidth = weight_precision.width + input_precision.width + math.ceil(np.log2(num_acc))
+    integer = weight_precision.integer + input_precision.integer + math.ceil(np.log2(num_acc))
     signed = weight_precision.signed or input_precision.signed
 
     # Because calculating precision, no need to round or sautration