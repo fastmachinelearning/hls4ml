84a8dc20fa6bcba502492518034137fc459fe223
Harish Jamakhandi <harish.jamakhandi@siemens.com>
2023-07-19 03:11:05 -0700

    Adding Conv 2d with batch norm ntw for the Fashion mnist.

--- example-prjs/Con2DFlat/catapult.py
+++ example-prjs/Con2DFlat/catapult.py
@@ -11,79 +11,73 @@ from qkeras import *
 import json
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-import numpy as np
+from tensorflow.keras import layers
 
-# Load the MNIST dataset
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+# Load and preprocess the Fashion MNIST dataset
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
+x_train = x_train.reshape(-1, 28, 28, 1) / 255.0
+x_test = x_test.reshape(-1, 28, 28, 1) / 255.0
 
-# Preprocess the data
-X_train = X_train.reshape(-1, 28, 28, 1) / 255.0
-X_test = X_test.reshape(-1, 28, 28, 1) / 255.0
-y_train = tf.keras.utils.to_categorical(y_train, num_classes=10)
-y_test = tf.keras.utils.to_categorical(y_test, num_classes=10)
+# Resize the input data to 4x4
+x_train_resized = tf.image.resize(x_train, size=(4, 4)).numpy()
+x_test_resized = tf.image.resize(x_test, size=(4, 4)).numpy()
 
-# Define the model architecture
-def mnist_model():
-    model = Sequential()
-    model.add(Conv2D(20, (5, 5), use_bias=True, padding='same', activation='relu', input_shape=(28, 28, 1)))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(50, (3, 3), use_bias=True, padding='same', activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Flatten())
-    model.add(Dense(10, use_bias=True, kernel_initializer='normal', activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
+num_classes = 10
+input_shape = (4, 4, 1)
 
-# Create an instance of the model
-model = mnist_model()
+# Build the model
+model = tf.keras.Sequential()
+
+model.add(layers.Input(shape=input_shape))
+model.add(layers.Conv2D(32, kernel_size=(3, 3), activation="relu"))
+model.add(layers.BatchNormalization())
+model.add(layers.Flatten())
+model.add(layers.Dense(num_classes, activation="softmax"))
+
+# Compile the model
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
 # Train the model
-model.fit(X_train, y_train, batch_size=128, epochs=5, validation_data=(X_test, y_test))
+model.fit(x_train_resized, y_train, batch_size=128, epochs=10, validation_split=0.1)
+
+# Evaluate the model
+test_loss, test_acc = model.evaluate(x_test_resized, y_test)
+print(f"Test Loss: {test_loss}")
+print(f"Test Accuracy: {test_acc}")
+
 
 # Save the input feature set and output predictions in .dat format
-np.savetxt('tb_data/tb_input_features.dat', X_test.reshape(X_test.shape[0], -1), fmt='%f')
-np.savetxt('tb_data/tb_output_predictions.dat', np.argmax(model.predict(X_test), axis=1), fmt='%d')
+np.savetxt('tb_data/tb_input_features.dat', x_test_resized.reshape(x_test_resized.shape[0], -1), fmt='%f')
+np.savetxt('tb_data/tb_output_predictions.dat', np.argmax(model.predict(x_test_resized), axis=1), fmt='%d')
 
 # Serialize model to json
 json_model = model.to_json()
 
 # Save the model architecture to JSON file
-with open('densemnist_model.json', 'w') as json_file:
+with open('BatchConMnist_model.json', 'w') as json_file:
     json_file.write(json_model)
 
 # Saving the weights of the model
 print("\n============================================================================================")
 print("Writing weights file")
-model.save_weights('DenseMNIST_weights.h5')
-model.save('DenseMNIST.h5')
+model.save_weights('BatchConMnist_weights.h5')
+model.save('BatchConMnist.h5')
 
 # Write Json
 print("\n============================================================================================")
 print("Writing json file")
-with open('densemnist_model.json', 'r') as f:
+with open('BatchConMnist_model.json', 'r') as f:
     x = json.loads(f.read())
 # Rewrite pretty-printed
-with open('densemnist_model2.json', 'w') as f:
+with open('BatchConMnist_model2.json', 'w') as f:
     f.write(json.dumps(x, indent=2))
 
 print("\n============================================================================================")
 print("Configuring HLS4ML")
-config = {}
+with open('BatchConMnist_config.yml', 'r') as ymlfile:
+  config = yaml.safe_load(ymlfile)
 config['Backend'] = 'Catapult'
-config['ClockPeriod'] = 100
-config['HLSConfig'] = {'Model': {'Precision': 'ac_fixed<16,8>', 'ReuseFactor': 1}}
-config['IOType'] = 'io_stream'
-config['KerasH5'] = 'DenseMNIST_weights.h5'
-config['KerasJson'] = 'densemnist_model2.json'
-config['ProjectName'] = 'myproject'
-config['Part'] = 'xcku115-flvb2104-2-i'
-config['XilinxPart'] = 'xcku115-flvb2104-2-i'
 config['OutputDir'] = 'my-Catapult-test'
-config['InputData'] = 'tb_data/tb_input_features.dat'
-config['OutputPredictions'] = 'tb_data/tb_output_predictions.dat'
 
 print("\n============================================================================================")
 print("HLS4ML converting keras model to HLS C++")
