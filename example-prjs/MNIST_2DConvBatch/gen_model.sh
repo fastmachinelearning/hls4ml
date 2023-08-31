30ab5b8f5c991b221cbe642c7f2403bbcf012a51
Harish Jamakhandi <harish.jamakhandi@siemens.com>
2023-08-30 18:20:08 -0700

    Adding single script project for Conv2d and Dense

---
+++ example-prjs/MNIST_2DConvBatch/gen_model.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+# rm -rf tb_data my-* conv2d* a.out perf.data
+# mkdir tb_data
+# This script runs the Vivado flows to generate the TensorFlow Keras Model.
+VENV=../../../../venv
+
+MGC_HOME=/wv/hlsb/CATAPULT/TOT/CURRENT/aol/Mgc_home
+export MGC_HOME
+
+export PATH=/wv/hlstools/python/python38/bin:$PATH:$XILINX_VIVADO/bin:$MGC_HOME/bin
+export LD_LIBRARY_PATH=/wv/hlstools/python/python38/lib:$XILINX_VIVADO/lib/lnx64.o:$MGC_HOME/lib
+export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+
+# needed for pytest
+export OSTYPE=linux-gnu
+
+echo "Activating Virtual Environment..."
+#bash
+source $VENV/bin/activate
+
+# actually run HLS4ML + Vivado HLS
+python3 model.py
