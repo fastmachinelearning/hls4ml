8119029d178e373ca35c9bc0f723cac051e3ad1d
Jovan Mitrevski <jmitrevs@fnal.gov>
2024-01-21 12:13:18 -0600

    snapshot of work before reworking types

--- hls4ml/backends/oneapi/oneapi_types.py
+++ hls4ml/backends/oneapi/oneapi_types.py
@@ -1,7 +1,13 @@
 '''
 This package includes oneAPI-specific customizations to the variable types
 '''
-from hls4ml.backends.fpga.fpga_types import ArrayVariableConverter, VariableDefinition
+from hls4ml.backends.fpga.fpga_types import (
+    ArrayVariableConverter,
+    InplaceStreamVariableConverter,
+    StreamVariableConverter,
+    VariableDefinition,
+)
+from hls4ml.utils.string_utils import convert_to_pascal_case
 
 # region ArrayVarable
 
@@ -51,16 +57,16 @@ class InterfaceVariableConverter:
         self.prefix = prefix
         self.definition_cls = definition_cls
 
-    def convert(self, tensor_var, pipe_name, pipe_id, array_type, pragma='partition'):
+    def convert(self, tensor_var, pragma='partition'):
         if isinstance(tensor_var, self.definition_cls):  # Already converted
             return tensor_var
 
         tensor_var.pragma = pragma
         tensor_var.type = self.type_converter.convert(tensor_var.type)
 
-        tensor_var.pipe_name = pipe_name
-        tensor_var.pipe_id = pipe_id
-        tensor_var.array_type = array_type
+        tensor_var.pipe_name = f'{convert_to_pascal_case(tensor_var.name)}Pipe'
+        tensor_var.pipe_id = f'{convert_to_pascal_case(tensor_var.name)}PipeID'
+        tensor_var.array_type = f'{tensor_var.name}_array_t'
 
         tensor_var.__class__ = type(self.prefix + 'InterfaceMemberVariable', (type(tensor_var), self.definition_cls), {})
         return tensor_var
@@ -69,6 +75,41 @@ class InterfaceVariableConverter:
 class OneAPIInterfaceVariableConverter(InterfaceVariableConverter):
     def __init__(self, type_converter):
         super().__init__(type_converter=type_converter, prefix='OneAPI', definition_cls=OneAPIInterfaceVariableDefinition)
+
+
+# endregion
+
+
+# region StreamVariable
+class OneAPIStreamVariableDefinition(VariableDefinition):
+    def definition_cpp(self, name_suffix='', as_reference=True):
+        return f'{self.name}{name_suffix}'
+
+    def declare_cpp(self, pipe_min_size=0, indent=''):
+        lines = indent + f'class {self.pipe_id};\n'
+        lines += indent + f'using {self.array_type} = std::array<{self.type.name}, {self.size_cpp()}>;\n'
+        lines += indent + (
+            f'using {self.pipe_name} = sycl::ext::intel::experimental::pipe<{self.pipe_id}, '
+            + f'{self.array_type}, {pipe_min_size}>;\n'
+        )
+        return lines
+
+
+class OneAPIInplaceStreamVariableDefinition(VariableDefinition):
+    def definition_cpp(self):
+        return f'using {self.name} = {self.input_var.name}'
+
+
+class OneAPIStreamVariableConverter(StreamVariableConverter):
+    def __init__(self, type_converter):
+        super().__init__(type_converter=type_converter, prefix='OneAPI', definition_cls=OneAPIStreamVariableDefinition)
+
+
+class OneAPIInplaceStreamVariableConverter(InplaceStreamVariableConverter):
+    def __init__(self, type_converter):
+        super().__init__(
+            type_converter=type_converter, prefix='OneAPI', definition_cls=OneAPIInplaceStreamVariableDefinition
+        )
 
 
 # endregion
--- hls4ml/backends/oneapi/passes/transform_types.py
+++ hls4ml/backends/oneapi/passes/transform_types.py
@@ -2,11 +2,14 @@ from hls4ml.backends.fpga.fpga_types import ACTypeConverter, HLSTypeConverter, S
 from hls4ml.backends.oneapi.oneapi_types import (
     OneAPIArrayVariableConverter,
     OneAPIInplaceArrayVariableConverter,
+    OneAPIInplaceStreamVariableConverter,
     OneAPIInterfaceVariableConverter,
+    OneAPIStreamVariableConverter,
 )
 from hls4ml.model.optimizer import GlobalOptimizerPass
 from hls4ml.model.types import InplaceTensorVariable
-from hls4ml.utils.string_utils import convert_to_pascal_case
+
+# from hls4ml.utils.string_utils import convert_to_pascal_case
 
 
 class TransformTypes(GlobalOptimizerPass):
@@ -15,6 +18,8 @@ class TransformTypes(GlobalOptimizerPass):
         self.array_var_converter = OneAPIArrayVariableConverter(type_converter=self.type_converter)
         self.inplace_array_var_converter = OneAPIInplaceArrayVariableConverter(type_converter=self.type_converter)
         self.interface_var_converter = OneAPIInterfaceVariableConverter(type_converter=self.type_converter)
+        self.stream_var_converter = OneAPIStreamVariableConverter(type_converter=self.type_converter)
+        self.inplace_stream_var_converter = OneAPIInplaceStreamVariableConverter(type_converter=self.type_converter)
         self.weight_var_converter = StaticWeightVariableConverter(type_converter=self.type_converter)
 
     def transform(self, model, node):
@@ -22,24 +27,15 @@ class TransformTypes(GlobalOptimizerPass):
 
         for out_name, var in node.variables.items():
             if io_type == 'io_stream':
-                raise NotImplementedError("io_stream is not yet implemented for oneAPI")
+                if isinstance(var, InplaceTensorVariable):
+                    new_var = self.inplace_stream_var_converter.convert(var)
+                else:
+                    new_var = self.stream_var_converter.convert(var)
             elif io_type == 'io_parallel':
                 if out_name in node.model.inputs:
-                    new_var = self.interface_var_converter.convert(
-                        var,
-                        pragma='intel::fpga_register',
-                        pipe_name=f'{convert_to_pascal_case(var.name)}Pipe',
-                        pipe_id=f'{convert_to_pascal_case(var.name)}PipeID',
-                        array_type=f'{var.name}_array_t',
-                    )
+                    new_var = self.interface_var_converter.convert(var, pragma='intel::fpga_register')
                 elif out_name in node.model.outputs:
-                    new_var = self.interface_var_converter.convert(
-                        var,
-                        pragma='intel::fpga_register',
-                        pipe_name=f'{convert_to_pascal_case(var.name)}Pipe',
-                        pipe_id=f'{convert_to_pascal_case(var.name)}PipeID',
-                        array_type=f'{var.name}_array_t',
-                    )
+                    new_var = self.interface_var_converter.convert(var, pragma='intel::fpga_register')
                 elif isinstance(var, InplaceTensorVariable):
                     new_var = self.inplace_array_var_converter.convert(var, pragma='')
                 else:
