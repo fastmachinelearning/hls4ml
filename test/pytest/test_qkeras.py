ba27285f45f4b4b7e4833f3a43fb972a3fa3e458
Vladimir Loncar <vloncar@users.noreply.github.com>
2024-03-26 00:22:41 +0100

    Support negative_slope in quantized_relu

--- test/pytest/test_qkeras.py
+++ test/pytest/test_qkeras.py
@@ -308,6 +308,44 @@ def test_quantizer(randX_1000_1, quantizer, backend, io_type):
 
 
 @pytest.mark.parametrize(
+    'quantizer',
+    [
+        (quantized_relu(4, negative_slope=0.5)),
+        (quantized_relu(8, 4, negative_slope=1.0)),
+        (quantized_relu(10, 2, negative_slope=0.25)),
+    ],
+)
+@pytest.mark.parametrize('backend', ['Vivado', 'Vitis', 'Quartus'])
+@pytest.mark.parametrize('io_type', ['io_parallel', 'io_stream'])
+def test_relu_negative_slope(randX_1000_1, quantizer, backend, io_type):
+    '''
+    Test a a transformation of quantized_relu with negative_slope to leaky_relu activation layer.
+    '''
+    X = randX_1000_1
+    X = -X # Make it negative so leaky relu does something
+    X = np.round(X * 2**10) * 2**-10  # make it an exact ap_fixed<16,6>
+    model = Sequential()
+    model.add(QActivation(input_shape=(1,), activation=quantizer, name='quantizer'))
+    model.compile()
+
+    config = hls4ml.utils.config_from_keras_model(model, granularity='name')
+    output_dir = str(
+        test_root_path
+        / 'hls4mlprj_qkeras_leaky_relu_{}_{}_neg_slope_{}_{}_{}'.format(
+            quantizer.bits, quantizer.integer, quantizer.negative_slope, backend, io_type
+        )
+    )
+    hls_model = hls4ml.converters.convert_from_keras_model(
+        model, hls_config=config, output_dir=output_dir, backend=backend, io_type=io_type
+    )
+    hls_model.compile()
+
+    y_qkeras = model.predict(X)
+    y_hls4ml = hls_model.predict(X)
+    np.testing.assert_allclose(y_hls4ml, y_qkeras, rtol=1e-5, atol=0)
+
+
+@pytest.mark.parametrize(
     'weight_quantizer,activation_quantizer,',
     [
         ('binary', 'binary'),
