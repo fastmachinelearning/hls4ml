01c436be0b724336edf9d5b2dee125c43591cfd9
Vladimir Loncar <vloncar@users.noreply.github.com>
2024-03-28 21:47:00 +0100

    Add ability to configure writer in Vitis/Vivado, to optionally write namespace, tar file and weight txt files

--- hls4ml/writer/vitis_writer.py
+++ hls4ml/writer/vitis_writer.py
@@ -30,5 +30,4 @@ class VitisWriter(VivadoWriter):
         """
         super().write_hls(model)
         self.write_nnet_utils_overrides(model)
-        os.remove(model.config.get_output_dir() + '.tar.gz')
         self.write_tar(model)
