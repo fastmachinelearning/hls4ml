7dd01737d79856ce790d78e6ab50597a4ac74131
steltze <stel.tze09@gmail.com>
2024-04-19 14:41:21 +0200

    Set vitis accelerator writer

--- hls4ml/writer/__init__.py
+++ hls4ml/writer/__init__.py
@@ -1,6 +1,7 @@
 from hls4ml.writer.quartus_writer import QuartusWriter
 from hls4ml.writer.symbolic_writer import SymbolicExpressionWriter
-from hls4ml.writer.vitis_writer import VitisWriter
+from hls4ml.writer.vitis_writer import VitisWrite
+from hls4ml.writer.vitis_accelerator_writer import VitisAcceleratorWriter
 from hls4ml.writer.vivado_accelerator_writer import VivadoAcceleratorWriter
 from hls4ml.writer.vivado_writer import VivadoWriter
 from hls4ml.writer.writers import Writer, get_writer, register_writer  # noqa: F401
@@ -8,5 +9,6 @@ from hls4ml.writer.writers import Writer, get_writer, register_writer  # noqa: F
 register_writer('Vivado', VivadoWriter)
 register_writer('VivadoAccelerator', VivadoAcceleratorWriter)
 register_writer('Vitis', VitisWriter)
+register_writer('VitisAccelerator', VitisAcceleratorWriter)
 register_writer('Quartus', QuartusWriter)
 register_writer('SymbolicExpression', SymbolicExpressionWriter)
