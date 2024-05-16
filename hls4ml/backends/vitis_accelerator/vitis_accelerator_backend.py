b4dd93d867b18984822cc2578ed523341bd287c9
alex-yang-upenn <chukyang@sas.upenn.edu>
2024-05-15 20:11:28 -0700

    reverting unnecessary changes

--- hls4ml/backends/vitis_accelerator/vitis_accelerator_backend.py
+++ hls4ml/backends/vitis_accelerator/vitis_accelerator_backend.py
@@ -7,27 +7,6 @@ class VitisAcceleratorBackend(VitisBackend):
         super(VivadoBackend, self).__init__(name='VitisAccelerator')
         self._register_layer_attributes()
         self._register_flows()
-
-    def _register_flows(self):
-        validation_passes = [
-            'vitisaccelerator:validate_conv_implementation',
-            'vitisaccelerator:validate_strategy',
-        ]
-        validation_flow = register_flow('validation', validation_passes, requires=['vivado:init_layers'], backend=self.name)
-
-        # Any potential templates registered specifically for Vitis backend
-        template_flow = register_flow(
-            'apply_templates', self._get_layer_templates, requires=['vivado:init_layers'], backend=self.name
-        )
-
-        writer_passes = ['make_stamp', 'vitisaccelerator:write_hls']
-        self._writer_flow = register_flow('write', writer_passes, requires=['vitis:ip'], backend=self.name)
-
-        ip_flow_requirements = get_flow('vivado:ip').requires.copy()
-        ip_flow_requirements.insert(ip_flow_requirements.index('vivado:init_layers'), validation_flow)
-        ip_flow_requirements.insert(ip_flow_requirements.index('vivado:apply_templates'), template_flow)
-
-        self._default_flow = register_flow('ip', None, requires=ip_flow_requirements, backend=self.name)
     
     def create_initial_config(
         self,
@@ -59,3 +38,24 @@ class VitisAcceleratorBackend(VitisBackend):
         config['AcceleratorConfig']['Num_Thread'] = num_thread
         config['AcceleratorConfig']['Batchsize'] = batchsize
         return config
+    
+    def _register_flows(self):
+        validation_passes = [
+            'vitisaccelerator:validate_conv_implementation',
+            'vitisaccelerator:validate_strategy',
+        ]
+        validation_flow = register_flow('validation', validation_passes, requires=['vivado:init_layers'], backend=self.name)
+
+        # Any potential templates registered specifically for Vitis backend
+        template_flow = register_flow(
+            'apply_templates', self._get_layer_templates, requires=['vivado:init_layers'], backend=self.name
+        )
+
+        writer_passes = ['make_stamp', 'vitisaccelerator:write_hls']
+        self._writer_flow = register_flow('write', writer_passes, requires=['vitis:ip'], backend=self.name)
+
+        ip_flow_requirements = get_flow('vivado:ip').requires.copy()
+        ip_flow_requirements.insert(ip_flow_requirements.index('vivado:init_layers'), validation_flow)
+        ip_flow_requirements.insert(ip_flow_requirements.index('vivado:apply_templates'), template_flow)
+
+        self._default_flow = register_flow('ip', None, requires=ip_flow_requirements, backend=self.name)
