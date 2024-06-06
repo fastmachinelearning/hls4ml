b4ed5bc0d8e429d86ea4b3fa82f059c037b87e50
Jovan Mitrevski <jmitrevs@fnal.gov>
2024-06-06 09:26:19 -0500

    make queues static in bridge

--- hls4ml/templates/oneapi/myproject_bridge.cpp
+++ hls4ml/templates/oneapi/myproject_bridge.cpp
@@ -57,7 +57,7 @@ void myproject_float(
     // hls-fpga-machine-learning insert header #float
 ) {
     auto selector = sycl::ext::intel::fpga_emulator_selector_v;
-    sycl::queue q(selector, fpga_tools::exception_handler, sycl::property::queue::enable_profiling{});
+    static sycl::queue q(selector, fpga_tools::exception_handler, sycl::property::queue::enable_profiling{});
 
     // hls-fpga-machine-learning insert wrapper #float
 }
@@ -68,7 +68,7 @@ void myproject_double(
     // hls-fpga-machine-learning insert header #double
 ) {
     auto selector = sycl::ext::intel::fpga_emulator_selector_v;
-    sycl::queue q(selector, fpga_tools::exception_handler, sycl::property::queue::enable_profiling{});
+    static sycl::queue q(selector, fpga_tools::exception_handler, sycl::property::queue::enable_profiling{});
 
     // hls-fpga-machine-learning insert wrapper #double
 }
