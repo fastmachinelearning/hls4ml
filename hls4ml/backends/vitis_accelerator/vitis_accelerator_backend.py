c73114c7ffadfeca46d277497e9cb4812a7e68c5
Quentin Berthet <quentin.berthet@cern.ch>
2024-07-01 13:31:39 +0200

    Python black formating

--- hls4ml/backends/vitis_accelerator/vitis_accelerator_backend.py
+++ hls4ml/backends/vitis_accelerator/vitis_accelerator_backend.py
@@ -9,23 +9,23 @@ from hls4ml.model.flow import get_flow, register_flow
 
 class VitisAcceleratorBackend(VitisBackend):
     def __init__(self):
-        super(VivadoBackend, self).__init__(name='VitisAccelerator')
+        super(VivadoBackend, self).__init__(name="VitisAccelerator")
         self._register_layer_attributes()
         self._register_flows()
 
     def create_initial_config(
         self,
-        board='alveo-u55c',
+        board="alveo-u55c",
         part=None,
         clock_period=5,
-        io_type='io_parallel',
+        io_type="io_parallel",
         num_kernel=1,
         num_thread=1,
         batchsize=8192,
         hw_quant=False,
-        vivado_directives=[]
+        vivado_directives=[],
     ):
-        '''
+        """
         Create initial accelerator config with default parameters
 
         Args:
@@ -38,26 +38,42 @@ class VitisAcceleratorBackend(VitisBackend):
             vivado_directives: Directives passed down to Vivado that controls the hardware synthesis and implementation steps
         Returns:
             populated config
-        '''
-        board = board if board is not None else 'alveo-u55c'
+        """
+        board = board if board is not None else "alveo-u55c"
         config = super().create_initial_config(part, clock_period, io_type)
-        config['AcceleratorConfig'] = {}
-        config['AcceleratorConfig']['Board'] = board
-        config['AcceleratorConfig']['Num_Kernel'] = num_kernel
-        config['AcceleratorConfig']['Num_Thread'] = num_thread
-        config['AcceleratorConfig']['Batchsize'] = batchsize
-        config['AcceleratorConfig']['HW_Quant'] = hw_quant
-        config['AcceleratorConfig']['Vivado_Directives'] = vivado_directives
+        config["AcceleratorConfig"] = {}
+        config["AcceleratorConfig"]["Board"] = board
+        config["AcceleratorConfig"]["Num_Kernel"] = num_kernel
+        config["AcceleratorConfig"]["Num_Thread"] = num_thread
+        config["AcceleratorConfig"]["Batchsize"] = batchsize
+        config["AcceleratorConfig"]["HW_Quant"] = hw_quant
+        config["AcceleratorConfig"]["Vivado_Directives"] = vivado_directives
         return config
 
-    def build(self, model, reset=False, synth=True, vsynth=True, csim=False, cosim=False, debug=False, **kwargs):
-        if 'linux' in sys.platform:
-            if 'XILINX_VITIS' not in os.environ:
-                raise Exception("XILINX_VITIS environmental variable missing. Please install XRT and Vitis, and run the setup scripts before building")
-            if 'XILINX_XRT' not in os.environ:
-                raise Exception("XILINX_XRT environmental variable missing. Please install XRT and Vitis, and run the setup scripts before building")
-            if 'XILINX_VIVADO' not in os.environ:
-                raise Exception("XILINX_VIVADO environmental variable missing. Please install XRT and Vitis, and run the setup scripts before building")
+    def build(
+        self,
+        model,
+        reset=False,
+        synth=True,
+        vsynth=True,
+        csim=False,
+        cosim=False,
+        debug=False,
+        **kwargs,
+    ):
+        if "linux" in sys.platform:
+            if "XILINX_VITIS" not in os.environ:
+                raise Exception(
+                    "XILINX_VITIS environmental variable missing. Please install XRT and Vitis, and run the setup scripts before building"
+                )
+            if "XILINX_XRT" not in os.environ:
+                raise Exception(
+                    "XILINX_XRT environmental variable missing. Please install XRT and Vitis, and run the setup scripts before building"
+                )
+            if "XILINX_VIVADO" not in os.environ:
+                raise Exception(
+                    "XILINX_VIVADO environmental variable missing. Please install XRT and Vitis, and run the setup scripts before building"
+                )
 
             curr_dir = os.getcwd()
             os.chdir(model.config.get_output_dir())
@@ -68,7 +84,7 @@ class VitisAcceleratorBackend(VitisBackend):
                 if synth:
                     os.system("make cleanhls")
                 os.system("rm -rf host")
-            
+
             if vsynth:
                 if synth:
                     target = "all "
@@ -90,13 +106,17 @@ class VitisAcceleratorBackend(VitisBackend):
             command = "make " + target
 
             # Pre-loading libudev
-            ldconfig_output = subprocess.check_output(["ldconfig", "-p"]).decode("utf-8")
+            ldconfig_output = subprocess.check_output(["ldconfig", "-p"]).decode(
+                "utf-8"
+            )
             for line in ldconfig_output.split("\n"):
                 if "libudev.so" in line and "x86" in line:
-                    command = "LD_PRELOAD=" + line.split("=>")[1].strip() + " " + command
+                    command = (
+                        "LD_PRELOAD=" + line.split("=>")[1].strip() + " " + command
+                    )
                     break
             os.system(command)
-            
+
             os.chdir(curr_dir)
         else:
             raise Exception("Currently untested on non-Linux OS")
@@ -104,21 +124,23 @@ class VitisAcceleratorBackend(VitisBackend):
     def numpy_to_dat(self, model, x):
         if len(model.get_input_variables()) != 1:
             raise Exception("Currently unsupported for multi-input/output projects")
-        
+
         # Verify numpy array of correct shape
         expected_shape = model.get_input_variables()[0].size()
         actual_shape = np.prod(x.shape[1:])
         if expected_shape != actual_shape:
-            raise Exception(f'Input shape mismatch, got {x.shape}, expected (_, {expected_shape})')
-        
+            raise Exception(
+                f"Input shape mismatch, got {x.shape}, expected (_, {expected_shape})"
+            )
+
         # Write to tb_data/tb_input_features.dat
         samples = x.reshape(x.shape[0], -1)
-        input_dat = f'{model.config.get_output_dir()}/tb_data/tb_input_features.dat'
-        np.savetxt(input_dat, samples, fmt='%.4e')
+        input_dat = f"{model.config.get_output_dir()}/tb_data/tb_input_features.dat"
+        np.savetxt(input_dat, samples, fmt="%.4e")
 
     def dat_to_numpy(self, model):
         expected_shape = model.get_output_variables()[0].size()
-        output_file = f'{model.config.get_output_dir()}/tb_data/hw_results.dat'
+        output_file = f"{model.config.get_output_dir()}/tb_data/hw_results.dat"
         y = np.loadtxt(output_file, dtype=float).reshape(-1, expected_shape)
         return y
 
@@ -134,21 +156,37 @@ class VitisAcceleratorBackend(VitisBackend):
 
     def _register_flows(self):
         validation_passes = [
-            'vitisaccelerator:validate_conv_implementation',
-            'vitisaccelerator:validate_strategy',
+            "vitisaccelerator:validate_conv_implementation",
+            "vitisaccelerator:validate_strategy",
         ]
-        validation_flow = register_flow('validation', validation_passes, requires=['vivado:init_layers'], backend=self.name)
+        validation_flow = register_flow(
+            "validation",
+            validation_passes,
+            requires=["vivado:init_layers"],
+            backend=self.name,
+        )
 
         # Any potential templates registered specifically for Vitis backend
         template_flow = register_flow(
-            'apply_templates', self._get_layer_templates, requires=['vivado:init_layers'], backend=self.name
+            "apply_templates",
+            self._get_layer_templates,
+            requires=["vivado:init_layers"],
+            backend=self.name,
         )
 
-        writer_passes = ['make_stamp', 'vitisaccelerator:write_hls']
-        self._writer_flow = register_flow('write', writer_passes, requires=['vitis:ip'], backend=self.name)
+        writer_passes = ["make_stamp", "vitisaccelerator:write_hls"]
+        self._writer_flow = register_flow(
+            "write", writer_passes, requires=["vitis:ip"], backend=self.name
+        )
 
-        ip_flow_requirements = get_flow('vivado:ip').requires.copy()
-        ip_flow_requirements.insert(ip_flow_requirements.index('vivado:init_layers'), validation_flow)
-        ip_flow_requirements.insert(ip_flow_requirements.index('vivado:apply_templates'), template_flow)
+        ip_flow_requirements = get_flow("vivado:ip").requires.copy()
+        ip_flow_requirements.insert(
+            ip_flow_requirements.index("vivado:init_layers"), validation_flow
+        )
+        ip_flow_requirements.insert(
+            ip_flow_requirements.index("vivado:apply_templates"), template_flow
+        )
 
-        self._default_flow = register_flow('ip', None, requires=ip_flow_requirements, backend=self.name)
+        self._default_flow = register_flow(
+            "ip", None, requires=ip_flow_requirements, backend=self.name
+        )
--- hls4ml/backends/vitis_accelerator/vitis_accelerator_config.py
+++ hls4ml/backends/vitis_accelerator/vitis_accelerator_config.py
@@ -2,63 +2,66 @@
 import json
 import os
 
+
 class VitisAcceleratorConfig:
     def __init__(self, config):
         self.config = config.config
-        accel_config = self.config.get('AcceleratorConfig', None)
+        accel_config = self.config.get("AcceleratorConfig", None)
         if accel_config is None:
-             raise Exception('Missing AcceleratorConfig')
-        
-        self.board = accel_config.get('Board', 'alveo-u55c')
-        self.supported_boards = json.load(open(os.path.dirname(__file__) + '/supported_boards.json'))
+            raise Exception("Missing AcceleratorConfig")
+
+        self.board = accel_config.get("Board", "alveo-u55c")
+        self.supported_boards = json.load(
+            open(os.path.dirname(__file__) + "/supported_boards.json")
+        )
         if self.board in self.supported_boards.keys():
             board_info = self.supported_boards[self.board]
-            self.board_type = board_info['board_type']
-            self.part = board_info['part']
-            self.platform = board_info['platform']
-            self.memory_type = board_info['memory']['type']
-            self.memory_channel_count = board_info['memory']['channels']
+            self.board_type = board_info["board_type"]
+            self.part = board_info["part"]
+            self.platform = board_info["platform"]
+            self.memory_type = board_info["memory"]["type"]
+            self.memory_channel_count = board_info["memory"]["channels"]
         else:
-            raise Exception('The board does not appear in supported_boards.json file')
+            raise Exception("The board does not appear in supported_boards.json file")
 
-        if self.config.get('Part') is not None:
-            if self.config.get('Part') != self.part:
+        if self.config.get("Part") is not None:
+            if self.config.get("Part") != self.part:
                 print(
-                    'WARNING: You set a Part that does not correspond to the Board you specified.' 
-                    'The correct Part is now set.'
+                    "WARNING: You set a Part that does not correspond to the Board you specified."
+                    "The correct Part is now set."
                 )
-                self.config['Part'] = self.part
-        
-        self.num_kernel = accel_config.get('Num_Kernel', 1)
-        self.num_thread = accel_config.get('Num_Thread', 1)
-        self.batchsize = accel_config.get('Batchsize', 8192)
-        self.hw_quant = accel_config.get('HW_Quant', False)
+                self.config["Part"] = self.part
 
-        self.vivado_directives = accel_config.get('Vivado_Directives', [])    
+        self.num_kernel = accel_config.get("Num_Kernel", 1)
+        self.num_thread = accel_config.get("Num_Thread", 1)
+        self.batchsize = accel_config.get("Batchsize", 8192)
+        self.hw_quant = accel_config.get("HW_Quant", False)
+
+        self.vivado_directives = accel_config.get("Vivado_Directives", [])
 
     def get_board_type(self):
         return self.board_type
 
     def get_platform(self):
         return self.platform
-    
+
     def get_num_thread(self):
         return self.num_thread
-    
+
     def get_num_kernel(self):
         return self.num_kernel
-    
+
     def get_batchsize(self):
         return self.batchsize
-    
+
     def get_memory_type(self):
         return self.memory_type
-    
+
     def get_memory_channel_count(self):
         return self.memory_channel_count
-    
+
     def get_hw_quant(self):
         return self.hw_quant
-    
+
     def get_vivado_directives(self):
         return self.vivado_directives
