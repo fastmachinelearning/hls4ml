421e7c456939dcd90ce1341b2614c20f6ec4235f
Giuseppe Di Guglielmo <gdg@fnal.gov>
2024-09-19 21:29:03 -0700

    [Bug fix] Set axi_master interface variable sizes

--- hls4ml/writer/vivado_accelerator_writer.py
+++ hls4ml/writer/vivado_accelerator_writer.py
@@ -139,10 +139,12 @@ class VivadoAcceleratorWriter(VivadoWriter):
                     newline = ''
                     newline += indent + '#pragma HLS INTERFACE s_axilite port=return bundle=CTRL_BUS\n'
                     newline += indent + '#pragma HLS INTERFACE m_axi depth={} port=in offset=slave bundle=IN_BUS\n'.format(
-                        model.get_input_variables()[0].pragma[1]
+                        #model.get_input_variables()[0].pragma[1]
+                        model.get_input_variables()[0].size()
                     )
                     newline += indent + '#pragma HLS INTERFACE m_axi depth={} port=out offset=slave bundle=OUT_BUS\n'.format(
-                        model.get_output_variables()[0].pragma[1]
+                        #model.get_output_variables()[0].pragma[1]
+                        model.get_output_variables()[0].size()
                     )
                 elif self.vivado_accelerator_config.get_interface() == 'axi_stream':
                     newline = ''
