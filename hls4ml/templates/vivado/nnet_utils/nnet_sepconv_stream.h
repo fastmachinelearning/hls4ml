d9cdb768b959c9752209be7f92173a5817077d57
stzelepi <stylianos.tzelepis@cern.ch>
2024-10-08 13:42:12 +0200

    Split stream depthwise resource in 3 cases

--- hls4ml/templates/vivado/nnet_utils/nnet_sepconv_stream.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_sepconv_stream.h
@@ -4,11 +4,249 @@
 #include "hls_stream.h"
 #include "nnet_common.h"
 #include "nnet_conv_stream.h"
+#include <iostream>
+#include <type_traits>
+
+template<int N>
+struct print_value;
+
+template<>
+struct print_value<true> {};
+
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
+template<int multiplier_limit, int nout, int rufactor, int n_chan, int nin>
+void check_values() {
+    // This will give a static_assert failure with values printed
+    static_assert((nin == multiplier_limit*rufactor), 
+                  "Static Assertion failed: multiplier_limit = " 
+                  TOSTRING(multiplier_limit) ", nout = " 
+                  TOSTRING(nout) ", n_chan = " 
+                  TOSTRING(n_chan) ", nin = " TOSTRING(nin));
+}
 
 namespace nnet {
 
 template <class data_T, class res_T, typename CONFIG_T>
-void depthwise_product(data_T data[CONFIG_T::kernel_size * CONFIG_T::n_chan], res_T res[CONFIG_T::n_chan],
+void depthwise_product_resource_rf_lt_nchan(data_T data[CONFIG_T::kernel_size * CONFIG_T::n_chan], res_T res[CONFIG_T::n_chan],
+                       typename CONFIG_T::weight_t weights[CONFIG_T::kernel_size * CONFIG_T::n_chan],
+                       typename CONFIG_T::bias_t biases[CONFIG_T::n_chan]) {
+                    
+    const int nin = CONFIG_T::kernel_size * CONFIG_T::n_chan;
+    const int nout = CONFIG_T::n_chan;
+    const int rufactor = MIN(CONFIG_T::reuse_factor, nin);
+    const int multfactor = MIN(nin, CONFIG_T::reuse_factor);
+    const int multiplier_limit = DIV_ROUNDUP(nin, multfactor);
+    const int block_factor = DIV_ROUNDUP(nin, CONFIG_T::reuse_factor);
+    // const int multscale = multiplier_limit;
+
+    std::cout << "LT filters = " <<  CONFIG_T::n_chan << " rf = " << CONFIG_T::reuse_factor << " Block factor = " << block_factor << "\n";
+    check_values<multiplier_limit, nout, rufactor, CONFIG_T::n_chan, nin>();
+    assert((multiplier_limit % nout == 0 || rufactor > CONFIG_T::n_chan) && "The current Reuse Factor is not allowed");
+    assert((multiplier_limit == block_factor) && "This function is correct only for RF <= N_IN");
+
+    #pragma HLS function_instantiate variable=weights,biases
+    //#pragma HLS RESOURCE variable=weights core=RAM_2P_BRAM Commenting out the deisgnation HLS seems to choose correctly
+    #pragma HLS ARRAY_RESHAPE   variable=weights block factor=block_factor
+    #pragma HLS ARRAY_RESHAPE   variable=data block factor=block_factor
+
+    #pragma HLS ARRAY_PARTITION variable=biases complete
+
+    typename CONFIG_T::accum_t acc[CONFIG_T::n_chan];
+    #pragma HLS ARRAY_PARTITION variable=acc type=complete
+    
+InitAccum:  
+    for (int iacc = 0; iacc < CONFIG_T::n_chan; iacc++) {
+        #pragma HLS UNROLL
+        acc[iacc] = (typename CONFIG_T::accum_t)biases[iacc];
+    }
+
+ReuseLoop:
+    for (int ir = 0; ir < rufactor; ir++) {
+        #pragma HLS PIPELINE II=1 rewind
+
+        int in_index = ir;
+        int out_index = ir;
+
+    MultLoop:
+        for (int im = 0; im < block_factor; im++) {
+            #pragma HLS UNROLL
+            
+            acc[out_index] += static_cast<typename CONFIG_T::accum_t>(CONFIG_T::mult_config::template product<data_T, typename CONFIG_T::mult_config::weight_t>::product(data[in_index], weights[in_index]));
+
+            in_index+=rufactor;
+            out_index+=rufactor;
+
+            if (out_index >= CONFIG_T::n_chan) {
+                out_index -= CONFIG_T::n_chan;
+            }
+            
+        }
+    }
+
+// Cast to "res_t" type
+Result:
+    for (int ires = 0; ires < nout; ires++) {
+        #pragma HLS UNROLL
+        res[ires] = cast<data_T, res_T, CONFIG_T>(acc[ires]);
+    }
+}
+
+template <class data_T, class res_T, typename CONFIG_T>
+void depthwise_product_resource_rf_geq_nchan_rem0(data_T data[CONFIG_T::kernel_size * CONFIG_T::n_chan], res_T res[CONFIG_T::n_chan],
+                       typename CONFIG_T::weight_t weights[CONFIG_T::kernel_size * CONFIG_T::n_chan],
+                       typename CONFIG_T::bias_t biases[CONFIG_T::n_chan]) {
+
+    const int nin = CONFIG_T::kernel_size * CONFIG_T::n_chan;
+    const int nout = CONFIG_T::n_chan;
+
+    const int rufactor = MIN(CONFIG_T::reuse_factor, nin);
+    const int multfactor = MIN(nin, CONFIG_T::reuse_factor);
+    const int multiplier_limit = DIV_ROUNDUP(nin, multfactor);
+    const int block_factor = DIV_ROUNDUP(nin, CONFIG_T::reuse_factor);
+    // const int multscale = multiplier_limit;
+
+    std::cout << "REM0 filters = " <<  CONFIG_T::n_chan << " rf = " << CONFIG_T::reuse_factor << "\n";
+
+    assert((multiplier_limit % nout == 0 || rufactor >= CONFIG_T::n_chan) && "The current Reuse Factor is not allowed");
+    assert((rufactor >= CONFIG_T::n_chan && rufactor % CONFIG_T::n_chan == 0) && "This function is correct only for RF >= N_IN && RF % N_IN == 0");
+
+    #pragma HLS function_instantiate variable=weights,biases
+    //#pragma HLS RESOURCE variable=weights core=RAM_2P_BRAM Commenting out the deisgnation HLS seems to choose correctly
+    #pragma HLS ARRAY_RESHAPE   variable=weights type=block factor=block_factor
+    #pragma HLS ARRAY_RESHAPE   variable=data type=block factor=block_factor
+
+    #pragma HLS ARRAY_PARTITION variable=biases complete
+
+    typename CONFIG_T::accum_t acc[CONFIG_T::n_chan];
+    #pragma HLS ARRAY_PARTITION variable=acc type=complete
+
+InitAccum:  
+    for (int iacc = 0; iacc < CONFIG_T::n_chan; iacc++) {
+        #pragma HLS UNROLL
+        acc[iacc] = (typename CONFIG_T::accum_t)biases[iacc];
+    }
+
+// int outidx[rufactor];
+// int outstep = 0;
+// IndexLoop:
+//     for (int ir = 0; ir < rufactor; ir++) {
+//         outidx[ir] = outstep;
+//         outstep++;
+//         if (outstep == CONFIG_T::n_chan) {
+//             outstep = 0;
+//         }
+//     }
+// int out_index = -1;
+
+ReuseLoop:
+    for (int ir = 0; ir < rufactor; ir++) {
+        #pragma HLS PIPELINE II=1 rewind
+
+        int in_index = ir;
+
+        // out_index = outidx[ir];
+        int out_index = ir % CONFIG_T::n_chan;
+
+    MultLoop:
+        for (int im = 0; im < block_factor; im++) {
+            #pragma HLS UNROLL
+            
+            acc[out_index] += static_cast<typename CONFIG_T::accum_t>(CONFIG_T::mult_config::template product<data_T, typename CONFIG_T::mult_config::weight_t>::product(data[in_index], weights[in_index]));
+
+            in_index+=rufactor;         
+        }
+    }
+
+// Cast to "res_t" type
+Result:
+    for (int ires = 0; ires < nout; ires++) {
+        #pragma HLS UNROLL
+        res[ires] = cast<data_T, res_T, CONFIG_T>(acc[ires]);
+    }
+}
+
+template <class data_T, class res_T, typename CONFIG_T>
+void depthwise_product_resource_rf_gt_nchan(data_T data[CONFIG_T::kernel_size * CONFIG_T::n_chan], res_T res[CONFIG_T::n_chan],
+                       typename CONFIG_T::weight_t weights[CONFIG_T::kernel_size * CONFIG_T::n_chan],
+                       typename CONFIG_T::bias_t biases[CONFIG_T::n_chan]) {
+
+    const int nin = CONFIG_T::kernel_size * CONFIG_T::n_chan;
+    const int nout = CONFIG_T::n_chan;
+
+    const int rufactor = MIN(CONFIG_T::reuse_factor, nin);
+    // const int multfactor = MIN(nin, CONFIG_T::reuse_factor);
+    // const int multiplier_limit = DIV_ROUNDUP(nin, multfactor);
+    const int block_factor = DIV_ROUNDUP(nin, CONFIG_T::reuse_factor);
+    // const int multscale = multiplier_limit;
+
+    std::cout << "GT filters = " <<  CONFIG_T::n_chan << " rf = " << CONFIG_T::reuse_factor << "\n";
+
+    // assert((multiplier_limit % nout == 0 || rufactor >= nin) && "The current Reuse Factor is not allowed");
+    assert((rufactor > CONFIG_T::n_chan) && "This function is correct only for RF > N_IN");
+
+    #pragma HLS function_instantiate variable=weights,biases
+    //#pragma HLS RESOURCE variable=weights core=RAM_2P_BRAM Commenting out the deisgnation HLS seems to choose correctly
+    #pragma HLS ARRAY_RESHAPE   variable=weights block factor=block_factor
+    #pragma HLS ARRAY_RESHAPE   variable=data block factor=block_factor
+
+    #pragma HLS ARRAY_PARTITION variable=biases complete
+
+    typename CONFIG_T::accum_t acc[CONFIG_T::n_chan];
+    #pragma HLS ARRAY_PARTITION variable=acc type=complete
+    
+InitAccum:  
+    for (int iacc = 0; iacc < CONFIG_T::n_chan; iacc++) {
+        #pragma HLS UNROLL
+        acc[iacc] = (typename CONFIG_T::accum_t)biases[iacc];
+    }
+
+const int remainder = CONFIG_T::reuse_factor % CONFIG_T::n_chan;
+
+int outidx[rufactor];
+int outstep = 0;
+IndexLoop:
+    for (int ir = 0; ir < rufactor; ir++) {
+        outidx[ir] = outstep;
+        outstep++;
+        if (outstep == CONFIG_T::n_chan) {
+            outstep = 0;
+        }
+    }
+
+ReuseLoop:
+    for (int ir = 0; ir < rufactor; ir++) {
+        #pragma HLS PIPELINE II=1 rewind
+
+        int in_index = ir;
+        int out_index = outidx[ir];
+        
+    MultLoop:
+        for (int im = 0; im < block_factor; im++) {
+            #pragma HLS UNROLL
+
+            // out_index = in_index % CONFIG_T::n_chan;
+            acc[out_index] += static_cast<typename CONFIG_T::accum_t>(CONFIG_T::mult_config::template product<data_T, typename CONFIG_T::mult_config::weight_t>::product(data[in_index], weights[in_index]));
+
+            in_index += rufactor;
+            out_index += remainder;
+            if (out_index >= CONFIG_T::n_chan) {
+                out_index -= CONFIG_T::n_chan;
+            }
+        }
+    }
+
+// Cast to "res_t" type
+Result:
+    for (int ires = 0; ires < nout; ires++) {
+        #pragma HLS UNROLL
+        res[ires] = cast<data_T, res_T, CONFIG_T>(acc[ires]);
+    }
+}
+
+template <class data_T, class res_T, typename CONFIG_T>
+void depthwise_product_latency(data_T data[CONFIG_T::kernel_size * CONFIG_T::n_chan], res_T res[CONFIG_T::n_chan],
                        typename CONFIG_T::weight_t weights[CONFIG_T::kernel_size * CONFIG_T::n_chan],
                        typename CONFIG_T::bias_t biases[CONFIG_T::n_chan]) {
     #pragma HLS INLINE
@@ -59,6 +297,22 @@ Result:
 }
 
 template <class data_T, class res_T, typename CONFIG_T>
+void depthwise_product_resource(data_T data[CONFIG_T::kernel_size * CONFIG_T::n_chan], res_T res[CONFIG_T::n_chan],
+                       typename CONFIG_T::weight_t weights[CONFIG_T::kernel_size * CONFIG_T::n_chan],
+                       typename CONFIG_T::bias_t biases[CONFIG_T::n_chan]) {
+
+    #pragma HLS INLINE recursive
+
+    if (CONFIG_T::reuse_factor < CONFIG_T::n_chan) {
+        depthwise_product_resource_rf_lt_nchan<data_T, res_T, CONFIG_T>(data, res, weights, biases);
+    } else if (CONFIG_T::reuse_factor % CONFIG_T::n_chan == 0) {
+        depthwise_product_resource_rf_geq_nchan_rem0<data_T, res_T, CONFIG_T>(data, res, weights, biases);
+    } else {
+        depthwise_product_resource_rf_gt_nchan<data_T, res_T, CONFIG_T>(data, res, weights, biases);
+    }
+}
+
+template <class data_T, class res_T, typename CONFIG_T>
 void depthwise_mult_buffer(hls::stream<typename data_T::value_type> data_window[CONFIG_T::kernel_size * CONFIG_T::n_chan],
                            res_T &res_pack, hls::stream<res_T> &res_stream, unsigned &outputs_ready,
                            typename CONFIG_T::weight_t weights[CONFIG_T::kernel_size * CONFIG_T::n_chan],
@@ -78,9 +332,9 @@ InitData:
 
     #pragma HLS INLINE recursive
     if (CONFIG_T::strategy == nnet::latency) {
-        depthwise_product<typename data_T::value_type, typename res_T::value_type, CONFIG_T>(data, res, weights, biases);
+        depthwise_product_latency<typename data_T::value_type, typename res_T::value_type, CONFIG_T>(data, res, weights, biases);
     } else {
-        assert("Resource strategy for DepthwiseConv2D is not supported." && false);
+        depthwise_product_resource<typename data_T::value_type, typename res_T::value_type, CONFIG_T>(data, res, weights, biases);
     }
 
 CastLoop:
@@ -202,10 +456,11 @@ void compute_depthwise_output_buffer_1d(const data_T &in_elem, hls::stream<res_T
         // Dense multiply
         #pragma HLS INLINE recursive
         if (CONFIG_T::strategy == nnet::latency) {
-            depthwise_product<typename data_T::value_type, typename res_T::value_type, CONFIG_T>(kernel_data, res_out,
+            depthwise_product_latency<typename data_T::value_type, typename res_T::value_type, CONFIG_T>(kernel_data, res_out,
                                                                                                  weights, biases);
         } else {
-            assert("Resource strategy for DepthwiseConv1D is not supported." && false);
+            depthwise_product_resource<typename data_T::value_type, typename res_T::value_type, CONFIG_T>(kernel_data, res_out,
+                                                                                           weights, biases);
         }
 
     // Pack output
@@ -267,10 +522,11 @@ void compute_depthwise_output_buffer_2d(const data_T &in_elem,
         // Dense multiply
         #pragma HLS INLINE recursive
         if (CONFIG_T::strategy == nnet::latency) {
-            depthwise_product<typename data_T::value_type, typename res_T::value_type, CONFIG_T>(kernel_data, res_out,
+            depthwise_product_latency<typename data_T::value_type, typename res_T::value_type, CONFIG_T>(kernel_data, res_out,
                                                                                                  weights, biases);
         } else {
-            assert("Resource strategy for DepthwiseConv2D is not supported." && false);
+            depthwise_product_resource<typename data_T::value_type, typename res_T::value_type, CONFIG_T>(kernel_data, res_out,
+                                                                                                 weights, biases);
         }
 
     // Pack output
