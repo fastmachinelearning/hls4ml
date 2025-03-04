change default precision to fixed<32,16> throughout

test/pytest/test_recurrent_pytorch.py
@@ -43,7 +43,11 @@ def test_gru(backend, io_type):
     pytorch_prediction = model(torch.Tensor(X_input), torch.Tensor(h0)).detach().numpy()
 
     config = config_from_pytorch_model(
-        model, [(None, 1, 10), (None, 1, 20)], channels_last_conversion="off", transpose_outputs=False
+        model,
+        [(None, 1, 10), (None, 1, 20)],
+        channels_last_conversion="off",
+        transpose_outputs=False,
+        default_precision='fixed<32,16>',
     )
     output_dir = str(test_root_path / f'hls4mlprj_pytorch_api_gru_{backend}_{io_type}')
 
@@ -66,10 +70,14 @@ def test_gru_stream(backend, io_type):
 
     pytorch_prediction = model(torch.Tensor(X_input)).detach().numpy()
 
-    config = config_from_pytorch_model(model, (None, 1, 10), channels_last_conversion="off", transpose_outputs=False)
+    config = config_from_pytorch_model(
+        model, (None, 1, 10), channels_last_conversion="off", transpose_outputs=False, default_precision='fixed<32,16>'
+    )
     output_dir = str(test_root_path / f'hls4mlprj_pytorch_api_gru_{backend}_{io_type}')
 
-    hls_model = convert_from_pytorch_model(model, hls_config=config, output_dir=output_dir, backend=backend, io_type=io_type)
+    hls_model = convert_from_pytorch_model(
+        model, hls_config=config, output_dir=output_dir, backend=backend, io_type=io_type, default_precision='fixed<32,16>'
+    )
 
     hls_model.compile()
 
@@ -111,7 +119,11 @@ def test_lstm(backend, io_type):
     pytorch_prediction = model(torch.Tensor(X_input), torch.Tensor(h0), torch.tensor(c0)).detach().numpy()
 
     config = config_from_pytorch_model(
-        model, [(None, 1, 10), (None, 1, 20), (None, 1, 20)], channels_last_conversion="off", transpose_outputs=False
+        model,
+        [(None, 1, 10), (None, 1, 20), (None, 1, 20)],
+        channels_last_conversion="off",
+        transpose_outputs=False,
+        default_precision='fixed<32,16>',
     )
     output_dir = str(test_root_path / f'hls4mlprj_pytorch_api_lstm_{backend}_{io_type}')
 
@@ -143,7 +155,9 @@ def test_lstm_stream(backend, io_type):
 
         pytorch_prediction = model(torch.Tensor(X_input)).detach().numpy()
 
-        config = config_from_pytorch_model(model, [(None, 1, 10)], channels_last_conversion="off", transpose_outputs=False)
+        config = config_from_pytorch_model(
+            model, [(None, 1, 10)], channels_last_conversion="off", transpose_outputs=False, default_precision='fixed<32,16>'
+        )
         output_dir = str(test_root_path / f'hls4mlprj_pytorch_api_lstm_{backend}_{io_type}')
 
         hls_model = convert_from_pytorch_model(
@@ -184,12 +198,21 @@ def test_rnn(backend, io_type):
         pytorch_prediction = model(torch.Tensor(X_input), torch.Tensor(h0)).detach().numpy()
 
         config = config_from_pytorch_model(
-            model, [(1, 10), (1, 20)], channels_last_conversion="off", transpose_outputs=False
+            model,
+            [(1, 10), (1, 20)],
+            channels_last_conversion="off",
+            transpose_outputs=False,
+            default_precision='fixed<32,16>',
         )
         output_dir = str(test_root_path / f'hls4mlprj_pytorch_api_rnn_{backend}_{io_type}')
 
         hls_model = convert_from_pytorch_model(
-            model, hls_config=config, output_dir=output_dir, backend=backend, io_type=io_type
+            model,
+            hls_config=config,
+            output_dir=output_dir,
+            backend=backend,
+            io_type=io_type,
+            default_precision='fixed<32,16>',
         )
 
         hls_model.compile()