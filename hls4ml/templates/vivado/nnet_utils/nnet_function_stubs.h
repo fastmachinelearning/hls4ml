c3f70851f20466511b1191add54a45e84e72ebe2
Chang Sun <chsun@cern.ch>
2025-03-07 16:40:17 -0800

    distributed arithmetic templates

--- hls4ml/templates/vivado/nnet_utils/nnet_function_stubs.h
+++ hls4ml/templates/vivado/nnet_utils/nnet_function_stubs.h
@@ -55,6 +55,35 @@ template <class data_T, class res_T, typename CONFIG_T> class Conv1DKernel {
     }
 };
 
+template <int s, int b, int i, ap_q_mode Q, ap_o_mode O, int N> ap_fixed<b, i + s> bit_shift(ap_fixed<b, i, Q, O, N> x) {
+    #pragma HLS INLINE
+    ap_fixed<b, i + s> r;
+    r.range() = x.range();
+    return r;
+};
+
+template <int s, int b, int i, ap_q_mode Q, ap_o_mode O, int N> ap_ufixed<b, i + s> bit_shift(ap_ufixed<b, i, Q, O, N> x) {
+    #pragma HLS INLINE
+    ap_ufixed<b, i + s> r;
+    r.range() = x.range();
+    return r;
+};
+
+template <int s, int b> ap_fixed<b, s> bit_shift(ap_int<b> x) {
+    #pragma HLS INLINE
+    ap_fixed<b, s> r;
+    r.range() = x.range();
+    return r;
+};
+
+template <int s, int b> ap_ufixed<b, s> bit_shift(ap_uint<b> x) {
+    #pragma HLS INLINE
+    ap_ufixed<b, s> r;
+    r.range() = x.range();
+    return r;
+};
+// hls4ml insert code
+
 } // namespace nnet
 
 #endif
