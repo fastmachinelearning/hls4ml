softmax and activation fix

hls4ml/converters/keras_v3/hgq2/unary_lut.py
@@ -2,7 +2,7 @@ import typing
 from typing import Sequence
 
 import numpy as np
-from quantizers import float_quantize, get_fixed_quantizer
+from quantizers import float_quantize, get_fixed_quantizer_np
 
 from hls4ml.model.types import FixedPrecisionType
 
@@ -35,35 +35,31 @@ class SQUnaryLUTHandler(SQLayerHandler, KerasV3LayerHandler):
         assert not layer._allow_heterogeneous_table, 'Heterogeneous table is not supported in QUnaryFunctionLUT layer'
 
         iq = layer.iq.quantizer
-        _min = Decimal(float(ops.min(iq.min)))  # type: ignore
-        _max = Decimal(float(ops.max(iq.max)))  # type: ignore
-        _eps = Decimal(float(ops.min(iq.epsilon)))  # type: ignore
-        N = (_max - _min) / _eps + 1
-        assert float(N).is_integer(), 'Invalid quantizer range'
-        N = int(N)
-        assert N <= 1e6, 'Too large quantizer range'
-        assert np.log2(N).is_integer(), f'Invalid quantizer range: N must be power of 2, got {N}'
-
-        all_inputs = ops.linspace(float(_min), float(_max), N)
-
-        config = {}
-        config.update(self.default_config)
-
         if isinstance(iq, FixedPointQuantizerBase):
-            table = ops.convert_to_numpy(layer.activation(all_inputs))
-            if _min < 0:
+            k, i, f = (Decimal(int(ops.max(x))) for x in iq.kif)
+            _min = -k * 2**i
+            _eps = 2**-f
+            _max = 2**i - _eps
+            N = (_max - _min) / _eps + 1
+            assert float(N).is_integer(), 'Invalid quantizer range'
+            N = int(N)
+            assert N <= 1e6, 'Too large quantizer range'
+            assert np.log2(N).is_integer(), f'Invalid quantizer range: N must be power of 2, got {N}'
+
+            all_inputs = np.linspace(float(_min), float(_max), N, dtype=np.float32)
+
+            config = {}
+            config.update(self.default_config)
+            table = layer.activation(all_inputs)
+            if layer.enable_oq:
+                table = layer.oq(table[None, ...])[0]
+            table = ops.convert_to_numpy(table)
+            if k:
                 # idx by binary repr, move the positive part to the front
                 table_pos, table_neg = table[N // 2 :], table[: N // 2]
                 table = np.concatenate([table_pos, table_neg])
         else:
-            assert isinstance(iq, FloatPointQuantizer), f'{layer.name}: Unknown quantizer class {type(iq)}'
-            mee0 = (ops.convert_to_numpy(x) for x in (iq.m, iq.e, iq.e0))
-            assert all(
-                x.size == 1 for x in mee0
-            ), f'{layer.name}: Only homogeneous input quantizer is supported for minifloat'
-            m, e, e0 = (int(x.ravel().item()) for x in mee0)
-            all_inputs = float_quantize(all_inputs, m, e, e0)
-            table = ops.convert_to_numpy(layer.activation(all_inputs))
+            raise NotImplementedError('FloatPointQuantizer is not supported yet')
 
         oq = layer.oq.quantizer
         if isinstance(oq, FixedPointQuantizerBase):
@@ -71,7 +67,7 @@ class SQUnaryLUTHandler(SQLayerHandler, KerasV3LayerHandler):
             if round_mode.startswith('S_'):
                 round_mode = round_mode[2:]
             overflow_mode = oq.overflow_mode
-            fixed_q = get_fixed_quantizer(round_mode, overflow_mode)
+            fixed_q = get_fixed_quantizer_np(round_mode, overflow_mode)
             k, i, f = (ops.convert_to_numpy(x).ravel().item() for x in oq.kif)
             table = fixed_q(table, k, i, f)  # type: ignore
 

hls4ml/model/optimizer/passes/bit_exact.py
@@ -492,7 +492,8 @@ def _(layer: Softmax):
     i_exp, f_exp = I_exp, b_exp - I_exp
     i_inv, f_inv = I_inv, b_inv - I_inv
     k = np.zeros(out_shape, dtype=np.int8)
-    i = np.full(out_shape, min(i_exp + i_inv, 1), dtype=np.int8)
+
+    i = np.full(out_shape, i_exp + i_inv, dtype=np.int8)
     f = np.full(out_shape, f_exp + f_inv, dtype=np.int8)
 
     return k, i, f
@@ -653,18 +654,17 @@ def _(node: Softmax):
     inv_inp_t: FixedPrecisionType = node.attributes['inv_inp_t'].precision
     exp_table_t: FixedPrecisionType = node.attributes['exp_table_t'].precision
     accum_t = copy(inv_inp_t)
-    n_in = node.attributes['n_in']
-    scale = ceil(log2(n_in))
+    n_slice = node.attributes['n_in'] // node.attributes.get('n_inner', 1) // node.attributes.get('n_outer', 1)
+    scale = ceil(log2(n_slice))
     f_exp = exp_table_t.width - exp_table_t.integer
     f = f_exp + scale
     accum_t.width = f + inv_inp_t.integer
     accum_t.width += scale
     if inv_inp_t.saturation_mode != SaturationMode.WRAP:
         accum_t.saturation_mode = SaturationMode.WRAP
-        n_in = node.attributes['n_in']
-        scale = ceil(log2(n_in))
         accum_t.width += scale
         accum_t.integer += scale
+    accum_t.width = max(accum_t.width, 1)  # Prevent crashes on absurd bw configurations
     accum_t.rounding_mode = RoundingMode.TRN
     default_register_precision(node)
     impl = node.attributes['implementation']
@@ -776,10 +776,14 @@ class FixInputPrecision(OptimizerPass):
             return False
 
         # Unhandled input precision, usually by a heterogeneous quantizer with non-WRAP saturation
-        return node.get_output_variable().type.precision.width > 120
+        return node.get_output_variable().type.precision.width > 100
 
     def transform(self, model, node: Layer):
-        out_layers: list[FixedPointQuantizer] = get_output_layers(node)
+        out_layers: list[FixedPointQuantizer] = get_output_layers(node)  # type: ignore
+        for layer in out_layers:
+            assert isinstance(
+                layer, FixedPointQuantizer
+            ), f'Input {node.name} connected to non-quantizer {layer.name} with non-trivial configuration'
 
         if len(out_layers) == 0:  # Input connected to nothing
             new_type = to_hls4ml_fixed(0, 0, 1, f'{node.name}_t')
@@ -793,16 +797,30 @@ class FixInputPrecision(OptimizerPass):
 
         sat_modes = [l.SAT for l in out_layers]
         sat_modes_set = set(sat_modes)
+        rnd_modes = [l.RND for l in out_layers]
+        rnd_modes_set = set(rnd_modes)
         illegal_sat_modes = sat_modes_set - {'WRAP', 'SAT', 'SAT_SYM'}
+        illegal_rnd_modes = rnd_modes_set - {'TRN', 'RND'}
         if illegal_sat_modes:
-            raise ValueError(f'Input {node.name} has quantizer with illegal saturation mode {illegal_sat_modes} after.')
+            warn(f'Saturation mode {illegal_sat_modes} may compromise bit-exactness.')
+        if len(sat_modes_set) > 1 and not all('SAT' in s for s in sat_modes):
+            warn(f'Inconsistent saturation modes {sat_modes_set} in the input quantizers may compromise bit-exactness.')
+        if illegal_rnd_modes:
+            warn(f'Saturation mode {illegal_rnd_modes} may compromise bit-exactness. Forcing at maximum 24 fractional bits.')
 
         kifs = [_produce_kif(l) for l in out_layers]
         i = np.max([np.max(i) for _, i, _ in kifs])
         k = np.max([np.max(k) for k, _, _ in kifs])
-        f = node.get_output_variable().type.precision.fractional
+        if illegal_rnd_modes:
+            f = 24
+        else:
+            f = node.get_output_variable().type.precision.fractional
         new_type = to_hls4ml_fixed(k, i, f, f'{node.name}_t')
-        new_type.precision.saturation_mode = 'SAT'
+        if not all('SAT' in s for s in sat_modes):
+            # If any of the quantizers are not in SAT mode, set the input to WRAP mode
+            new_type.precision.saturation_mode = 'WRAP'
+        else:
+            new_type.precision.saturation_mode = 'SAT'
         node.get_output_variable().type = new_type
         node.model.config.layer_name_precision[node.name] = str(new_type)
         return False