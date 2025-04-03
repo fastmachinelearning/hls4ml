f97506627266903177397f8df28697b7e4733d18
Enrico Lupi <enlupi@olhsw-01.cern.ch>
2025-04-03 10:17:51 +0200

    ADD parsing for bidirectional RNN layers

--- hls4ml/converters/keras/recurrent.py
+++ hls4ml/converters/keras/recurrent.py
@@ -9,6 +9,7 @@ from hls4ml.converters.keras_v2_to_hls import (
     parse_default_keras_layer,
     parse_keras_model,
 )
+import numpy as np
 
 rnn_layers = ['SimpleRNN', 'LSTM', 'GRU']
 
@@ -18,6 +19,7 @@ def parse_rnn_layer(keras_layer, input_names, input_shapes, data_reader):
     assert keras_layer['class_name'] in rnn_layers or keras_layer['class_name'][1:] in rnn_layers
 
     layer = parse_default_keras_layer(keras_layer, input_names)
+    layer['direction'] = 'forward'
 
     layer['return_sequences'] = keras_layer['config']['return_sequences']
     layer['return_state'] = keras_layer['config']['return_state']
@@ -108,5 +110,70 @@ def parse_time_distributed_layer(keras_layer, input_names, input_shapes, data_re
     output_shape[len(layer_output_shape) - 1 :] = layer_output_shape
     layer['output_shape'] = output_shape[1:]  # Remove the batch dimension
     layer['n_time_steps'] = output_shape[1]
+
+    
+@keras_handler('Bidirectional')
+def parse_bidirectional_layer(keras_layer, input_names, input_shapes, data_reader):
+    assert keras_layer['class_name'] == 'Bidirectional'
+    
+    rnn_layer = keras_layer['config']['layer']
+    assert rnn_layer['class_name'] in rnn_layers or rnn_layer['class_name'][1:] in rnn_layers
+
+    layer = parse_default_keras_layer(rnn_layer, input_names)
+    layer['name'] = keras_layer['config']['name']
+    layer['direction'] = 'bidirectional'
+
+    layer['return_sequences'] = rnn_layer['config']['return_sequences']
+    layer['return_state'] = rnn_layer['config']['return_state']
+
+    if 'SimpleRNN' not in layer['class_name']:
+        layer['recurrent_activation'] = rnn_layer['config']['recurrent_activation']
+
+    layer['time_major'] = rnn_layer['config']['time_major'] if 'time_major' in rnn_layer['config'] else False
+
+    # TODO Should we handle time_major?
+    if layer['time_major']:
+        raise Exception('Time-major format is not supported by hls4ml')
+
+    layer['n_timesteps'] = input_shapes[0][1]
+    layer['n_in'] = input_shapes[0][2]
+
+    layer['n_out'] = 2*rnn_layer['config']['units']
+
+
+    if 'SimpleRNN' in layer['class_name']:
+        cell_name = 'simple_rnn'
+    else:
+        cell_name = layer['class_name'].lower()
+    weight_data_f, recurrent_weight_data_f, bias_data_f = get_weights_data(
+        data_reader, layer['name'], [f'forward_{cell_name}/{cell_name}_cell/kernel',
+                                     f'forward_{cell_name}/{cell_name}_cell/recurrent_kernel', 
+                                     f'forward_{cell_name}/{cell_name}_cell/bias']
+    )
+    weight_data_b, recurrent_weight_data_b, bias_data_b = get_weights_data(
+        data_reader, layer['name'], [f'backward_{cell_name}/{cell_name}_cell/kernel',
+                                     f'backward_{cell_name}/{cell_name}_cell/recurrent_kernel', 
+                                     f'backward_{cell_name}/{cell_name}_cell/bias']
+    )
+    layer['weight_data'] = np.stack((weight_data_f, weight_data_b), axis=0)
+    layer['recurrent_weight_data'] = np.stack((recurrent_weight_data_f, recurrent_weight_data_b), axis=0)
+    layer['bias_data'] = np.stack((bias_data_f, bias_data_b), axis=0)
+
+    if 'GRU' in layer['class_name']:
+        layer['apply_reset_gate'] = 'after' if rnn_layer['config']['reset_after'] else 'before'
+
+        # biases array is actually a 2-dim array of arrays (bias + recurrent bias)
+        # both arrays have shape: n_units * 3 (z, r, h_cand)
+        biases = layer['bias_data']
+        layer['bias_data'] = biases[0]
+        layer['recurrent_bias_data'] = biases[1]
+
+    if layer['return_sequences']:
+        output_shape = [input_shapes[0][0], layer['n_timesteps'], layer['n_out']]
+    else:
+        output_shape = [input_shapes[0][0], layer['n_out']]
+
+    if layer['return_state']:
+        raise Exception('"return_state" of {} layer is not yet supported.')
 
     return layer, output_shape
