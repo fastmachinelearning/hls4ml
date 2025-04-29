ef501c1ccd7ccdb1e130f009155ac4dc2f799504
Giuseppe Di Guglielmo <gdg@fnal.gov>
2025-04-28 19:26:24 -0700

    Add accumulator precision when plotting the model information

---
+++ test/pytest/test_plot_model.py
@@ -0,0 +1,61 @@
+import hls4ml
+
+import os
+import pytest
+import logging
+import warnings
+from pathlib import Path
+from hls4ml.utils.plot import plot_model
+from tensorflow.keras.models import model_from_json
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+test_root_path = Path(__file__).parent
+example_model_path = (test_root_path / '../../example-models').resolve()
+
+#co = {}
+#_add_supported_quantized_objects(co)
+
+@pytest.fixture(scope='module')
+def load_jettagging_model():
+    model_path = example_model_path / 'keras/KERAS_3layer.json'
+    with model_path.open('r') as f:
+        jsons = f.read()
+    model = model_from_json(jsons)
+    model.load_weights(example_model_path / 'keras/KERAS_3layer_weights.h5')
+    return model
+
+@pytest.fixture(scope='module')
+def convert(load_jettagging_model):
+    model = load_jettagging_model
+    config = hls4ml.utils.config_from_keras_model(model, granularity='name', backend='vitis')
+    hls_model = hls4ml.converters.convert_from_keras_model(
+        model,
+        hls_config=config,
+        output_dir=str(test_root_path / f'hls4mlprj_qkeras'),
+        part='xcu250-figd2104-2L-e',
+    )
+    hls_model.compile()
+    return hls_model
+
+def test_plot_model(convert):
+    # Test the plot_model function with a sample model.
+    output_file = "model_plot.png"
+    hls_model = convert
+
+    plot_model(
+        model=hls_model,
+        to_file=str(output_file),
+        show_shapes=True,
+        show_layer_names=True,
+        show_precision=True,
+        rankdir='TB',
+        dpi=96,
+    )
+
+    # Assert that the output file was created and is not empty
+    assert os.path.exists(output_file), f"Plot file was not created: {output_file}"
+    assert os.path.getsize(output_file) > 0, f"Plot file is empty: {output_file}"
+
+    logger.info(f"Plot file generated at: {os.path.abspath(output_file)}")
