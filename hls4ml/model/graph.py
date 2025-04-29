42c8183b92906256b4c31a8d16495ff11e20a983
Vladimir Loncar <vloncar@users.noreply.github.com>
2025-04-29 22:30:56 +0200

    Support for pseudo-deserialization from existing projects

--- hls4ml/model/graph.py
+++ hls4ml/model/graph.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 
 import numpy as np
 import numpy.ctypeslib as npc
+import yaml
 
 from hls4ml.backends import get_backend
 from hls4ml.model.flow import get_flow
@@ -443,6 +444,13 @@ class ModelGraph(Serializable):
         model._make_graph(layer_list)
         for flow in model.config.flows:
             model.apply_flow(flow)
+
+        model.config.config['InputShapes'] = {}
+        for input_var in model.get_input_variables():
+            model.config.config['InputShapes'][input_var.name] = list(input_var.shape)
+        model.config.config['OutputShapes'] = {}
+        for output_var in model.get_output_variables():
+            model.config.config['OutputShapes'][output_var.name] = list(output_var.shape)
 
         return model
 
@@ -1009,3 +1017,68 @@ class ModelGraph(Serializable):
         raise Exception(
             f'{cls.__name__} is not intended to be deserialized directly. Use {cls.__name__}.from_saved_state instead.'
         )
+
+
+class FilesystemModelGraph(ModelGraph):
+    """
+    A subclass of ModelGraph that can link with an existing project in the filesystem.
+
+    This allows the user to call `compile()`, `predict()` and `build()` functions.
+    All other methods are disabled and will raise an exception if accessed.
+    """
+
+    def __init__(self, project_dir):
+        if not os.path.exists(project_dir + '/hls4ml_config.yml'):
+            raise Exception(f'Cannot find hls4ml_config.yml in the directory {project_dir}.')
+
+        self._allowed_methods = {'compile', 'predict', 'build', 'get_input_variables', 'get_output_variables'}
+
+        yaml.add_multi_constructor('!keras_model', lambda loader, suffix, node: None, Loader=yaml.SafeLoader)
+        config = yaml.safe_load(open(project_dir + '/hls4ml_config.yml'))
+
+        self.in_vars = []
+        self.out_vars = []
+        for var_name, var_shape in config['InputShapes'].items():
+            var = self.VariableWrapper(var_name, var_shape)
+            self.in_vars.append(var)
+        for var_name, var_shape in config['OutputShapes'].items():
+            var = self.VariableWrapper(var_name, var_shape)
+            self.out_vars.append(var)
+
+        self.config = HLSConfig(config)
+        self._top_function_lib = None
+
+    def __getattribute__(self, name):
+        # Allow access to private attributes and explicitly allowed methods
+        if name.startswith('_') or name in object.__getattribute__(self, '_allowed_methods'):
+            return object.__getattribute__(self, name)
+        # Raise an exception for all other methods
+        if name in dir(ModelGraph):
+            raise Exception(f'The method "{name}" should not be invoked on FilesystemModelGraph.')
+        return object.__getattribute__(self, name)
+
+    def get_input_variables(self):
+        return self.in_vars
+
+    def get_output_variables(self):
+        return self.out_vars
+
+    def compile(self):
+        return super()._compile()
+
+    def predict(self, x):
+        return super().predict(x)
+
+    def build(self, **kwargs):
+        return self.config.backend.build(self, **kwargs)
+
+    class VariableWrapper:
+        def __init__(self, name, shape):
+            self.name = name
+            self.shape = shape
+
+        def size(self):
+            nelem = 1
+            for dim in self.shape:
+                nelem *= dim
+            return nelem
