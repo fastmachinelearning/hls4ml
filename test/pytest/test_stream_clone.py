69f483f56824415b639f514c0a9b8a3b3765583b
Vladimir Loncar <vloncar@users.noreply.github.com>
2025-05-14 20:42:21 +0200

    Support cloning up to 6 times

--- test/pytest/test_stream_clone.py
+++ test/pytest/test_stream_clone.py
@@ -11,7 +11,7 @@ test_root_path = Path(__file__).parent
 
 
 @pytest.fixture(scope='module')
-def model_clone_precision_inherition():
+def model_clone_precision_inheritance():
     inp = keras.Input(shape=(10,), name='inp')
     x = Dense(10, name='x')(inp)
     y = Dense(10, name='y')(inp)
@@ -22,21 +22,42 @@ def model_clone_precision_inherition():
 
 @pytest.fixture(scope='module')
 def model_multi_clone():
+    # inp will be cloned 6 times
+    # a will be cloned 5 times
+    # ab will be cloned 4 times
+    # abac will be cloned 3 times
+    # abacabad will be cloned 2 times
+
     inp = keras.Input(shape=(10,))
-    x = Dense(10)(inp)
-    y = Dense(10)(inp)
-    z = Dense(10)(inp)
-    xy = Add()([x, y])
-    xy = Add()([xy, y])
-    out = Add()([xy, z])
+    a = Dense(10, name='dense_5_copies')(inp)
+    b = Dense(10)(inp)
+    c = Dense(10)(inp)
+    d = Dense(10)(inp)
+    e = Dense(10)(inp)
+    f = Dense(10)(inp)
+    ab = Add(name='add_4_copies')([a, b])
+    ac = Add()([a, c])
+    ad = Add()([a, d])
+    ae = Add()([a, e])
+    af = Add()([a, f])
+    abac = Add(name='add_3_copies')([ab, ac])
+    abad = Add()([ab, ad])
+    abae = Add()([ab, ae])
+    abaf = Add()([ab, af])
+    abacabad = Add(name='add_2_copies')([abac, abad])
+    abacabae = Add()([abac, abae])
+    abacabaf = Add()([abac, abaf])
+    abacabadabacabae = Add()([abacabad, abacabae])
+    abacabadabacabaf = Add()([abacabad, abacabaf])
+    out = Add()([abacabadabacabae, abacabadabacabaf])
     model = keras.Model(inp, out)
     return model
 
 
 @pytest.fixture(scope='module')
 def data():
-    X = np.random.normal(0, 1, (1000, 10))
-    X = np.clip(X, -16, 15)
+    X = np.random.normal(0, 0.2, (100, 10))
+    X = np.clip(X, -2, 2)
     return X
 
 
@@ -59,7 +80,7 @@ def test_multi_clone(model_multi_clone, data, backend: str):
 
 
 @pytest.mark.parametrize('backend', ['Vivado', 'Quartus', 'Vitis', 'oneAPI'])
-def test_clone_precision_inherition(model_clone_precision_inherition, data, backend: str):
+def test_clone_precision_inheritance(model_clone_precision_inheritance, data, backend: str):
     output_dir = str(test_root_path / f'hls4mlprj_stream_clone_precision_{backend}')
     layer_config = {
         'inp': {'Precision': 'fixed<32,5>'},
@@ -71,7 +92,7 @@ def test_clone_precision_inherition(model_clone_precision_inherition, data, back
     }
     hls_config = {'Model': {'Precision': 'fixed<1,0>', 'ReuseFactor': 1}, 'LayerName': layer_config}
     model_hls = convert_from_keras_model(
-        model_clone_precision_inherition,
+        model_clone_precision_inheritance,
         backend=backend,
         output_dir=output_dir,
         hls_config=hls_config,
@@ -84,10 +105,10 @@ def test_clone_precision_inherition(model_clone_precision_inherition, data, back
 
     model_hls.compile()
     r_hls = model_hls.predict(data)
-    r_keras = model_clone_precision_inherition(data).numpy()
+    r_keras = model_clone_precision_inheritance(data).numpy()
 
     assert np.allclose(r_hls, r_keras, atol=1e-5, rtol=0)
 
 
 if __name__ == '__main__':
-    test_clone_precision_inherition(model_clone_precision_inherition(), data(), 'Vivado')
+    test_clone_precision_inheritance(model_clone_precision_inheritance(), data(), 'Vivado')
