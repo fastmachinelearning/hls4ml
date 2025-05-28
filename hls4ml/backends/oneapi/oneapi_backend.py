c640c468312535437dffb06e7e3494948893daa5
Chang Sun <chsun@cern.ch>
2025-05-28 06:22:14 -0700

    skip oneapi test if icpx doesnt exist

--- hls4ml/backends/oneapi/oneapi_backend.py
+++ hls4ml/backends/oneapi/oneapi_backend.py
@@ -175,6 +175,12 @@ class OneAPIBackend(FPGABackend):
         try:
             subprocess.run('which icpx', shell=True, cwd=builddir, check=True)
         except subprocess.CalledProcessError:
+            try:
+                import pytest
+
+                pytest.skip('icpx not present')
+            except ImportError:
+                pass
             raise RuntimeError('Could not find icpx. Please configure oneAPI appropriately')
         subprocess.run('cmake ..', shell=True, cwd=builddir, check=True)
         subprocess.run('make lib', shell=True, cwd=builddir, check=True)
@@ -200,6 +206,12 @@ class OneAPIBackend(FPGABackend):
         try:
             subprocess.run('which icpx', shell=True, cwd=builddir, check=True)
         except subprocess.CalledProcessError:
+            try:
+                import pytest
+
+                pytest.skip('icpx not present')
+            except ImportError:
+                pass
             raise RuntimeError('Could not find icpx. Please configure oneAPI appropriately')
         subprocess.run('cmake ..', shell=True, cwd=builddir, check=True)
         subprocess.run(f'make {build_type}', shell=True, cwd=builddir, check=True)
