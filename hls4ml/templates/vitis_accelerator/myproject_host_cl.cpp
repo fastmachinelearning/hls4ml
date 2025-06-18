363014826dcbb047fc9212dd8087a79cb77609dd
Djokzer <abivarmank@gmail.com>
2025-06-18 15:32:17 +0200

    feat[shared_library]: started a predict function in host code

--- hls4ml/templates/vitis_accelerator/myproject_host_cl.cpp
+++ hls4ml/templates/vitis_accelerator/myproject_host_cl.cpp
@@ -6,6 +6,12 @@
 #include "kernel_wrapper.h"
 #include "xcl2.hpp"
 
+void predict(double *input, uint64_t input_size, double *output, uint64_t output_size) {
+    // TODO : Modify the databatcher so it can take those arrays instead of reading and writing files.
+
+    return;
+}
+
 int main(int argc, char **argv) {
 
     Params params(argc, argv);
