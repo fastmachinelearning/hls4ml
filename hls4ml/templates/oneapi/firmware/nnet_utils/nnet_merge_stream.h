78cc97e818f906f98421c6e114dab48a0120d1e2
Chang Sun <chsun@cern.ch>
2025-06-27 01:36:23 -0700

    more oneapi fix

--- hls4ml/templates/oneapi/firmware/nnet_utils/nnet_merge_stream.h
+++ hls4ml/templates/oneapi/firmware/nnet_utils/nnet_merge_stream.h
@@ -107,8 +107,9 @@ MaxLoop:
     MaxPack:
         #pragma unroll
         for (int j = 0; j < outputSize; j++) {
-            out_data[j] = static_cast<typename ExtractPipeType<res_pipe>::value_type::value_type>(
-                (in_data1[j] > in_data2[j]) ? static_cast<res_T>(in_data1[j]) : static_cast<res_T>(in_data2[j]));
+            out_data[j] = (in_data1[j] > in_data2[j])
+                              ? static_cast<typename ExtractPipeType<res_pipe>::value_type::value_type>(in_data1[j])
+                              : static_cast<typename ExtractPipeType<res_pipe>::value_type::value_type>(in_data2[j]);
         }
 
         res_pipe::write(out_data);
@@ -130,8 +131,9 @@ MinLoop:
     MinPack:
         #pragma unroll
         for (int j = 0; j < outputSize; j++) {
-            out_data[j] = static_cast<typename ExtractPipeType<res_pipe>::value_type::value_type>(
-                (in_data1[j] < in_data2[j]) ? static_cast<res_T>(in_data1[j]) : static_cast<res_T>(in_data2[j]));
+            out_data[j] = (in_data1[j] < in_data2[j])
+                              ? static_cast<typename ExtractPipeType<res_pipe>::value_type::value_type>(in_data1[j])
+                              : static_cast<typename ExtractPipeType<res_pipe>::value_type::value_type>(in_data2[j]);
         }
 
         res_pipe::write(out_data);
