d14da6c60e86453ef1ec99e1741f96d6eea69902
laurilaatu <l.laatu@imperial.ac.uk>
2025-07-17 16:31:30 +0100

    Remove commented lines

--- hls4ml/templates/oneapi/firmware/nnet_utils/nnet_code_gen.h
+++ hls4ml/templates/oneapi/firmware/nnet_utils/nnet_code_gen.h
@@ -1,14 +1,6 @@
 #ifndef NNET_INSTR_GEN_H_
 #define NNET_INSTR_GEN_H_
 
-//#include "nnet_conv1d_latency.h"
-//#include "nnet_helpers.h"
-
-//#include "hls_stream.h"
-//#include "nnet_common.h"
-//#include "nnet_function_stubs.h"
-//#include "nnet_mult.h"
-
 namespace nnet {
 
 // hls4ml insert code
