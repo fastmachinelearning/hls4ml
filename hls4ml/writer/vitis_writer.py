56fb6d6df4d3ac6cf6b1c39fa7c56db17edc1565
Benjamin Ramhorst <benjamin.ramhorst@inf.ethz.ch>
2025-07-23 21:07:32 +0200

    Add overwrite for Vitis HLS build_prj, which exists but isn't being used.

--- hls4ml/writer/vitis_writer.py
+++ hls4ml/writer/vitis_writer.py
@@ -56,6 +56,14 @@ class VitisWriter(VivadoWriter):
         dstpath = f'{model.config.get_output_dir()}/build_opt.tcl'
         copyfile(srcpath, dstpath)
 
+    def write_build_prj_override(self, model):
+        filedir = Path(__file__).parent
+
+        # build_prj.tcl
+        srcpath = (filedir / '../templates/vitis/build_prj.tcl').resolve()
+        dstpath = f'{model.config.get_output_dir()}/build_prj.tcl'
+        copyfile(srcpath, dstpath)
+
     def write_hls(self, model):
         """
         Write the HLS project. Calls the steps from VivadoWriter, adapted for Vitis
@@ -63,5 +71,6 @@ class VitisWriter(VivadoWriter):
         super().write_hls(model)
         self.write_nnet_utils_overrides(model)
         self.write_board_script_override(model)
+        self.write_build_prj_override(model)
         self.write_build_opts(model)
         self.write_tar(model)
