235bc2dfd407876acc8663afe1d942dd81405b60
tanawin <tanawin1701d@outlook.com>
2025-08-15 17:32:02 +0200

    draft write_hls initialization

--- hls4ml/writer/vitis_unified_writer/__init__.py
+++ hls4ml/writer/vitis_unified_writer/__init__.py
@@ -5,13 +5,22 @@ from shutil import copyfile
 
 from hls4ml.writer.vitis_writer import VitisWriter
 
+import build_gen       as bg
+import meta_gen        as mtg
+import mm_gen          as mmg
+import stream_gen      as sg
+import test_bridge_gen as tbg
+import test_cosim_gen  as tcg
+
+from meta import VitisUnifiedWriterMeta
+
 
 
 class VitisUnifiedWriter(VitisWriter):
 
     def __init__(self):
         super().__init__()
-        self.vitis_unified_config = None
+        self.writer_meta = VitisUnifiedWriterMeta()
 
     def write_hls(self, model, is_multigraph=False):
 
@@ -21,13 +30,16 @@ class VitisUnifiedWriter(VitisWriter):
             model.config, model.get_input_variables(), model.get_output_variables()
         )
         super().write_hls(model)
-        if not is_multigraph:
-            self.write_board_script(model)
-            self.write_driver(model)
-            self.write_wrapper_test(model)
-            self.write_axi_wrapper(model)
-            self.modify_build_script(model)
-            self.write_new_tar(model)
-        else:
-            self.write_bridge_multigraph(model)
+        sg.write_axi_wrapper  (self.writer_meta, model)
+        bg .write_board_script(self.writer_meta, model)
+        bg .write_driver      (self.writer_meta, model)
+        bg.modify_build_script(self.writer_meta, model)
+        tcg.write_wrapper_test(self.writer_meta, model)
+
+
+        #self.write_new_tar(model)
+        #if not is_multigraph:
+
+        #else:
+        #    self.write_bridge_multigraph(model)
             # self.modify_write_build_script_multigraph(model)
