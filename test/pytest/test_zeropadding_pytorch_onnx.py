152cd6e836dd47a83d040746eadecc283e11bf5c
Vladimir Loncar <vloncar@users.noreply.github.com>
2025-10-31 20:56:43 +0100

    Make ONNX models manually instead of using torch's export

--- test/pytest/test_zeropadding_pytorch_onnx.py
+++ test/pytest/test_zeropadding_pytorch_onnx.py
@@ -1,15 +1,43 @@
 from pathlib import Path
 
 import numpy as np
-import qonnx.util.cleanup
-import torch
 import torch.nn as nn
-from qonnx.core.modelwrapper import ModelWrapper
+from onnx import TensorProto, helper
 
 from hls4ml.converters import convert_from_onnx_model, convert_from_pytorch_model
 from hls4ml.utils.config import config_from_onnx_model, config_from_pytorch_model
 
 test_root_path = Path(__file__).parent
+
+
+def _make_constantpad_onnx_1d():
+    input_tensor = helper.make_tensor_value_info('global_in', TensorProto.FLOAT, [1, 2, 4])
+    output_tensor = helper.make_tensor_value_info('global_out', TensorProto.FLOAT, [1, 2, 9])
+    pads_tensor = helper.make_tensor_value_info('pads', TensorProto.INT64, [6])
+    value_tensor = helper.make_tensor_value_info('value', TensorProto.FLOAT, [])
+
+    # Pads = [N_before, C_before, W_before, N_after, C_after, W_after]
+    pads = [0, 0, 2, 0, 0, 3]
+
+    pads_initializer = helper.make_tensor(name='pads', data_type=TensorProto.INT64, dims=[6], vals=pads)
+    value_initializer = helper.make_tensor(name='value', data_type=TensorProto.FLOAT, dims=[], vals=[0.0])
+
+    pad_node = helper.make_node(
+        'Pad', name='const_pad', inputs=['global_in', 'pads', 'value'], outputs=['global_out'], mode='constant'
+    )
+
+    graph = helper.make_graph(
+        nodes=[pad_node],
+        name='Pad1DGraph',
+        inputs=[input_tensor],
+        outputs=[output_tensor],
+        initializer=[pads_initializer, value_initializer],
+        value_info=[pads_tensor, value_tensor],
+    )
+
+    model = helper.make_model(graph)
+
+    return model
 
 
 def test_constantpad_1d():
@@ -30,10 +58,7 @@ def test_constantpad_1d():
 
     hls_model_pytorch.compile()
 
-    onnx_path = str(test_root_path / 'hls4mlprj_constpad_1d/pad1d.onnx')
-    torch.onnx.export(model, torch.randn(1, 2, 4), onnx_path, dynamo=True)
-    qonnx.util.cleanup.cleanup(onnx_path, out_file=onnx_path)
-    pad1d_onnx = ModelWrapper(onnx_path)
+    pad1d_onnx = _make_constantpad_onnx_1d()
 
     config_onnx = config_from_onnx_model(pad1d_onnx)
     hls_model_onnx = convert_from_onnx_model(
@@ -47,6 +72,36 @@ def test_constantpad_1d():
     pred_onnx = hls_model_onnx.predict(input_data)
 
     np.testing.assert_allclose(pred_pytorch, pred_onnx, rtol=0, atol=1e-5)
+
+
+def _make_constantpad_onnx_2d():
+    input_tensor = helper.make_tensor_value_info('global_in', TensorProto.FLOAT, [1, 2, 3, 4])
+    output_tensor = helper.make_tensor_value_info('global_out', TensorProto.FLOAT, [1, 2, 10, 7])
+    pads_tensor = helper.make_tensor_value_info('pads', TensorProto.INT64, [8])
+    value_tensor = helper.make_tensor_value_info('value', TensorProto.FLOAT, [])
+
+    # Pads = [N_before, C_before, H_before, W_before, N_after, C_after, H_after, W_after]
+    pads = [0, 0, 3, 1, 0, 0, 4, 2]
+
+    pads_initializer = helper.make_tensor(name='pads', data_type=TensorProto.INT64, dims=[8], vals=pads)
+    value_initializer = helper.make_tensor(name='value', data_type=TensorProto.FLOAT, dims=[], vals=[0.0])
+
+    pad_node = helper.make_node(
+        'Pad', name='const_pad', inputs=['global_in', 'pads', 'value'], outputs=['global_out'], mode='constant'
+    )
+
+    graph = helper.make_graph(
+        nodes=[pad_node],
+        name='Pad2DGraph',
+        inputs=[input_tensor],
+        outputs=[output_tensor],
+        initializer=[pads_initializer, value_initializer],
+        value_info=[pads_tensor, value_tensor],
+    )
+
+    model = helper.make_model(graph)
+
+    return model
 
 
 def test_constantpad_2d():
@@ -67,10 +122,7 @@ def test_constantpad_2d():
 
     hls_model_pytorch.compile()
 
-    onnx_path = str(test_root_path / 'hls4mlprj_constpad_2d/pad2d.onnx')
-    torch.onnx.export(model, torch.randn(1, 2, 3, 4), onnx_path, dynamo=True)
-    qonnx.util.cleanup.cleanup(onnx_path, out_file=onnx_path)
-    pad2d_onnx = ModelWrapper(onnx_path)
+    pad2d_onnx = _make_constantpad_onnx_2d()
 
     config_onnx = config_from_onnx_model(pad2d_onnx)
     hls_model_onnx = convert_from_onnx_model(
