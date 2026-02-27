b52af9c843da66c84ee36c59be24737988fb4be9
tanawin <tanawin1701d@outlook.com>
2026-02-27 15:11:07 +0700

    add last packet checking to axi_stream

--- hls4ml/templates/vitis_unified/myproject_axi_stream.cpp
+++ hls4ml/templates/vitis_unified/myproject_axi_stream.cpp
@@ -1,6 +1,7 @@
 // hls-fpga-machine-learning insert include
 
-void load_input(hls::stream<dma_data_packet> &axi_input_stream, hls::stream<INPUT_LAYER_TYPE> &model_input_stream) {
+void load_input(hls::stream<dma_data_packet> &axi_input_stream, hls::stream<INPUT_LAYER_TYPE> &model_input_stream,
+                bool &is_last) {
 load_input_loop:
     dma_data_packet axi_packet;
     for (unsigned chunk_idx = 0; chunk_idx < N_IN / INPUT_LAYER_TYPE::size; ++chunk_idx) {
@@ -8,19 +9,24 @@ load_input_loop:
         for (unsigned elem_idx = 0; elem_idx < INPUT_LAYER_TYPE::size; elem_idx++) {
             axi_input_stream.read(axi_packet);
             input_chunk[elem_idx] = axi_packet.data;
+            is_last = axi_packet.last;
         }
         model_input_stream.write(input_chunk);
     }
+    axi_packet.last = 0;
 }
 
-void store_result(hls::stream<OUTPUT_LAYER_TYPE> &model_output_stream, hls::stream<dma_data_packet> &axi_output_stream) {
+void store_result(hls::stream<OUTPUT_LAYER_TYPE> &model_output_stream, hls::stream<dma_data_packet> &axi_output_stream,
+                  bool &is_last) {
 store_result_loop:
     dma_data_packet axi_packet;
     for (unsigned chunk_idx = 0; chunk_idx < N_OUT / OUTPUT_LAYER_TYPE::size; ++chunk_idx) {
         OUTPUT_LAYER_TYPE output_chunk = model_output_stream.read();
         for (unsigned elem_idx = 0; elem_idx < OUTPUT_LAYER_TYPE::size; elem_idx++) {
             axi_packet.data = (OUTPUT_GMEM_TYPE)(output_chunk[elem_idx]);
-            axi_packet.last = ((chunk_idx + 1) * (elem_idx + 1) == N_OUT);
+            if (is_last) {
+                axi_packet.last = ((chunk_idx + 1) * (elem_idx + 1) == N_OUT);
+            }
             axi_output_stream.write(axi_packet);
         }
     }
@@ -34,7 +40,9 @@ void MY_PROJECT_TOP_FUNC(hls::stream<dma_data_packet> &axi_input_stream, hls::st
 
     #pragma HLS DATAFLOW
 
-    load_input(axi_input_stream, model_input_stream);
+    bool is_last = false;
+
+    load_input(axi_input_stream, model_input_stream, is_last);
     MY_PROJECT(model_input_stream, model_output_stream);
-    store_result(model_output_stream, axi_output_stream);
+    store_result(model_output_stream, axi_output_stream, is_last);
 }
