8055e71fde22c8c6696161e74d9828c34eeddad5
Jovan Mitrevski <jmitrevs@fnal.gov>
2026-03-22 19:10:56 -0500

    update nnet_stream_beat to use return values

--- hls4ml/templates/oneapi/firmware/nnet_utils/nnet_stream_beat.h
+++ hls4ml/templates/oneapi/firmware/nnet_utils/nnet_stream_beat.h
@@ -11,53 +11,43 @@ struct sideband_config {
 };
 
 // *************************************************
-// Remove sideband and passes it to end via skip pipe
+// Remove sideband and passes it to end via return
 // *************************************************
-template <class data_pipe, class res_pipe, class skip_pipe, typename CONFIG_T>
-[[intel::use_stall_enable_clusters]] void extract_sideband_stream() {
+template <class data_pipe, class res_pipe, typename CONFIG_T>
+[[intel::use_stall_enable_clusters]] bool extract_sideband_stream() {
 
     [[intel::fpga_register]] typename ExtractPipeType<res_pipe>::value_type out_data;
 
-    bool sop = false;
     bool eop = false;
 
 LinearActLoop:
-    [[intel::initiation_interval(1)]] while (!eop) {
-        for (int i = 0; i < CONFIG_T::n_in / std::tuple_size<typename ExtractPipeType<res_pipe>::value_type>{}; i++) {
-            auto in_data = data_pipe::read();
+    [[intel::initiation_interval(
+        1)]] for (int i = 0; i < CONFIG_T::n_in / std::tuple_size<typename ExtractPipeType<res_pipe>::value_type>{}; i++) {
+        auto in_data = data_pipe::read();
 
-        LinearPackLoop:
-            #pragma unroll
-            for (int j = 0; j < std::tuple_size<typename ExtractPipeType<res_pipe>::value_type>{}; j++) {
-                out_data[j] = in_data.data[j];
-            }
-
-            res_pipe::write(out_data);
-
-            if (i == 0) {
-                sop = in_data.sop;
-            }
-            eop = in_data.eop;
+    LinearPackLoop:
+        #pragma unroll
+        for (int j = 0; j < std::tuple_size<typename ExtractPipeType<res_pipe>::value_type>{}; j++) {
+            out_data[j] = in_data.data[j];
         }
-        typename nnet::ExtractPipeType<skip_pipe>::value_type skip_data; // this is a two-element array, {sop, eop}.
-        skip_data[0] = sop;
-        skip_data[1] = eop;
-        skip_pipe::write(skip_data);
+
+        res_pipe::write(out_data);
+
+        eop = in_data.eop;
     }
+    return (!eop);
 }
 
 // *************************************************
-// Recieves sideband via skip pipe, and makees it sideband
+// Recieves sideband via call argument, and makes it sideband
 // *************************************************
 
-template <class data_pipe, class res_pipe, class skip_pipe, typename CONFIG_T>
-[[intel::use_stall_enable_clusters]] void merge_sideband_stream() {
+template <class data_pipe, class res_pipe, typename CONFIG_T>
+[[intel::use_stall_enable_clusters]] void merge_sideband_stream(bool keep_going, uint32_t count) {
     using ResT = typename ExtractDataType<typename ExtractPipeType<res_pipe>::value_type>::value_type;
     [[intel::fpga_register]] typename ExtractPipeType<res_pipe>::value_type out_data;
 
     constexpr auto num_transfers = CONFIG_T::n_in / std::tuple_size<ResT>{};
-
-    auto skip_data = skip_pipe::read();
 
 LinearActLoop:
     [[intel::initiation_interval(1)]] for (int i = 0; i < num_transfers; i++) {
@@ -68,8 +58,8 @@ LinearActLoop:
         for (int j = 0; j < std::tuple_size<ResT>{}; j++) {
             out_data.data[j] = in_data[j];
         }
-        out_data.sop = (i == 0) ? static_cast<bool>(skip_data[0]) : false;
-        out_data.eop = (i == num_transfers - 1) ? static_cast<bool>(skip_data[1]) : false;
+        out_data.sop = (i == 0) ? count == 0 : false;
+        out_data.eop = (i == num_transfers - 1) ? !keep_going : false;
         res_pipe::write(out_data);
     }
 }
