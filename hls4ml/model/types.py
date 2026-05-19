5e0196cd566c9d4de79b0c06f6be9aec75679734
Haris1299 <hmurugan@uni-mainz.de>
2026-05-19 20:16:47 +0200

    use math.prod

--- hls4ml/model/types.py
+++ hls4ml/model/types.py
@@ -5,6 +5,7 @@ The Precision types are given names for convenience (``NamedType``). Named types
 higher-dimensional tensors, which are defined as arrays or FIFO streams in the generated code.
 """
 
+import math
 from enum import Enum
 
 import numpy as np
@@ -837,10 +838,7 @@ class ExponentWeightVariable(WeightVariable):
 
     def __iter__(self):
         data = self._format()
-        if hasattr(np, 'product'):
-            self._iterator = iter(data.reshape((np.product(data.shape[:-1]), 2)))
-        else:
-            self._iterator = iter(data.reshape((np.prod(data.shape[:-1]), 2)))
+        self._iterator = iter(data.reshape((math.prod(data.shape[:-1]), 2)))
         return self
 
     def __next__(self):
