f4342b0c77243df511d6b7bd0ae0b1cc1c25e8e0
makoeppel <koeppel.ma@googlemail.com>
2026-05-21 00:40:28 +0200

    add back all tests

--- test/pytest/generate_ci_yaml.py
+++ test/pytest/generate_ci_yaml.py
@@ -88,58 +88,58 @@ def generate_test_yaml(test_root='.'):
     idxs = sorted(idxs, key=lambda i: f'{need_example_models[i]}_{path_to_name(test_paths[i])}')
 
     yml = None
-    # for batch_idxs in batched(idxs, n_test_files_per_yml):
-    #     batch_paths: list[Path] = [test_paths[i] for i in batch_idxs]
-    #     names = [path_to_name(path) for path in batch_paths]
-    #     name = '+'.join(names)
-    #     test_files = ' '.join([str(path.relative_to(test_root)) for path in batch_paths])
-    #     batch_need_example_model = int(any([need_example_models[i] for i in batch_idxs]))
-    #     diff_yml = yaml.safe_load(template.format(name, '.pytest', test_files, batch_need_example_model))
-    #     if yml is None:
-    #         yml = diff_yml
-    #     else:
-    #         yml.update(diff_yml)
+    for batch_idxs in batched(idxs, n_test_files_per_yml):
+        batch_paths: list[Path] = [test_paths[i] for i in batch_idxs]
+        names = [path_to_name(path) for path in batch_paths]
+        name = '+'.join(names)
+        test_files = ' '.join([str(path.relative_to(test_root)) for path in batch_paths])
+        batch_need_example_model = int(any([need_example_models[i] for i in batch_idxs]))
+        diff_yml = yaml.safe_load(template.format(name, '.pytest', test_files, batch_need_example_model))
+        if yml is None:
+            yml = diff_yml
+        else:
+            yml.update(diff_yml)
 
-    # test_paths = [path for path in test_root.glob('**/test_*.py') if path.stem in LONGLIST]
-    # for path in test_paths:
-    #     name = path.stem.replace('test_', '')
-    #     test_file = str(path.relative_to(test_root))
-    #     needs_examples = uses_example_model(path)
-    #     diff_yml = yaml.safe_load(template.format(name, '.pytest', test_file, int(needs_examples)))
-    #     yml.update(diff_yml)
+    test_paths = [path for path in test_root.glob('**/test_*.py') if path.stem in LONGLIST]
+    for path in test_paths:
+        name = path.stem.replace('test_', '')
+        test_file = str(path.relative_to(test_root))
+        needs_examples = uses_example_model(path)
+        diff_yml = yaml.safe_load(template.format(name, '.pytest', test_file, int(needs_examples)))
+        yml.update(diff_yml)
 
-    # test_paths = [path for path in test_root.glob('**/test_*.py') if path.stem in SPLIT_BY_TEST_CASE]
-    # for path in test_paths:
-    #     stem = path.stem
-    #     name_base = stem.replace('test_', '')
-    #     test_file = str(path.relative_to(test_root))
-    #     test_ids = collect_test_functions_from_ast(test_file)
-    #     chunk_size = SPLIT_BY_TEST_CASE[stem]
-    #     needs_examples = uses_example_model(path)
+    test_paths = [path for path in test_root.glob('**/test_*.py') if path.stem in SPLIT_BY_TEST_CASE]
+    for path in test_paths:
+        stem = path.stem
+        name_base = stem.replace('test_', '')
+        test_file = str(path.relative_to(test_root))
+        test_ids = collect_test_functions_from_ast(test_file)
+        chunk_size = SPLIT_BY_TEST_CASE[stem]
+        needs_examples = uses_example_model(path)
 
-    #     for i, batch in enumerate(batched(test_ids, chunk_size)):
-    #         job_name = f'{name_base}_part{i}'
-    #         test_file_args = ' '.join(batch).strip().replace('\n', ' ')
-    #         diff_yml = yaml.safe_load(template.format(job_name, '.pytest', test_file_args, int(needs_examples)))
-    #         if yml is None:
-    #             yml = diff_yml
-    #         else:
-    #             yml.update(diff_yml)
+        for i, batch in enumerate(batched(test_ids, chunk_size)):
+            job_name = f'{name_base}_part{i}'
+            test_file_args = ' '.join(batch).strip().replace('\n', ' ')
+            diff_yml = yaml.safe_load(template.format(job_name, '.pytest', test_file_args, int(needs_examples)))
+            if yml is None:
+                yml = diff_yml
+            else:
+                yml.update(diff_yml)
 
-    # keras3_paths = [path for path in test_root.glob('**/test_*.py') if path.stem in KERAS3_LIST]
-    # keras3_need_examples = [uses_example_model(path) for path in keras3_paths]
+    keras3_paths = [path for path in test_root.glob('**/test_*.py') if path.stem in KERAS3_LIST]
+    keras3_need_examples = [uses_example_model(path) for path in keras3_paths]
 
-    # k3_idxs = list(range(len(keras3_need_examples)))
-    # k3_idxs = sorted(k3_idxs, key=lambda i: f'{keras3_need_examples[i]}_{path_to_name(keras3_paths[i])}')
+    k3_idxs = list(range(len(keras3_need_examples)))
+    k3_idxs = sorted(k3_idxs, key=lambda i: f'{keras3_need_examples[i]}_{path_to_name(keras3_paths[i])}')
 
-    # for batch_idxs in batched(k3_idxs, n_test_files_per_yml):
-    #     batch_paths: list[Path] = [keras3_paths[i] for i in batch_idxs]
-    #     names = [path_to_name(path) for path in batch_paths]
-    #     name = 'keras3-' + '+'.join(names)
-    #     test_files = ' '.join([str(path.relative_to(test_root)) for path in batch_paths])
-    #     batch_need_example_model = int(any([keras3_need_examples[i] for i in batch_idxs]))
-    #     diff_yml = yaml.safe_load(template.format(name, '.pytest-keras3-only', test_files, batch_need_example_model))
-    #     yml.update(diff_yml)
+    for batch_idxs in batched(k3_idxs, n_test_files_per_yml):
+        batch_paths: list[Path] = [keras3_paths[i] for i in batch_idxs]
+        names = [path_to_name(path) for path in batch_paths]
+        name = 'keras3-' + '+'.join(names)
+        test_files = ' '.join([str(path.relative_to(test_root)) for path in batch_paths])
+        batch_need_example_model = int(any([keras3_need_examples[i] for i in batch_idxs]))
+        diff_yml = yaml.safe_load(template.format(name, '.pytest-keras3-only', test_files, batch_need_example_model))
+        yml.update(diff_yml)
 
     qkeras3_paths = [path for path in test_root.glob('**/test_*.py') if path.stem in QKERAS3_LIST]
     qkeras3_need_examples = [uses_example_model(path) for path in qkeras3_paths]
@@ -154,8 +154,7 @@ def generate_test_yaml(test_root='.'):
         test_files = ' '.join([str(path.relative_to(test_root)) for path in batch_paths])
         batch_need_example_model = int(any([qkeras3_need_examples[i] for i in batch_idxs]))
         diff_yml = yaml.safe_load(template.format(name, '.pytest-qkeras-v3-only', test_files, batch_need_example_model))
-        yml = diff_yml
-        #yml.update(diff_yml)
+        yml.update(diff_yml)
 
     return yml
 
