314c1dcaa273a7d7cfd48fb4ee57cbaff9752ce5
Jovan Mitrevski <jmitrevs@fnal.gov>
2026-08-21 12:36:27 -0500

    pre-commit fix

--- test/pytest/test_softmax.py
+++ test/pytest/test_softmax.py
@@ -72,8 +72,8 @@ def test_softmax(
     if custom_accum:
         if backend not in ['Vivado', 'Vitis']:
             pytest.skip('Custom accumulators are only supported for Vivado and Vitis backends')
-        #W, I = map(int, input_bits.split(','))  # noqa: E741
-        #cfg['LayerName']['softmax']['Precision']['inv_inp'] = f'ufixed<{W + 2},{I + 2}>'
+        # W, I = map(int, input_bits.split(','))  # noqa: E741
+        # cfg['LayerName']['softmax']['Precision']['inv_inp'] = f'ufixed<{W + 2},{I + 2}>'
     inp_layer_name = next(iter(cfg['LayerName'].keys()))
     cfg['LayerName'][inp_layer_name]['Precision']['result'] = f'fixed<{input_bits}, RND, SAT>'
 
