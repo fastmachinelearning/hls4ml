796b32392a51313fd724135794723a40ef6e7c11
Sebastian Cao <cycsmail@gmail.com>
2026-09-23 21:11:06 +0800

    Serialize NumPy arrays nested inside lists and tuples (#1540)
    * Serialize NumPy arrays nested inside lists and tuples
    
    * address review: per-index prefix for dicts in sequences, note on tuples
    
    ---------
    
    Co-authored-by: Jan-Frederik Schulte <jschulte@cern.ch>

---
+++ test/pytest/test_serialization_utils.py
@@ -0,0 +1,85 @@
+import json
+
+import numpy as np
+
+from hls4ml.utils.serialization import _deserialize_array_attrs, _serialize_array_attrs
+
+
+def test_serialize_array_attrs_in_sequences(tmp_path):
+    """Arrays nested inside lists/tuples (e.g., mask_kbi of FixedPointQuantizer) should be serialized. See #1526."""
+    model_arch = {
+        'layer': {
+            'state': {
+                'attributes': {
+                    'name': 'layer',
+                    'mask_kbi': (
+                        np.array([[0, 1, 0]], dtype=np.int16),
+                        np.array([[1, 2, 3]], dtype=np.int16),
+                        np.array([[4, 5, 6]], dtype=np.int16),
+                    ),
+                    'nested_list': [[np.array([1.5, 2.5])], 'foo', np.int32(7)],
+                }
+            }
+        }
+    }
+
+    for layer_name, layer_dict in model_arch.items():
+        _serialize_array_attrs(layer_dict, layer_name, tmp_path)
+
+    attrs = model_arch['layer']['state']['attributes']
+    assert attrs['mask_kbi'] == (
+        '@ndarray:layer_mask_kbi_0.npy',
+        '@ndarray:layer_mask_kbi_1.npy',
+        '@ndarray:layer_mask_kbi_2.npy',
+    )
+    assert attrs['nested_list'] == [['@ndarray:layer_nested_list_0_0.npy'], 'foo', 7]
+
+    # The resulting architecture must be JSON serializable
+    arch_json = json.dumps(model_arch)
+
+    # Round-trip: arrays inside (JSON-decoded) lists should be restored on deserialization
+    restored_arch = json.loads(arch_json)
+    _deserialize_array_attrs(tmp_path, restored_arch)
+
+    restored_attrs = restored_arch['layer']['state']['attributes']
+    k, b, i = restored_attrs['mask_kbi']
+    np.testing.assert_array_equal(k, np.array([[0, 1, 0]], dtype=np.int16))
+    np.testing.assert_array_equal(b, np.array([[1, 2, 3]], dtype=np.int16))
+    np.testing.assert_array_equal(i, np.array([[4, 5, 6]], dtype=np.int16))
+    np.testing.assert_array_equal(restored_attrs['nested_list'][0][0], np.array([1.5, 2.5]))
+    assert restored_attrs['nested_list'][1:] == ['foo', 7]
+
+
+def test_serialize_array_attrs_dicts_in_sequence(tmp_path):
+    """Dicts inside a sequence get a per-index prefix, so their arrays do not overwrite each other."""
+    model_arch = {
+        'layer': {
+            'state': {
+                'attributes': {
+                    'name': 'layer',
+                    'quantizers': [
+                        {'scale': np.array([1, 2, 3], dtype=np.int16)},
+                        {'scale': np.array([4, 5, 6], dtype=np.int16)},
+                    ],
+                }
+            }
+        }
+    }
+
+    for layer_name, layer_dict in model_arch.items():
+        _serialize_array_attrs(layer_dict, layer_name, tmp_path)
+
+    attrs = model_arch['layer']['state']['attributes']
+    assert attrs['quantizers'] == [
+        {'scale': '@ndarray:layer_quantizers_0_scale.npy'},
+        {'scale': '@ndarray:layer_quantizers_1_scale.npy'},
+    ]
+    assert (tmp_path / 'layer_quantizers_0_scale.npy').is_file()
+    assert (tmp_path / 'layer_quantizers_1_scale.npy').is_file()
+
+    restored_arch = json.loads(json.dumps(model_arch))
+    _deserialize_array_attrs(tmp_path, restored_arch)
+
+    first, second = restored_arch['layer']['state']['attributes']['quantizers']
+    np.testing.assert_array_equal(first['scale'], np.array([1, 2, 3], dtype=np.int16))
+    np.testing.assert_array_equal(second['scale'], np.array([4, 5, 6], dtype=np.int16))
